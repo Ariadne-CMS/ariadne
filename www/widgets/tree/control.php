46877c2f788fc0ab96745558d474728eb7703d6f
Auke van Slooten <auke@muze.nl>
2001-03-23 13:06:16 +0000

    new layout, more explorer style
    git-svn-id: https://svn.muze.nl/svn/ariadne-php5/trunk@178 7882d7ee-46e0-0310-a255-a28a61b628e4

--- www/widgets/tree/control.php
+++ www/widgets/tree/control.php
@@ -1,15 +1,46 @@
-<body bgcolor='#404074'>
-<table border="0" cellpadding="0" cellspacing="0" width="90%" align="center">
+<html>
+<head>
+<style>
+TD {
+	FONT: 13px sans-serif;
+}
+BODY 	{
+	FONT: 13px sans-serif;
+	BORDER: 0;
+  <?php
+    global $HTTP_USER_AGENT;
+    if (eregi(".*msie ([0-9]+).*",$HTTP_USER_AGENT, $regs) &&
+       $regs[1] && (intval($regs[1])>=4)) { //ie4+
+  ?>
+	BACKGROUND-COLOR: buttonface;
+  <?php
+    } else {
+  ?>
+	BACKGROUND-COLOR: #BBBBBB;
+  <?php
+    }
+  ?>
+}
+INPUT {
+	FONT: 13px sans-serif;
+}
+</style>
+</head>
+<body>
+<fieldset>
+<table border="0" height="100%" cellpadding="1" cellspacing="0" width="90%" align="center">
 <tr>
-  <td align="left">
-    <form action="<?php echo $PATH_INFO; ?>logoff.phtml" target="_top">
+  <form action="<?php echo $PATH_INFO; ?>logoff.phtml" target="_top">
+    <td align="left" valign="middle">
       <input type="submit" value="Log Off">
-    </form>
-  </td><td align="right">
-    <form action="<?php echo $PATH_INFO; ?>treeinit.phtml" target="treeload">
+    </td>
+  </form><form action="<?php echo $PATH_INFO; ?>treeinit.phtml" target="treeload">
+    <td align="right" valign="middle">
       <input type="submit" value="Refresh">
-    </form>
-  </td>
+    </td>
+  </form>
 </tr>
 </table>
+</fieldset>
 </body>
+</html>
