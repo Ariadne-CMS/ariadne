07faa8b373b0ba198c08682e9174ecf4cee4ac58
Auke van Slooten <auke@muze.nl>
2001-03-26 14:06:30 +0000

    Language values for HTMLEdit widget (ParagraphStyles).
    git-svn-id: https://svn.muze.nl/svn/ariadne-php5/trunk@180 7882d7ee-46e0-0310-a255-a28a61b628e4

---
+++ www/widgets/htmledit/ie/Inc/nls.js
@@ -0,0 +1,22 @@
+// nls info for blockformat commands (as DECMD_GETBLOCKFORMATNAMES doesn't
+// seem to work.
+var wgHTMLEditParagraphStyles=new Array;
+wgHTMLEditParagraphStyles.Normal=new Array;
+wgHTMLEditParagraphStyles.Normal[0]='Normal';
+wgHTMLEditParagraphStyles.Normal[1]='Heading 1';
+wgHTMLEditParagraphStyles.Normal[2]='Heading 2';
+wgHTMLEditParagraphStyles.Normal[3]='Heading 3';
+wgHTMLEditParagraphStyles.Normal[4]='Heading 4';
+wgHTMLEditParagraphStyles.Normal[5]='Heading 5';
+wgHTMLEditParagraphStyles.Normal[6]='Heading 6';
+wgHTMLEditParagraphStyles.Normal[7]='Address';
+wgHTMLEditParagraphStyles.Normal[8]='Formatted';
+wgHTMLEditParagraphStyles.Normaal=new Array;
+wgHTMLEditParagraphStyles.Normaal[0]='Normaal';
+wgHTMLEditParagraphStyles.Normaal[1]='Kop 1';
+wgHTMLEditParagraphStyles.Normaal[2]='Kop 2';
+wgHTMLEditParagraphStyles.Normaal[3]='Kop 3';
+wgHTMLEditParagraphStyles.Normaal[4]='Kop 4';
+wgHTMLEditParagraphStyles.Normaal[5]='Kop 5';
+wgHTMLEditParagraphStyles.Normaal[6]='Kop 6';
+wgHTMLEditParagraphStyles.Normaal[7]='Adres';
