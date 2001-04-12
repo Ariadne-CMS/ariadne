0111e8b31f933f4702b01fa168a82724589c3b24
Auke van Slooten <auke@muze.nl>
2001-04-12 14:29:04 +0000

    changed object_fs window size
    git-svn-id: https://svn.muze.nl/svn/ariadne-php5/trunk@220 7882d7ee-46e0-0310-a255-a28a61b628e4

--- www/widgets/menu/ie/menu.js
+++ www/widgets/menu/ie/menu.js
@@ -483,7 +483,7 @@ function showWindowedObjects(show) {
 // default values for opening windows
 windowprops=new Array();
 windowprops['common']='directories=no,location=no,menubar=no,status=no,toolbar=no,resizable=yes';
-windowprops['object_fs']=windowprops['common']+',width=450,height=250';
+windowprops['object_fs']=windowprops['common']+',height=100,width=400';
 windowprops['object_new']=windowprops['common']+',height=275,width=450';
 windowprops['edit_find']=windowprops['common']+',height=400,width=500';
 windowprops['edit_preferences']=windowprops['common']+',height=400,width=500';
