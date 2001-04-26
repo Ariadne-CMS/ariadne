3fa8c5f88b5acefe595992b522413e7c68797bf7
Auke van Slooten <auke@muze.nl>
2001-04-26 08:41:38 +0000

    changed textarea to <pre> </pre>
    git-svn-id: https://svn.muze.nl/svn/ariadne-php5/trunk@285 7882d7ee-46e0-0310-a255-a28a61b628e4

--- www/install/install.php
+++ www/install/install.php
@@ -1,5 +1,5 @@
 #!/usr/local/bin/php -q
-<form><textarea style="width: 100%;" cols=80 rows=24>
+<pre>
 <?php
   require("../ariadne.inc");
   require($ariadne."/configs/ariadne.phtml");
@@ -11,112 +11,114 @@
   
   echo "== creating main Ariadne Object Store\n\n";
 
-  $store->initialize();
+  if ($store->initialize()) {
 
-  $store->add_type("pshortcut","pobject");
-  $store->add_type("pshortcut","pshortcut");
-  $store->add_type("puser","pobject");
-  $store->add_type("puser","pdir");
-  $store->add_type("puser","puser");
-  $store->add_type("pgroup","pobject");
-  $store->add_type("pgroup","pdir");
-  $store->add_type("pgroup","pgroup");
-  $store->add_type("pfile","pobject");
-  $store->add_type("pfile","pfile");
-  $store->add_type("ppage","pobject");
-  $store->add_type("ppage","ppage");
-  $store->add_type("psite","pobject");
-  $store->add_type("psite","pdir");
-  $store->add_type("psite","psite");
-  $store->add_type("pphoto","pobject");
-  $store->add_type("pphoto","pphoto");
-  $store->add_type("pphoto","pfile");
-  $store->add_type("pphoto","ppage");
-  $store->add_type("pphotobook","pobject");
-  $store->add_type("pphotobook","pphotobook");
-  $store->add_type("pphotobook","pdir");
-  $store->add_type("pphotobook","pphoto");
+    $store->add_type("pshortcut","pobject");
+    $store->add_type("pshortcut","pshortcut");
+    $store->add_type("puser","pobject");
+    $store->add_type("puser","pdir");
+    $store->add_type("puser","puser");
+    $store->add_type("pgroup","pobject");
+    $store->add_type("pgroup","pdir");
+    $store->add_type("pgroup","pgroup");
+    $store->add_type("pfile","pobject");
+    $store->add_type("pfile","pfile");
+    $store->add_type("ppage","pobject");
+    $store->add_type("ppage","ppage");
+    $store->add_type("psite","pobject");
+    $store->add_type("psite","pdir");
+    $store->add_type("psite","psite");
+    $store->add_type("pphoto","pobject");
+    $store->add_type("pphoto","pphoto");
+    $store->add_type("pphoto","pfile");
+    $store->add_type("pphoto","ppage");
+    $store->add_type("pphotobook","pobject");
+    $store->add_type("pphotobook","pphotobook");
+    $store->add_type("pphotobook","pdir");
+    $store->add_type("pphotobook","pphoto");
 
-  $name["value"]["string"]=50;
-  $store->create_property("name", $name);
+    $name["value"]["string"]=50;
+    $store->create_property("name", $name);
 
-  $value["value"]["string"]=128;
-  $store->create_property("value",$value);
+    $value["value"]["string"]=128;
+    $store->create_property("value",$value);
 
-  $ptext["value"]["string"]=128;
-  $store->create_property("text",$ptext);
+    $ptext["value"]["string"]=128;
+    $store->create_property("text",$ptext);
 
-  $locked["id"]["string"]=16;
-  $locked["duration"]["number"]=1;
-  $store->create_property("locked", $locked);
+    $locked["id"]["string"]=16;
+    $locked["duration"]["number"]=1;
+    $store->create_property("locked", $locked);
 
-  $login["value"]["string"]=16;
-  $store->create_property("login", $login);
+    $login["value"]["string"]=16;
+    $store->create_property("login", $login);
 
-  $members["login"]["string"]=16;
-  $store->create_property("members", $members);
+    $members["login"]["string"]=16;
+    $store->create_property("members", $members);
 
-  // now install all pcalendar and pcalitem objects.
+    // now install all pcalendar and pcalitem objects.
 
-  $timeframe["start"]["number"]=1;
-  $timeframe["end"]["number"]=1;
-  $store->create_property("timeframe", $timeframe);
+    $timeframe["start"]["number"]=1;
+    $timeframe["end"]["number"]=1;
+    $store->create_property("timeframe", $timeframe);
 
-  $priority["value"]["number"]=1;
-  $store->create_property("priority", $priority);
+    $priority["value"]["number"]=1;
+    $store->create_property("priority", $priority);
 
-  $store->add_type("pcalitem","pobject");
-  $store->add_type("pcalitem","pcalitem");
-  $store->add_type("pcalendar","pobject");
-  $store->add_type("pcalendar","pdir");
-  $store->add_type("pcalendar","pcalendar");
+    $store->add_type("pcalitem","pobject");
+    $store->add_type("pcalitem","pcalitem");
+    $store->add_type("pcalendar","pobject");
+    $store->add_type("pcalendar","pdir");
+    $store->add_type("pcalendar","pcalendar");
 
-  // newspaper types and default objects
+    // newspaper types and default objects
 
-  $article["start"]["number"]=1;
-  $article["end"]["number"]=1;
-  $article["display"]["string"]=50;
-  $store->create_property("article", $article);
+    $article["start"]["number"]=1;
+    $article["end"]["number"]=1;
+    $article["display"]["string"]=50;
+    $store->create_property("article", $article);
 
-  $published["value"]["number"]=1;
-  $store->create_property("published",$published);
+    $published["value"]["number"]=1;
+    $store->create_property("published",$published);
 
-  $store->add_type("pscenario","pobject");
-  $store->add_type("pscenario","pscenario");
-  $store->add_type("particle","pobject");
-  $store->add_type("particle","ppage");
-  $store->add_type("particle","particle");
-  $store->add_type("pnewspaper","pobject");
-  $store->add_type("pnewspaper","pdir");
-  $store->add_type("pnewspaper","pnewspaper");
+    $store->add_type("pscenario","pobject");
+    $store->add_type("pscenario","pscenario");
+    $store->add_type("particle","pobject");
+    $store->add_type("particle","ppage");
+    $store->add_type("particle","particle");
+    $store->add_type("pnewspaper","pobject");
+    $store->add_type("pnewspaper","pdir");
+    $store->add_type("pnewspaper","pnewspaper");
 
-  // Addressbook types and default objects
+    // Addressbook types and default objects
 
-  $store->add_type("paddressbook","pobject");
-  $store->add_type("paddressbook","pdir");
-  $store->add_type("paddressbook","paddressbook");
-  $store->add_type("pperson","pobject");
-  $store->add_type("pperson","address");
-  $store->add_type("pperson","pperson");
-  $store->add_type("porganization","pobject");
-  $store->add_type("porganization","address");
-  $store->add_type("porganization","porganization");
+    $store->add_type("paddressbook","pobject");
+    $store->add_type("paddressbook","pdir");
+    $store->add_type("paddressbook","paddressbook");
+    $store->add_type("pperson","pobject");
+    $store->add_type("pperson","address");
+    $store->add_type("pperson","pperson");
+    $store->add_type("porganization","pobject");
+    $store->add_type("porganization","address");
+    $store->add_type("porganization","porganization");
 
-  $address["street"]["string"]=50;
-  $address["zipcode"]["string"]=6;
-  $address["city"]["string"]=50;
-  $address["country"]["string"]=50;
-  $store->create_property("address", $address);
+    $address["street"]["string"]=50;
+    $address["zipcode"]["string"]=6;
+    $address["city"]["string"]=50;
+    $address["country"]["string"]=50;
+    $store->create_property("address", $address);
 
-  echo "== importing ariadne.ax file\n\n";
-  $import_path="/"; $ax_file="ariadne.ax";
-//  printf("install: ".$AR->ax->cmd_untar."\n", $ax_file, "");
-  include($ariadne."/includes/import.phtml");
+    echo "== importing ariadne.ax file\n\n";
+    $import_path="/"; $ax_file="ariadne.ax";
+  //  printf("install: ".$AR->ax->cmd_untar."\n", $ax_file, "");
+    include($ariadne."/includes/import.phtml");
 
-  if ($error) {
-    error($error);
+    if ($error) {
+      error($error);
+    }
+  } else {
+	error("store not initialized.");
   }
-
   $store->close(); 
 
   // session store
@@ -127,12 +129,15 @@
   
   echo "== creating Ariadne Session Store\n\n";
 
-  $sessionstore->initialize();
+  if ($sessionstore->initialize()) {
 
-  $sessionstore->add_type("psession","pobject");
-  $sessionstore->add_type("psession","psession");
+    $sessionstore->add_type("psession","pobject");
+    $sessionstore->add_type("psession","psession");
 
+  } else {
+	error("store not initialized.");
+  }
   $sessionstore->close();
   
 ?>
-</textarea></form>
+</pre>
