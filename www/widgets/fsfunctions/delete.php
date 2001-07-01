2dde45106791e5a9ceed670bdc3dff8df78a96e4
Auke van Slooten <auke@muze.nl>
2001-07-01 19:02:39 +0000

    should have been removed earlier.
    git-svn-id: https://svn.muze.nl/svn/ariadne-php5/trunk@592 7882d7ee-46e0-0310-a255-a28a61b628e4

--- www/widgets/fsfunctions/delete.php
+++
@@ -1,8 +0,0 @@
-<html>
-<head>
-</head>
-<frameset rows="1,*" BORDER="0" FRAMEBORDER="no" FRAMESPACING="0">
-	<frame name="action" SCROLLING="no" FRAMEBORDER="no" MARGINHEIGHT="0" MARGINWIDTH="0" src="<?php echo $root.$path."delete.js.phtml"; ?>"> 
-	<frame name="dialog" SCROLLING="auto" FRAMEBORDER="no" MARGINHEIGHT="0" MARGINWIDTH="0" src="dialog.php?message=<?php echo urlencode($message); ?>">
-</frameset>
-</html>
