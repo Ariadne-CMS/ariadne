a951bd4be1cf7b7d7d110beaef431edbbf33203b
Gijsbert te Riet <gijs@muze.nl>
2001-09-05 10:30:22 +0000

    New KDE-style interface. Contributed by Gabriel Wicke.
      - The 'new' interface code will now also run under mozilla and netscape
      - The icons have been taken from kde
    
    
    git-svn-id: https://svn.muze.nl/svn/ariadne-php5/trunk@732 7882d7ee-46e0-0310-a255-a28a61b628e4

--- www/widgets/tree/control.php
+++ www/widgets/tree/control.php
@@ -8,18 +8,18 @@ INPUT {
 </style>
 </head>
 <body>
-<table border="0" height="100%" cellpadding="0" cellspacing="0" width="90%" align="center">
+<table border="0" height="100%" cellspacing="0" width="100%" align="center">
 <tr>
   <form action="<?php echo $arUserPath; ?>logoff.phtml" target="_top">
-    <td align="left" valign="top">
-      <input type="submit" value="Log Off">
+    <td align="right" valign="top">
+      <input type="submit" value="Log Off">&nbsp;
     </td>
   </form><form action="<?php echo $arUserPath; ?>tree.init.phtml" target="treeload">
-    <td align="right" valign="top">
-      <input type="submit" value="Refresh">
+    <td align="left" valign="top">
+      &nbsp;<input type="submit" value="Refresh">
     </td>
   </form>
 </tr>
-</table>
+</table> 
 </body>
 </html>
