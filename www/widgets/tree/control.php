0f90e5d8bdfa0bd839c55cf4298c42d772989632
Auke van Slooten <auke@muze.nl>
2002-01-24 13:24:32 +0000

    fixed layout for mozilla
    git-svn-id: https://svn.muze.nl/svn/ariadne-php5/trunk@1154 7882d7ee-46e0-0310-a255-a28a61b628e4

--- www/widgets/tree/control.php
+++ www/widgets/tree/control.php
@@ -7,19 +7,26 @@ INPUT {
 }
 </style>
 </head>
-<body class="border">
-<table border="0" height="100%" cellspacing="0" width="100%" align="center">
+<body><div class="border-top"><div id="spanner" style="border: 0px; margin: 0px; padding: 0px; height: 28px;">
+<table border="0" cellspacing="0" width="100%" align="center">
 <tr>
   <form action="<?php echo $arUserPath; ?>logoff.phtml" target="_top">
     <td align="right" valign="top">
-      <input type="submit" value="Log Off">&nbsp;
+      <input type="submit" value="Log Off" style="width: 75px;">&nbsp;
     </td>
   </form><form action="<?php echo $arUserPath; ?>tree.init.phtml" target="treeload">
     <td align="left" valign="top">
-      &nbsp;<input type="submit" value="Refresh">
+      &nbsp;<input type="submit" value="Refresh" style="width: 75px;">
     </td>
   </form>
 </tr>
 </table> 
+</div></div>
+<script>
+  if (document.all) {
+    document.body.style.border='1px inset';
+    spanner.style.height='26px';
+  }
+</script>
 </body>
 </html>
