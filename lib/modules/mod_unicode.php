6cb250757cd90f6e1f0d66aa675f0bbee2885113
Auke van Slooten <auke@muze.nl>
2002-01-27 16:13:13 +0000

    unicode conversion routines. Converts to ascii (0 - 127) or 8859-1 (0 - 255), optionally convert other characters to unicode entities (&#xxxx;) (default) or '?' otherwise. readable entities for more common characters will be added in the future.
    git-svn-id: https://svn.muze.nl/svn/ariadne-php5/trunk@1165 7882d7ee-46e0-0310-a255-a28a61b628e4

---
+++ lib/modules/mod_unicode.php
@@ -0,0 +1,70 @@
+<?php
+	/* code by lars(at)ioflux.net, reproduced from www.php.net */
+	/* todo: add a list of common unicode characters (&euro, etc.) */
+
+	class unicode {
+
+		function utf8convert($string, $maxchar=0x7F, $entities=true) {
+			
+			$returns = "";
+			$UTF8len = array(	1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+								1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 
+								0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 
+								2, 2, 3, 3, 3, 3, 4, 4, 5, 6);
+			$pos = 0;
+			$total = strlen($string);
+			
+			do { 
+				$c = ord($string[$pos]);
+				$len = $UTF8len[($c >> 2) & 0x3F];
+				switch ($len) 
+				{
+					case 6: 
+						$u = $c & 0x01; 
+						break;
+					case 5: 
+						$u = $c & 0x03; 
+						break;
+					case 4: 
+						$u = $c & 0x07; 
+						break;
+					case 3: 
+						$u = $c & 0x0F; 
+						break;
+					case 2: 
+						$u = $c & 0x1F; 
+						break;
+					case 1: 
+						$u = $c & 0x7F; 
+						break;
+					case 0:	/* unexpected start of a new character */
+						$u = $c & 0x3F; 
+						$len = 5; 
+						break;
+				}
+				while (--$len && (++$pos < $total && $c = ord($string[$pos]))) {
+					if (($c & 0xC0) == 0x80) {
+						$u = ($u << 6) | ($c & 0x3F);
+					} else { 
+						/* unexpected start of a new character */
+						$pos--;
+						break;
+					}
+				}
+				if ($u <= $maxchar) { 
+					$returns .= chr($u);
+				} else if ($entities) { 
+					$returns .= '&#'.$u.';';
+				} else {
+					$returns .= '?';
+				}
+			} while (++$pos < $total);
+			return $returns;
+		} 
+
+		function utf8toiso8859($string, $entities=true) {
+			return unicode::utf8convert($string, 0xFF, $entities);
+		} 
+
+	}
+?>
