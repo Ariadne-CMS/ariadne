8d63cb65c72fcfd87c95133fc31330e9c7a10859
Wouter Commandeur <wouter@muze.nl>
2002-05-29 14:29:51 +0000

    NLS files for the AriadneXP 0.3 by Chris Versteeg. Polish translation by Piotr Romanczuk German and Spanish translations still need to be done
    git-svn-id: https://svn.muze.nl/svn/ariadne-php5/trunk@1453 7882d7ee-46e0-0310-a255-a28a61b628e4

---
+++ lib/nls/winxp.pl
@@ -0,0 +1,43 @@
+<?php
+	$ARnls["xp_new"]						=	"Dodaj nowy obiekt";
+	$ARnls["xp_rename"]						=	"Zmień nazwę ";
+	$ARnls["xp_copy"]						=	"Kopiuj ";
+	$ARnls["xp_link"]						=	"Link ";
+	$ARnls["xp_edit"]						=	"Edycja ";
+	$ARnls["xp_shortcut"]					=	"Edycja skrótu";
+	$ARnls["xp_delete"]						=	"Usuń ";
+	$ARnls["xp_cache"]						=	"Cache dla ";
+	$ARnls["xp_templates"]					=	"Szablony dla ";
+	$ARnls["xp_customfields"]				=	"Opcjonalne dane dla ";
+	$ARnls["xp_language"]					=	"Język dla ";
+	$ARnls["xp_grants"]						=	"Prawa dla ";
+	$ARnls["xp_types"]						=	"Typy dla ";
+	$ARnls["xp_priority"]					=	"Priorytet dla ";
+	$ARnls["xp_viewweb"]					=	"Strona";
+	$ARnls["xp_view"]						=	"Widok";
+	$ARnls["xp_explore"]					=	"Przejdź";
+	
+	//headers
+	$ARnls["xp_settings"]					=	"Ustawienia";
+	$ARnls["xp_info"]						=	"Info";
+	$ARnls["xp_detailsH"]					=	"Więcej...";
+	$ARnls["xp_browsebyletter"]				=	"Przeglądanie wg litery";
+	$ARnls["xp_browsebydate"]				=	"Przeglądanie wg daty";
+	
+	//toolbar
+	$ARnls["xp_up"]							= 	"Do góry";
+	$ARnls["xp_search"]						= 	"Szukaj";
+	$ARnls["xp_folders"]					= 	"Foldery";
+	$ARnls["xp_small"]						= 	"Lista";
+	$ARnls["xp_large"]						= 	"Ikony";
+	$ARnls["xp_details"]					= 	"Szczegóły";
+	$ARnls["xp_iconview"]					= 	"Widoki";
+	$ARnls["xp_import"]						= 	"Import";
+	$ARnls["xp_export"]						= 	"Eksport";
+	$ARnls["xp_preferences"]				= 	"Ustawienia";
+	$ARnls["xp_help"]						= 	"Pomoc";
+	$ARnls["xp_tutorials"]					= 	"Samouczki";
+	$ARnls["xp_about"]						= 	"O Ariadne...";
+
+
+?>
