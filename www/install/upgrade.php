ea8bef503647f548b46013f5c7d32b96b977cab1
Wouter Commandeur <wouter@muze.nl>
2002-05-30 17:54:01 +0000

    upgrade to 2.1 (still needs upgrade.ax)
    git-svn-id: https://svn.muze.nl/svn/ariadne-php5/trunk@1471 7882d7ee-46e0-0310-a255-a28a61b628e4

---
+++ www/install/upgrade.php
@@ -0,0 +1,60 @@
+#!/usr/local/bin/php -q
+<pre>
+<?php
+	require("../ariadne.inc");
+	require($ariadne."/configs/ariadne.phtml");
+	require($ariadne."/configs/store.phtml");
+	require($ariadne."/configs/axstore.phtml");
+	include_once($store_config['code']."includes/loader.web.php");
+	include_once($store_config['code']."stores/".$ax_config["dbms"]."store.phtml");
+	include_once($store_config['code']."stores/".$store_config["dbms"]."store_install.phtml");
+	$ERRMODE="text";
+
+
+	echo "== upgrading ariadne\n\n";
+
+	$inst_store = $store_config["dbms"]."store_install";
+	$store=new $inst_store(".",$store_config);
+	
+
+		// install pprofile type
+		
+		$store->add_type("pprofile","pobject");
+		$store->add_type("pprofile","pdir");
+		$store->add_type("pprofile","ppage");
+		$store->add_type("pprofile","pprofile");
+
+		echo "== importing upgrade.ax file\n\n";
+
+		$ARCurrent->options["verbose"]=true;
+		// become admin
+		$AR->user=new object;
+		$AR->user->data=new object;
+		$AR->user->data->login=$ARLogin="admin";
+
+		$ax_config["writeable"]=false;
+		$ax_config["database"]="upgrade.ax";
+		echo "ax file (".$ax_config["database"].")\n";
+		set_time_limit(0);
+		$inst_store = $ax_config["dbms"]."store";
+		$axstore=new $inst_store("", $ax_config);
+		if (!$axstore->error) {
+			$ARCurrent->importStore=&$store;
+			$args="srcpath=/&destpath=/";
+			$axstore->call("system.export.phtml", $args,
+				$axstore->get("/"));
+			$error=$axstore->error;
+			$axstore->close();
+		} else {
+			$error=$axstore->error;
+		}
+
+		if ($error) {
+			error($error);
+		}
+
+	$store->close(); 
+
+	
+?>
+</pre>
