38009cbd43b7eedf80b64ca8df2649d8996f1218
Gijsbert te Riet <gijs@muze.nl>
2002-10-22 13:15:17 +0000

    generates charset lookup tables
    git-svn-id: https://svn.muze.nl/svn/ariadne-php5/trunk@1739 7882d7ee-46e0-0310-a255-a28a61b628e4

---
+++ bin/utils/iconv.php
@@ -0,0 +1,19 @@
+<?php
+	$charset = "CP1252";
+	$table_start = 0x80;
+	$table_end = 0xff;
+
+	echo "<?php\n";
+	echo "	\$table_start = $table_start;\n";
+	echo "	\$table_end = $table_end;\n";
+
+	for ($c = $table_start; $c <= $table_end; $c++) {
+		$tstr = iconv($charset, "UTF-8", chr($c));
+		echo "	\$table[$c] = \"";
+		for ($i = 0; $i < strlen($tstr); $i++) {			
+			echo  "\\x".dechex(ord($tstr[$i]));
+		}
+		echo "\";\n";
+	}
+	echo "?>";
+?>
