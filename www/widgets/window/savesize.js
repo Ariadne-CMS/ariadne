d2f98d398e2cc48c789d58b363ae3a82a8f77dd4
Wouter Commandeur <wouter@muze.nl>
2002-10-25 09:51:39 +0000

    Added top. to each window reference. The size is now properly saved.
    git-svn-id: https://svn.muze.nl/svn/ariadne-php5/trunk@1748 7882d7ee-46e0-0310-a255-a28a61b628e4

--- www/widgets/window/savesize.js
+++ www/widgets/window/savesize.js
@@ -2,20 +2,20 @@
   function arSaveSize() {
     dprops='';
     if (document.all) {
-      temp=window.document.body;
+      temp=top.window.document.body;
       dheight=temp.clientHeight-4;
       dwidth=temp.clientWidth-4;
       dprops='height='+dheight+',width='+dwidth;
     } else {
-      dheight=window.outerHeight;
-      dwidth=window.outerWidth;
-      dleft=window.screenX;
-      dtop=window.screenY;
+      dheight=top.window.outerHeight;
+      dwidth=top.window.outerWidth;
+      dleft=top.window.screenX;
+      dtop=top.window.screenY;
       dprops='height='+dheight+',width='+dwidth+',top='+dtop+',left='+dleft;
     }
     if (top.window.opener && !top.window.opener.closed) {
       if (top.window.opener.top.Set) {
-        top.window.opener.top.Set(window.name, dprops);
+        top.window.opener.top.Set(top.window.name, dprops);
       /*
         FIXME: this somehow breaks in IE5.0, maybe others too 
 		} else if (top.window.opener.top.window.opener && 
