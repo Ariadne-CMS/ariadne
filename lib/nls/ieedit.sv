6eecf5e1ee753b28defb93b6f29825694d919cd7
Wouter Commandeur <wouter@muze.nl>
2003-03-26 10:38:50 +0000

    Added Swedish Translation by Niklas Koskela <niklas.koskela@htc-sweden.com>
    git-svn-id: https://svn.muze.nl/svn/ariadne-php5/trunk@2162 7882d7ee-46e0-0310-a255-a28a61b628e4

---
+++ lib/nls/ieedit.sv
@@ -0,0 +1,106 @@
+<?php
+
+	/*****************************************************************************
+
+		The Swedish nls files are contributed by: Niklas Koskela <ntkk@htc.se>
+                                                      Karl Thysell <karl@htc.se>
+
+	*****************************************************************************/
+
+	// Attention: The WYSIWYG editor and its context menus require UTF-8!
+	// (The external dialogs do not)
+
+	// lib/widgets/htmledit/ie/edit.php
+
+	$ARnls["e_cut"]				=	"Klipp ut";
+	$ARnls["e_copy"]			=	"Kopiera";
+	$ARnls["e_paste"]			=	"Klistra in";
+	$ARnls["e_delete"]			=	"Radera";
+
+	$ARnls["e_insertrow"]			=	"Infoga Rader";
+	$ARnls["e_deleterows"]			=	"Radera Rader";
+	$ARnls["e_insertcol"]			=	"Infoga Kolumner";
+	$ARnls["e_deletecols"]			=	"Raders Kolumner";
+	$ARnls["e_insertcell"]			=	"Infoga Celler";
+	$ARnls["e_deletecells"]			=	"Raders Celler";
+	$ARnls["e_mergecells"]			=	"Sammanfoga Celler";
+	$ARnls["e_splitcell"]			=	"Dela Celler";
+
+	$ARnls["e_z_order"]			=	"Z Order";
+	$ARnls["e_send_to_back"]		=	"Sänd Längst Bak";
+	$ARnls["e_bring_to_front"]		=	"Sänd Längst Fram";
+	$ARnls["e_send_backward"]		=	"Sänd Bakåt";
+	$ARnls["e_bring_forward"]		=	"Sänd Framåt";
+	$ARnls["e_send_below_text"]		=	"Sänd Under Text";
+	$ARnls["e_bring_above_text"]		=	"Sänd Över Text";
+	$ARnls["e_visible_borders"]		=	"Synliga Kanter";
+	$ARnls["e_show_details"]		=	"Visa Detaljer";
+	$ARnls["e_make_absolute"]		=	"Gör Absolut";
+	$ARnls["e_lock"]			=	"Lås";
+	$ARnls["e_snap_to_grid"]		=	"Fäst Mot Mönster";
+
+	$ARnls["e_file"]			=	"Fil";
+	$ARnls["e_save_file"]			=	"Spara Fil";
+
+	$ARnls["e_edit"]			=	"Ändra";
+	$ARnls["e_undo"]			=	"Ångra";
+	$ARnls["e_redo"]			=	"Upprepa";
+	$ARnls["e_select_all"]			=	"Markera Allt";
+	$ARnls["e_find"]			=	"Sök...";
+
+	$ARnls["e_view"]			=	"Vy";
+	$ARnls["e_standard"]			=	"Standard";
+	$ARnls["e_toolbars"]			=	"Verktygsfält";
+	$ARnls["e_formatting"]			=	"Formatering";
+	$ARnls["e_absolute_positioning"]	=	"Absolut Positionering";
+	$ARnls["e_table"]			=	"Tabell";
+
+	$ARnls["e_bold"]			=	"Fet";
+	$ARnls["e_italic"]			=	"Kursiv";
+	$ARnls["e_underline"]			=	"Understruken";
+
+	$ARnls["e_set_foreground_color"]	=	"Sätt Förgrunds Färg...";
+	$ARnls["e_set_background_color"]	=	"Sätt Bakgrunds Färg...";
+	$ARnls["e_foreground_color"]		=	"Förgrunds Färg";
+	$ARnls["e_background_color"]		=	"Bakgrunds Färg";
+
+	$ARnls["e_align_left"]			=	"Justera Vänster";
+	$ARnls["e_align_center"]		=	"Centrera";
+	$ARnls["e_align_right"]			=	"Justera Höger";
+
+	$ARnls["e_numbered_list"]		=	"Numrerad Lista";
+	$ARnls["e_bulleted_list"]		=	"Punkt Lista";
+
+	$ARnls["e_decrease_indent"]		=	"Minska Indrag";
+	$ARnls["e_increase_indent"]		=	"Öka Indrag";
+
+	$ARnls["e_link"]			=	"Länk";
+	$ARnls["e_insert_image"]		=	"Infoga Bild";
+	$ARnls["e_insert_table"]		=	"Infoga Tabell";
+
+	// lib/templates/pobject/edit.object.html.selectcolor.phtml
+
+	$ARnls["e_select_color"]		=	"Välj Färg";
+	$ARnls["e_color"]			=	"Färg";
+
+	// lib/templates/pobject/edit.object.html.image.phtml
+
+	$ARnls["e_picture"]			=	"Bild";
+	$ARnls["e_picture_source"]		=	"Bild&nbsp;Källa";
+	$ARnls["e_alternate_text"]		=	"Alternativ&nbsp;Text";
+	$ARnls["e_layout"]			=	"Layout";
+	$ARnls["e_alignment"]			=	"Placering";
+	$ARnls["e_border_thickness"]		=	"Kant&nbsp;Tjocklek";
+	$ARnls["e_spacing"]			=	"Mellanrum";
+	$ARnls["e_horizontal"]			=	"Horisontell";
+	$ARnls["e_vertical"]			=	"Vertikal";
+
+	// lib/templates/pobject/edit.object.html.link*.phtml
+
+	$ARnls["e_hyperlink"]			=	"Hyperlänk";
+	$ARnls["e_hyperlink_information"]	=	"Hyperlänk&nbsp;Information";
+	$ARnls["e_hyperlink_type"]		=	"Typ";
+	$ARnls["e_hyperlink_path"]		=	"Sökväg";
+	$ARnls["e_hyperlink_affiliate"]		=	"Anslut";
+
+?>
---
+++ lib/nls/menu.sv
@@ -0,0 +1,46 @@
+<?PHP
+
+	/*****************************************************************************
+
+		The Swedish nls files are contributed by: Niklas Koskela <ntkk@htc.se>
+                                                      Karl Thysell <karl@htc.se>
+
+	*****************************************************************************/
+
+	$ARnls["m_about"]						=	"Om&nbsp;Ariadne";
+	$ARnls["m_cache"]						=	"Cache";
+	$ARnls["m_config"]						=	"Konfiguration";
+	$ARnls["m_copy"]						=	"Kopiera";
+	$ARnls["m_custom"]						=	"Anpassade&nbsp;F&auml;lt";
+	$ARnls["m_data"]						=	"Data";
+	$ARnls["m_delete"]						=	"Ta bort";
+	$ARnls["m_details"]						=	"Detaljer";
+	$ARnls["m_edit"]						=	"Redigera";
+	$ARnls["m_explorerbar"]					=	"Explorer&nbsp;F&auml;lt";
+	$ARnls["m_export"]						=	"Exportera";
+	$ARnls["m_find"]						=	"S&ouml;k";
+	$ARnls["m_fonts"]						=	"Teckensnitt";
+	$ARnls["m_grants"]						=	"R&auml;ttigheter";
+	$ARnls["m_help"]						=	"Hj&auml;lp";
+	$ARnls["m_import"]						=	"Importera";
+	$ARnls["m_index"]						=	"Index";
+	$ARnls["m_language"]					=	"Spr&aring;k";
+	$ARnls["m_large"]						=	"Stora&nbsp;Ikoner";
+	$ARnls["m_layout"]						=	"Layout";
+	$ARnls["m_link"]						=	"L&auml;nk";
+	$ARnls["m_logoff"]						=	"Logga&nbsp;av";
+	$ARnls["m_new"]							=	"Nytt";
+	$ARnls["m_object"]						=	"Objekt";
+	$ARnls["m_preferences"]					=	"Inst&auml;llningar";
+	$ARnls["m_priority"]					=	"Prioritet";
+	$ARnls["m_rename"]						=	"Byt namn";
+	$ARnls["m_search"]						=	"S&ouml;k";
+	$ARnls["m_shortcut"]					=	"Genv&auml;g";
+	$ARnls["m_small"]						=	"Sm&aring;&nbsp;Ikoner";
+	$ARnls["m_templates"]					=	"Mallar";
+	$ARnls["m_tutorials"]					=	"V&auml;gledning";
+	$ARnls["m_types"]						=	"Typer";
+	$ARnls["m_view"]						=	"Visa";
+	$ARnls["m_web"]							=	"Webbsida";
+	$ARnls["m_page"]						=	"Sida";
+?>
