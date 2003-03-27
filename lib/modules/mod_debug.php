5a13342cdc2c9dae40165fb73a50c66060a64cd3
Wouter Commandeur <wouter@muze.nl>
2003-03-27 18:57:50 +0000

    Forgot head some time ago :)
    git-svn-id: https://svn.muze.nl/svn/ariadne-php5/trunk@2166 7882d7ee-46e0-0310-a255-a28a61b628e4

--- lib/modules/mod_debug.php
+++ lib/modules/mod_debug.php
@@ -1,4 +1,36 @@
 <?php
+    /******************************************************************
+     mod_debug.php                                         Muze Ariadne
+     ------------------------------------------------------------------
+     Author: Wouter Commandeur (Muze) (info@muze.nl)
+     Date: 18 march 2003
+
+     Copyright 2003 Muze
+
+     This file is part of Ariadne.
+
+     Ariadne is free software; you can redistribute it and/or modify
+     it under the terms of the GNU General Public License as published 
+     by the Free Software Foundation; either version 2 of the License, 
+     or (at your option) any later version.
+ 
+     Ariadne is distributed in the hope that it will be useful,
+     but WITHOUT ANY WARRANTY; without even the implied warranty of
+     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+     GNU General Public License for more details.
+
+     You should have received a copy of the GNU General Public License
+     along with Ariadne; if not, write to the Free Software 
+     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  
+     02111-1307  USA
+
+    -------------------------------------------------------------------
+
+     Description:
+
+	Debug Module
+
+    ******************************************************************/
 
 	$DB["all"]=5;
 	$DB["store"]=4;
