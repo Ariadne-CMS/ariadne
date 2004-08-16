3049fc4ac1f881bf2f372680db701cd0b5e2b24a
Auke van Slooten <auke@muze.nl>
2004-08-16 14:55:18 +0000

    added mod_multipart which is a more generic and powerfull implementation of the multipart page code. added new version of mod_edit, which is working, but its not yet finished, it needs error checking at least.
    git-svn-id: https://svn.muze.nl/svn/ariadne-php5/trunk@2984 7882d7ee-46e0-0310-a255-a28a61b628e4

--- lib/modules/mod_edit.php
+++ lib/modules/mod_edit.php
@@ -1,92 +1,83 @@
 <?php
+	/*
+		This class is meant to make it easy to create inline editable pages. The class should not be instantiated, instead
+		you just call (in pinp) edit::showSpan($data->name, "$nls[name]");
+	
+	*/
 
 	class pinp_edit {
 
-		function _setEditmode($mode=false) {
+		function _setEditMode($mode=false, $template='user.edit.html', $target='_top') {
 			global $mod_edit_data;
 			$mod_edit_data['editmode']=$mode;
+			$mod_edit_data['edittemplate']=$template;
+			$mod_edit_data['edittarget']=$target;
 		}
 
-		function _getEditmode() {
+		function _getEditMode() {
 			global $mod_edit_data;
 			return $mod_edit_data['editmode'];
 		}
 
-		function _showSpan($var, $name) {
+		function _getEditTemplate() {
 			global $mod_edit_data;
-			if ($mod_edit_data['editmode']) {
-				$id=++$mod_edit_data['id'];
-				echo "<span onMouseDown='startedit(this, true)' class='editable' id='editable_$id' ar:path='".$this->path."' ar:id='".$this->id."'>";
+			return $mod_edit_data['edittemplate'];
+		}
+
+		function _getEditTarget() {
+			global $mod_edit_data;
+			return $mod_edit_data['edittarget'];
+		}
+
+		function registerDataField($name) {
+			/* private method */
+			global $mod_edit_data;
+			$id=++$mod_edit_data['id'];
+			echo "<script> parent.registerDataField('editable_$id','".AddCSlashes($name, ARESCAPE)."','".$this->path."',".$this->id."); </script>\n";
+			return $id;
+		}
+
+		function _showInputText($var, $name, $title='') {
+			if (pinp_edit::_getEditMode() && $this->CheckSilent('edit')) {
+				$id=pinp_edit::registerDataField($name);
+				echo "<input type='text' class='editable' id='editable_$id' ar:path='".$this->path."' ar:id='".$this->id."' title='$title' value='";
+				echo $var;
+				echo "'>";
+			} else {
+				echo $var;
+			}
+		}
+
+		function _showSpan($var, $name, $title='') {
+			if (pinp_edit::_getEditMode() && $this->CheckSilent('edit')) {
+				$id=pinp_edit::registerDataField($name);
+				echo "<span class='editable' id='editable_$id' ar:path='".$this->path."' ar:id='".$this->id."' title='$title'>";
 				echo $var;
 				echo "</span>";
-				$mod_edit_data['formdata'][$this->path][$name][]=$id;
 			} else {
 				echo $var;
 			}
 		}
 
-		function _showDiv($var, $name) {
-			global $mod_edit_data;
-			if ($mod_edit_data['editmode']) {
-				$id=++$mod_edit_data['id'];
-				echo "<div class='editable' id='editable_$id' ar:path='".$this->path."' ar:id='".$this->id."'>";
+		function _showDiv($var, $name, $title='') {
+			if (pinp_edit::_getEditMode() && $this->CheckSilent('edit')) {
+				$id=pinp_edit::registerDataField($name);
+				echo "<div class='editable' id='editable_$id' ar:path='".$this->path."' ar:id='".$this->id."' title='$title'>";
 				echo $var;
 				echo "</div>";
-				$mod_edit_data['formdata'][$this->path][$name][]=$id;
 			} else {
 				echo $var;
 			}
 		}
 
-		function _openForm() {
-			global $mod_edit_data, $formcounter;
-			if ($mod_edit_data['editmode']) {
-				$id=++$mod_edit_data['formcounter'];
-				echo "<form id='edit_form_".$id."' ar:path='".$this->path."' ar:id='".$this->id."'>";
-				$mod_edit_data['forms'][$this->path][]=$id;
+		function _showLink($path, $extra='') {
+			if (pinp_edit::_getEditMode()) {
+				echo "<a href='".$this->make_url($path).pinp_edit::_getEditTemplate()."' $extra target='".pinp_edit::_getEditTarget()."'>";
+			} else {
+				echo "<a href='".$this->make_url($path)."' $extra>";				
 			}
 		}
-
-		function _closeForm() {
-			global $mod_edit_data;
-			if ($mod_edit_data['editmode']) {
-				echo "</form>";
-			}
-		}
-
-		function _registerData() {
-			global $mod_edit_data;
-			if ($mod_edit_data['editmode']) {
-				echo "<script>\n";
-				echo "  window.editableElements=new Array();\n";
-				echo "  window.idList=new Array();\n";
-				reset($mod_edit_data['formdata']);
-				while (list($path, $elementlist)=each($mod_edit_data['formdata'])) {
-					echo "  window.editableElements['$path']=new Array();\n";
-					while (list($name, $idlist)=each($elementlist)) {
-						echo "  window.editableElements['$path']['$name']=new Array();\n";
-						while (list($key, $id)=each($idlist)) {
-							echo "  window.editableElements['$path']['$name'][$key]='editable_$id';\n";
-						}
-						reset($idlist);
-						while (list($key, $id)=each($idlist)) {
-							echo "  window.idList['editable_$id']=editableElements['$path']['$name'];\n";
-						}
-					}
-				}
-				echo "  window.editableForms=new Array();\n";
-				if (is_array($mod_edit_data['forms'])) {
-					reset($mod_edit_data['forms']);
-					while (list($path, $idlist)=each($mod_edit_data['forms'])) {
-						echo "  window.editableForms['$path']=new Array();\n";
-						while (list($key, $id)=each($idlist)) {
-							echo "  window.editableForms['$path'][$key]='edit_form_$id';\n";
-						}
-					}
-				}
-				echo "</script>";
-			}
-		}
+		
 	}
 
 ?>
---
+++ lib/modules/mod_multipart.php
@@ -0,0 +1,80 @@
+<?php
+
+class pinp_multipart {
+    
+	function _CutPage($tag, $withtags=true, $withtitles=true, $page=false) {
+		return multipart::CutPage($tag, $withtags, $withtitles, $page);
+	}
+
+	function _ShowSection($section, $template="", $args="", $recurse=false, $level=0) {
+		return multipart::ShowSection($section, $template, $args, $recurse, $level);
+	}
+
+	function _ShowSections($sections, $template="", $args="", $recurse=false, $level=0) {
+		return multipart::ShowSections($sections, $template, $args, $recurse, $level);
+	}
+
+}
+	
+class multipart {
+
+	function CutPage($tag='h1', $withtags=true, $withtitles=true, $page=false) {
+		$sections=Array();
+		if ($withtitles) {
+			$regexp='/(<'.$tag.'[^>]*>(.*)<\/'.$tag.'[^>]*>)/Usi';
+		} else {
+			$regexp='/(<('.$tag.')[^>]*>)/Usi';
+		}
+		if (!$page) {
+			$page=$this->GetPage();
+		}
+		if ($matches=preg_split($regexp, " ".$page, -1, PREG_SPLIT_DELIM_CAPTURE)) {
+			$leader=trim($matches[0]);
+			if ($leader) {
+				$sections[0]['content']=$leader;
+			}
+			$start=1;
+			$max=count($matches)-1;
+			$count=$start;
+			for ($i=$start; $i<$max; $i+=3) {
+				$split=$matches[$i];
+				if ($withtitles) {
+					$sections[$count]['title']=$matches[$i+1];
+				}
+				if ($withtags) {
+					$sections[$count]['content']=$split.$matches[$i+2];					
+				} else {
+					$sections[$count]['content']=$matches[$i+2];
+				}
+				$count++;
+			}
+		} 
+		return $sections;
+	}
+
+	function ShowSection($section, $template="", $args="", $recurse=false, $level=0) {
+		if ($recurse && is_array($section['content'])) {
+			multipart::ShowSections($section['content'], $template, $args, $recurse, $level++);
+		} else {
+			if (!$template) {
+				echo "<div class='section'><div class='title'>".$section['title']."</div>";
+				echo $section['content']."</div>";
+			} else {
+				$this->call($template, array_merge($args, Array('level' => $level, 'section_title' => $section['title'], 'section_content' => $section['content'])));
+			}
+		}
+	} 
+
+	function ShowSections($sections, $template="", $args="", $recurse=false, $level=0) {
+		if (is_array($sections)) {
+			$max=count($sections);
+			for ($i=0; $i<$max; $i++) {
+				multipart::ShowSection($sections[$i], $template, $args, $recurse, $level);
+				$this->_resetloopcheck();
+			}
+		}			
+	}
+
+}
+
+?>
