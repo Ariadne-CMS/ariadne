0b01dc57e1484894ad228e1ca2ba1ca06456c5bc
Gijsbert te Riet <gijs@muze.nl>
2004-09-03 14:36:12 +0000

    Updated oracle compiler.
    git-svn-id: https://svn.muze.nl/svn/ariadne-php5/trunk@3038 7882d7ee-46e0-0310-a255-a28a61b628e4

--- lib/stores/modules/oracle_compiler.php
+++ lib/stores/modules/oracle_compiler.php
@@ -1,4 +1,27 @@
 <?php
+/******************************************************************************
+  Oracle compiler 1.0b												Ariadne 2.4
+
+  Copyright (C) 1998-2004  Muze 
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+  --------------------------------------------------------------------------
+
+*******************************************************************************/
+
   include_once($this->code."stores/modules/sql_compiler.php");
 
   class oracle_compiler extends sql_compiler {
@@ -200,7 +223,7 @@
 			case 'or':
 				$left=$this->compile_tree($node["left"]);
 				$right=$this->compile_tree($node["right"]);
-				$result=" $left or $right ";
+				$result=" ($left or $right) ";
 			break;
 			case 'cmp':
 				$not="";
