5b432ee875f23b2c9bdeb482e30569bb1c172995
Gijsbert te Riet <gijs@muze.nl>
2004-10-12 19:45:47 +0000

    Added a bin/skeleton script.
    git-svn-id: https://svn.muze.nl/svn/ariadne-php5/trunk@3085 7882d7ee-46e0-0310-a255-a28a61b628e4

---
+++ bin/skeleton.php
@@ -0,0 +1,31 @@
+#!/usr/bin/php -q
+<?php
+	$ariadne = "../lib";
+	if (!@include_once($ariadne."/configs/ariadne.phtml")) {
+		chdir(substr($_SERVER['PHP_SELF'], 0, strrpos($_SERVER['PHP_SELF'], '/')));
+		if(!include_once($ariadne."/configs/ariadne.phtml")){
+			echo "could not open ariadne.phtml";
+			exit(1);
+		}
+	}
+	require($ariadne."/configs/store.phtml");
+	require($ariadne."/includes/loader.cmd.php");
+	require($ariadne."/stores/".$store_config["dbms"]."store.phtml");
+	include($ariadne."/nls/".$AR->nls->default);
+
+	/* instantiate the store */
+	$inst_store = $store_config["dbms"]."store";
+	$store = new $inst_store($root,$store_config);
+
+
+	/* now load a user (admin in this case)*/
+	$login = "admin";
+	$query = "object.implements = 'puser' and login.value='$login'";
+	$AR->user = current($store->call('system.get.phtml', '', $store->find('/system/users/', $query)));
+
+	/* do your stuff here */
+	//$store->call('show.name.phtml', '', $store->ls('/demo/fastbizz/'));
+
+	$store->close();
+
+?>
