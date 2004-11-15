08439d59896ff17d08ef8292d37ce40c564f9ea5
Wouter Commandeur <wouter@muze.nl>
2004-11-15 16:46:04 +0000

    Brand new soap module for PINP usage.
    git-svn-id: https://svn.muze.nl/svn/ariadne-php5/trunk@3139 7882d7ee-46e0-0310-a255-a28a61b628e4

---
+++ lib/modules/mod_soap.php
@@ -0,0 +1,51 @@
+<?php
+
+	require_once("SOAP/Client.php");
+
+	class pinp_SOAP {
+
+		function _Client($endpoint, $wsdl = false , $portName = false, $proxy_params=array() ) {
+			return new pinp_SOAP_Client( $endpoint, $wsdl, $portName, $proxy_params);
+		}
+		
+		function _Value($name, $type, $value, $namespaces=false) {
+			if( $namespaces===false ) {
+				return new SOAP_Value( $name, $type, $value);
+			} else {
+				return new SOAP_Value( $name, $type, $value, $namespaces );
+			}
+		}
+
+	}
+	
+	class pinp_SOAP_Client extends SOAP_Client {
+	
+		function pinp_SOAP_Client($endpoint, $wsdl = false, $portName = false, $proxy_params=array()) {
+			parent::SOAP_Client( $endpoint, $wsdl, $portName, $proxy_params );
+        }
+                     		
+		function _call( $function, $arguments=false, $namespace=false, $soapAction=false ) {
+			return $this->call( $function, $arguments, $namespace, $soapAction );
+		}
+
+		function _isError($value) {
+			return $this->isError($value);
+		}
+
+		function _isWarning($value) {
+			return $this->isWarning($value);
+		}
+
+		function _errorMessage($error) {
+			if (is_object($error) && get_class($error) == "soap_fault") {
+				$result = $error->message;
+			} else {
+				$result = false;
+			}
+			return $result;
+		}
+
+	}
+
+	
+?>
