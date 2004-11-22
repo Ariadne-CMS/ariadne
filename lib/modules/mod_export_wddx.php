963fc360076a8507c11ff853628f7ca493f3bf44
Robbert Müller <muller@muze.nl>
2004-11-22 13:40:31 +0000

    - updated the wddx export module, it uses fwrite instead of echo, which made 	  it possible to have debugging and/or verbose enabled during the export. 	  this also makes the usage of ob_ functions obsolete. this also reduces the 	  usage of memory
    git-svn-id: https://svn.muze.nl/svn/ariadne-php5/trunk@3178 7882d7ee-46e0-0310-a255-a28a61b628e4

--- lib/modules/mod_export_wddx.php
+++ lib/modules/mod_export_wddx.php
@@ -2,7 +2,12 @@
 
 class export_wddx {
 
-
+	function print_verbose($message){
+		global $ARCurrent;
+		if($ARCurrent->wddxoptions["verbose"]){
+			print $message;
+		}
+	}
 
 	function strtoxmldata($string) {
 		return str_replace("]]>","]]&gt;",str_replace("]]&","]]&amp;",$string));
@@ -12,134 +17,142 @@ class export_wddx {
 		return str_replace("]]&amp;","]]&",str_replace("]]&gt;","]]>",$string));
 	}
 
-	function export(&$object) {
-		echo "<var name=\"object".$object->id."\">\n";
-		echo "<struct type=\"object\" class=\"".$object->type."\">\n";
-		echo "<var name=\"id\">\n";
-		echo "<number>".$object->id."</number>\n";
-		echo "</var>\n";
-		echo "<var name=\"path\">\n";
-		echo "<string>".$object->path."</string>\n";
-		echo "</var>\n";
-		echo "<var name=\"type\">\n";
-		echo "<string>".$object->type."</string>\n";
-		echo "</var>\n";
-		echo "<var name=\"vtype\">\n";
-		echo "<string>".$object->vtype."</string>\n";
-		echo "</var>\n";
-		echo "<var name=\"priority\">\n";
-		echo "<number>".$object->priority."</number>\n";
-		echo "</var>\n";
-		echo "<var name=\"size\">\n";
-		echo "<number>".$object->size."</number>\n";
-		echo "</var>\n";
+	function export($fp,&$object) {
+		export_wddx::print_verbose('Exporting: '.$object->path."\n");
+		fwrite($fp,"<var name=\"object".$object->id."\">\n");
+		fwrite($fp,"<struct type=\"object\" class=\"".$object->type."\">\n");
+		fwrite($fp,"<var name=\"id\">\n");
+		fwrite($fp,"<number>".$object->id."</number>\n");
+		fwrite($fp,"</var>\n");
+		fwrite($fp,"<var name=\"path\">\n");
+		fwrite($fp,"<string>".$object->path."</string>\n");
+		fwrite($fp,"</var>\n");
+		fwrite($fp,"<var name=\"type\">\n");
+		fwrite($fp,"<string>".$object->type."</string>\n");
+		fwrite($fp,"</var>\n");
+		fwrite($fp,"<var name=\"vtype\">\n");
+		fwrite($fp,"<string>".$object->vtype."</string>\n");
+		fwrite($fp,"</var>\n");
+		fwrite($fp,"<var name=\"priority\">\n");
+		fwrite($fp,"<number>".$object->priority."</number>\n");
+		fwrite($fp,"</var>\n");
+		fwrite($fp,"<var name=\"size\">\n");
+		fwrite($fp,"<number>".$object->size."</number>\n");
+		fwrite($fp,"</var>\n");
 
-		export_wddx::export_data("data", $object->data);
-		export_wddx::export_properties($object);
-		export_wddx::export_templates($object);
-		export_wddx::export_files($object);
+		export_wddx::export_data($fp,"data", $object->data);
+		export_wddx::export_properties($fp,$object);
+		export_wddx::export_templates($fp,$object);
+		export_wddx::export_files($fp,$object);
 
-		echo "</struct>\n";
-		echo "</var>\n";
+		fwrite($fp,"</struct>\n");
+		fwrite($fp,"</var>\n");
 	}
 
-	function export_data($name, $value) {
+	function export_data($fp,$name, $value) {
 		if (!is_null($value)) {
-			echo "<var name=\"$name\">\n";
+			fwrite($fp,"<var name=\"$name\">\n");
 			if (is_bool($value)) {
 					$value = ($value) ? 'true' : 'false';
-				echo "<boolean>$value</boolean>\n";
+				fwrite($fp,"<boolean>$value</boolean>\n");
 			} else if (is_int($value)||is_real($value)||is_float($value)) {
-				echo "<number>$value</number>\n";
+				fwrite($fp,"<number>$value</number>\n");
 			} else if (is_string($value) || ($value==="")) {
-				echo "<string><![CDATA[".export_wddx::strtoxmldata($value)."]]></string>\n";
+				fwrite($fp,"<string><![CDATA[".export_wddx::strtoxmldata($value)."]]></string>\n");
 			} else if (is_array($value) || is_object($value)) {
 				if (is_array($value)) {
-					echo "<struct type=\"hash\">\n";
+					fwrite($fp,"<struct type=\"hash\">\n");
 				} else {
-					echo "<struct type=\"object\" class=\"".get_class($value)."\">\n";
+					fwrite($fp,"<struct type=\"object\" class=\"".get_class($value)."\">\n");
 				}
 				while (list($key, $val)=each($value)) {
-					export_wddx::export_data($key, $val);
+					export_wddx::export_data($fp,$key, $val);
 				}
-				echo "</struct>\n";
+				fwrite($fp,"</struct>\n");
 			}
-			echo "</var>\n";
+			fwrite($fp,"</var>\n");
 		}
-		flush();
 	}
 
-	function export_properties(&$object) {
+	function export_properties($fp,&$object) {
 		$properties=$object->load_properties();
-		export_wddx::export_data("properties",$properties);
+		export_wddx::export_data($fp,"properties",$properties);
 	}
 
-	function export_templates(&$object) {
+	function export_templates($fp,&$object) {
 		if ($object->data->config->pinp) {
-			echo "<var name=\"templates\">\n";
-			echo "<struct type=\"hash\">\n";
+			fwrite($fp,"<var name=\"templates\">\n");
+			fwrite($fp,"<struct type=\"hash\">\n");
 			$templates=$object->store->get_filestore("templates");
+			export_wddx::print_verbose("   Templates:\n");
 			while (list($type, $functions)=each($object->data->config->pinp)) {
-				echo "<var name=\"$type\">\n";
-				echo "<struct type=\"hash\" >\n";
+				fwrite($fp,"<var name=\"$type\">\n");
+				fwrite($fp,"<struct type=\"hash\" >\n");
 				while (list($function, $languages)=each($functions)) {
-					echo "<var name=\"$function\">\n";
-					echo "<struct type=\"hash\" >\n";
+					fwrite($fp,"<var name=\"$function\">\n");
+					fwrite($fp,"<struct type=\"hash\" >\n");
 					while (list($language, $ids)=each($languages)) {
-						echo "<var name=\"$language\" >\n";
-						echo "<struct type=\"hash\" class=\"file\" >\n";
-						echo "<var name=\"template\">\n";
 						$file=$type.".".$function.".".$language.".pinp";
+						export_wddx::print_verbose('              ');
+						export_wddx::print_verbose("[".$file."]: ");
+						fwrite($fp,"<var name=\"$language\" >\n");
+						fwrite($fp,"<struct type=\"hash\" class=\"file\" >\n");
+						fwrite($fp,"<var name=\"template\">\n");
 						$content = $templates->read($object->id, $file);
-						echo "<string><![CDATA[";
-						echo base64_encode($content);
-						echo "]]></string></var>\n";
-						echo "<var name=\"mtime\">\n";
-						echo "<number>".$templates->mtime($object->id, $file)."</number>\n";
-						echo "</var>\n";
-						echo "<var name=\"ctime\">\n";
-						echo "<number>".$templates->ctime($object->id, $file)."</number>\n";
-						echo "</var>\n";
-						echo "</struct>\n";
-						echo "</var>";
+						fwrite($fp,"<string><![CDATA[");
+						fwrite($fp,base64_encode($content));
+						fwrite($fp,"]]></string></var>\n");
+						fwrite($fp,"<var name=\"mtime\">\n");
+						fwrite($fp,"<number>".$templates->mtime($object->id, $file)."</number>\n");
+						fwrite($fp,"</var>\n");
+						fwrite($fp,"<var name=\"ctime\">\n");
+						fwrite($fp,"<number>".$templates->ctime($object->id, $file)."</number>\n");
+						fwrite($fp,"</var>\n");
+						fwrite($fp,"</struct>\n");
+						fwrite($fp,"</var>");
+						export_wddx::print_verbose("stored\n");
 					}
-					echo "</struct></var>\n";
+					fwrite($fp,"</struct></var>\n");
 				}
-				echo "</struct></var>\n";
+				fwrite($fp,"</struct></var>\n");
 			}
-			echo "</struct></var>\n";
+			fwrite($fp,"</struct></var>\n");
 			$templates->close();
 		}
 	}
 
-	function export_files(&$object) {
+	function export_files($fp,&$object) {
 		$files = $object->store->get_filestore("files");
 		$filearray = $files->ls($object->id);
 		if( is_array($filearray) ) {
-			echo "<var name=\"files\">\n";
-			echo "<struct type=\"hash\">\n";
+			export_wddx::print_verbose("       Files:\n");
+			fwrite($fp,"<var name=\"files\">\n");
+			fwrite($fp,"<struct type=\"hash\">\n");
 			
 			while( list( $key, $file ) = each($filearray) ) {
-				echo "<var name=\"".$file."\">\n";
-				echo "<struct type=\"hash\" >\n";
-				echo "<var name=\"file\" >\n";
-				echo "<string><![CDATA[";
-				echo base64_encode($files->read($object->id, $file));
-				echo "]]></string>\n";
-				echo "</var>\n";
-				echo "<var name=\"mtime\">\n";
-				echo "<number>".$files->mtime($object->id, $file)."</number>\n";
-				echo "</var>\n";
-				echo "<var name=\"ctime\">\n";
-				echo "<number>".$files->ctime($object->id, $file)."</number>\n";
-				echo "</var>\n";
-				echo "</struct>\n";
-				echo "</var>\n";
+				export_wddx::print_verbose('              ');
+				export_wddx::print_verbose("[".$name."]: ");
+				fwrite($fp,"<var name=\"".$file."\">\n");
+				fwrite($fp,"<struct type=\"hash\" >\n");
+				fwrite($fp,"<var name=\"file\" >\n");
+				fwrite($fp,"<string><![CDATA[");
+				fwrite($fp,base64_encode($files->read($object->id, $file)));
+				fwrite($fp,"]]></string>\n");
+				fwrite($fp,"</var>\n");
+				fwrite($fp,"<var name=\"mtime\">\n");
+				fwrite($fp,"<number>".$files->mtime($object->id, $file)."</number>\n");
+				fwrite($fp,"</var>\n");
+				fwrite($fp,"<var name=\"ctime\">\n");
+				fwrite($fp,"<number>".$files->ctime($object->id, $file)."</number>\n");
+				fwrite($fp,"</var>\n");
+				fwrite($fp,"</struct>\n");
+				fwrite($fp,"</var>\n");
+				export_wddx::print_verbose("stored\n");
 			}
 			
 			
-			echo "</struct>\n";
-			echo "</var>\n";
+			fwrite($fp,"</struct>\n");
+			fwrite($fp,"</var>\n");
 		}
 		$files->close();
 	}
