edecbaa77b4caaa62e54281a6e5df178dd27bb16
Wouter Commandeur <wouter@muze.nl>
2004-11-25 11:04:55 +0000

    Added header.
    git-svn-id: https://svn.muze.nl/svn/ariadne-php5/trunk@3191 7882d7ee-46e0-0310-a255-a28a61b628e4

--- lib/modules/mod_soap.php
+++ lib/modules/mod_soap.php
@@ -1,4 +1,36 @@
 <?php
+    /******************************************************************
+     mod_soap.php                                          Muze Ariadne
+     ------------------------------------------------------------------
+     Author: Muze (info@muze.nl)
+     Date: 25 november 2004
+
+     Copyright 2004 Muze
+
+     This file is part of Ariadne.
+
+     Ariadne is free software; you can redistribute it and/or modify
+     it under the terms of the GNU General Public License as published 
+     by the Free Software Foundation; either version 2 of the License, 
+     or (at your option) any later version.
+ 
+     Ariadne is distributed in the hope that it will be useful,
+     but WITHOUT ANY WARRANTY; without even the implied warranty of
+     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+     GNU General Public License for more details.
+
+     You should have received a copy of the GNU General Public License
+     along with Ariadne; if not, write to the Free Software 
+     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  
+     02111-1307  USA
+
+    -------------------------------------------------------------------
+
+     Description:
+
+		PINP wrapper to the pear SOAP library.
+
+    ******************************************************************/
 
 	require_once("SOAP/Client.php");
 
