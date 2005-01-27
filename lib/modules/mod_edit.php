a55609d0510d6a0943fe9dc2361e68a5b0f12f0f
Auke van Slooten <auke@muze.nl>
2005-01-27 09:43:39 +0000

    added showSelect and fixed showInput
    git-svn-id: https://svn.muze.nl/svn/ariadne-php5/trunk@3287 7882d7ee-46e0-0310-a255-a28a61b628e4

--- lib/modules/mod_edit.php
+++ lib/modules/mod_edit.php
@@ -7,34 +7,29 @@
 
 	class edit {
 
-		function setEditMode($mode=false, $template='user.edit.html', $target='_top') 
-		{
+		function setEditMode($mode=false, $template='user.edit.html', $target='_top') {
 			global $mod_edit_data;
 			$mod_edit_data['editmode']=$mode;
 			$mod_edit_data['edittemplate']=$template;
 			$mod_edit_data['edittarget']=$target;
 		}
 
-		function getEditMode() 
-		{
+		function getEditMode() {
 			global $mod_edit_data;
 			return $mod_edit_data['editmode'];
 		}
 
-		function getEditTemplate() 
-		{
+		function getEditTemplate() {
 			global $mod_edit_data;
 			return $mod_edit_data['edittemplate'];
 		}
 
-		function getEditTarget() 
-		{
+		function getEditTarget() {
 			global $mod_edit_data;
 			return $mod_edit_data['edittarget'];
 		}
 
-		function registerDataField($name) 
-		{
+		function registerDataField($name) {
 			/* private method */
 			global $mod_edit_data;
 			$id=++$mod_edit_data['id'];
@@ -42,177 +37,156 @@
 			return $id;
 		}
 
-		function showInputText($var, $name, $title='') 
-		{
-			if (edit::getEditMode() && $this->CheckSilent('edit')) 
-			{
+		function showInputText($var, $name, $title='') {
+			if (edit::getEditMode() && $this->CheckSilent('edit')) {
 				$id=edit::registerDataField($name);
 				echo "<input type='text' class='editable' id='editable_$id' ar:path='".$this->path."' ar:id='".$this->id."' title='$title' value='";
 				echo $var;
 				echo "'>";
-			} 
-			else if (!edit::isEmpty($var)) 
-			{
+			} else if (!edit::isEmpty($var)) {
 				echo $var;
 			}
 		}
 
-		function showInput($var, $name, $title, $type='text', $extra='') 
-		{
-			if (edit::getEditMode() && $this->CheckSilent('edit')) 
-			{
+		function showInput($var, $name, $title, $type='text', $extra='') {
+			if (edit::getEditMode() && $this->CheckSilent('edit')) {
 				$id=edit::registerDataField($name);
 				echo "<input name='$name' type='$type' class='editable' id='editable_$id' ar:path='".$this->path."' ar:id='".$this->id."' title='$title' value='";
 				echo $var;
 				echo "' $extra>";			
-			} 
-			else if (!edit::isEmpty($var)) 
-			{
+			} else if (!edit::isEmpty($var)) {
 				echo $var;
 			}
 		}
 
-		function showSpan($var, $name, $title='') 
-		{
-			if (edit::getEditMode() && $this->CheckSilent('edit')) 
-			{
+		function showSelect($var, $name, $title, $list, $extra) {
+			if (edit::getEditMode() && $this->CheckSilent('edit')) {
+				$id=edit::registerDataField($name);
+				echo "<select class='editable' id='editable_$id' ar:path='".$this->path."' ar:id='".$this->id."' title='$title'>";
+				foreach ($list as $key => $value) {
+					echo "<option";
+					if ($value==$var) {
+						echo " selected";
+					}
+					echo ">$value</option>\n";
+				}
+				echo "</select>";
+			} else if (!edit::isEmpty($var)) {
+				echo $var;
+			}
+		}
+
+		function showSpan($var, $name, $title='') {
+			if (edit::getEditMode() && $this->CheckSilent('edit')) {
 				$id=edit::registerDataField($name);
 				echo "<span class='editable' id='editable_$id' ar:path='".$this->path."' ar:id='".$this->id."' title='$title'>";
 				echo $var;
 				echo "</span>";
-			} 
-			else if (!edit::isEmpty($var)) 
-			{
+			} else if (!edit::isEmpty($var)) {
 				echo $var;
 			}
 		}
 
-		function showDiv($var, $name, $title='') 
-		{
-			if (edit::getEditMode() && $this->CheckSilent('edit')) 
-			{
+		function showDiv($var, $name, $title='') {
+			if (edit::getEditMode() && $this->CheckSilent('edit')) {
 				$id=edit::registerDataField($name);
 				echo "<div class='editable' id='editable_$id' ar:path='".$this->path."' ar:id='".$this->id."' title='$title'>";
 				echo $var;
 				echo "</div>";
-			} 
-			else if (!edit::isEmpty($var)) 
-			{
+			} else if (!edit::isEmpty($var)) {
 				echo $var;
 			}
 		}
 
-		function showLink($path='', $extra='') 
-		{
-			if (edit::getEditMode()) 
-			{
+		function showLink($path='', $extra='') {
+			if (edit::getEditMode()) {
 				echo "<a href='".$this->make_url($path).edit::getEditTemplate()."' $extra target='".edit::getEditTarget()."'>";
-			} 
-			else
-			{
+			} else {
 				echo "<a href='".$this->make_url($path)."' $extra>";				
 			}
 		}
 
-		function showEditableLink($path='', $extra='', $url=false) 
-		{
-			if (edit::getEditMode()) 
-			{
+		function showEditableLink($path='', $extra='', $url=false) {
+			if (edit::getEditMode()) {
 				echo "<a onClick=\"event.cancelBubble=true\" onDblClick=\"top.location='".$this->make_url($path).edit::getEditTemplate()."'\" $extra>";
-			}
-			else
-			{
-				if (!$url) 
-				{
+			} else {
+				if (!$url) {
 					$url=$this->make_url($path);
 				}
 				echo "<a href='".$url."' $extra>";				
 			}
 		}
 		
-		function showHref($path='', $extra='') 
-		{
-			if (edit::getEditMode()) 
-			{
+		function showHref($path='', $extra='') {
+			if (edit::getEditMode()) {
 				echo "href='".$this->make_url($path).edit::getEditTemplate()."' $extra target='".edit::getEditTarget()."'";
-			} 
-			else 
-			{
+			} else {
 				echo "href='".$this->make_url($path)."'";
 			}
 		}
 
-		function isEmpty($var) 
-		{
+		function isEmpty($var) {
 			return trim(ereg_replace('&nbsp;',' ',strip_tags($nlsdata->summary, '<img>'))); 
 		}
 	}
 
 	class pinp_edit {
 
-		function _setEditMode($mode=false, $template='user.edit.html', $target='_top') 
-		{
+		function _setEditMode($mode=false, $template='user.edit.html', $target='_top') {
 			return edit::setEditMode($mode, $template, $target);
 		}
 
-		function _getEditMode() 
-		{
+		function _getEditMode() {
 			return edit::getEditMode();
 		}
 
-		function _getEditTemplate() 
-		{
+		function _getEditTemplate() {
 			return edit::getEditTemplate();
 		}
 
-		function _getEditTarget() 
-		{
+		function _getEditTarget() {
 			return edit::getEditTarget();
 		}
 
-		function _registerDataField($name) 
-		{
+		function _registerDataField($name) {
 			return edit::registerDataField($name);
 		}
 
-		function _showInputText($var, $name, $title='') 
-		{
+		function _showInputText($var, $name, $title='') {
 			return edit::showInputText($var, $name, $title);
 		}
 
-		function _showInput($var, $name, $title, $type='text', $extra='') 
-		{
+		function _showInput($var, $name, $title, $type='text', $extra='') {
 			return edit::showInput($var, $name, $title, $type, $extra);
 		}
 
-		function _showSpan($var, $name, $title='') 
-		{
+		function _showSelect($var, $name, $title, $list, $extra='') {
+			return edit::showSelect($var, $name, $title, $list, $extra);
+		}
+
+		function _showSpan($var, $name, $title='') {
 			return edit::showSpan($var, $name, $title);
 		}
 
-		function _showDiv($var, $name, $title='') 
-		{
+		function _showDiv($var, $name, $title='') {
 			return edit::showDiv($var, $name, $title);
 		}
 
-		function _showLink($path='', $extra='') 
-		{
+		function _showLink($path='', $extra='') {
 			return edit::showLink($path, $extra);
 		}
 
-		function _showEditableLink($path='', $extra='', $url=false) 
-		{
+		function _showEditableLink($path='', $extra='', $url=false) {
 			return edit::showEditableLink($path, $extra, $url);			
 		}
 		
-		function _showHref($path='', $extra='') 
-		{
-			return edit::showHref($path, $extra);
+		function _showHref($path='') {
+			return edit::showHref($path);
 		}
 
-		function _isEmpty($var) 
-		{
+		function _isEmpty($var) {
 			return edit::isEmpty($var);
 		}
 	}
+
 ?>
