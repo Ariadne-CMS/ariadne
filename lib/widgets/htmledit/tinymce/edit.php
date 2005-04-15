65ee216c7f8bb80c8454c4c3916b38c2ae8e88b7
Gijsbert te Riet <gijs@muze.nl>
2005-04-15 11:27:19 +0000

    Added TinyMCE support (thanks to Stefan Schreinert!) (the tinymce code is not included in this commit. Just download it from the tinymce website and extract it in the ariadne/www/widgets/htmledit/tinymce directory).
    git-svn-id: https://svn.muze.nl/svn/ariadne-php5/trunk@3376 7882d7ee-46e0-0310-a255-a28a61b628e4

--- lib/widgets/htmledit/tinymce/edit.php
+++ lib/widgets/htmledit/tinymce/edit.php
@@ -1,48 +1,6 @@
-<?php
-	$proceed = false;
-
-	if ($path == $this->path) {
-		if ((($this->arIsNewObject &&
-				($parentobj=current($this->get($this->parent, "system.get.phtml"))) &&
-				$parentobj->CheckLogin("add", $this->type)) ||
-				(!$this->arIsNewObject && $this->CheckLogin("edit", $this->type)) )
-				&& $this->CheckConfig()) {
-
-			$proceed = true;
-		}
-	} else {
-		if ($this->exists($path)) {
-			$target_object = current($this->get($path, "system.get.phtml"));
-			if ($target_object->CheckLogin("edit", $target_object->type) && $this->CheckConfig()) {
-				$proceed = true;
-			}
-		} else {
-			$target_object = current($this->get(
-								$this->store->make_path($path, ".."),
-								"system.get.phtml"));
-			if ($this->CheckConfig()) {
-				$proceed = true;
-			}
-		}
-	}
-
-	if ($proceed) {
-		global $HTTP_SERVER_VARS;
-		if ( $AR->user->data->editor == "tinymce" ) {
-			include($this->store->get_config("code")."widgets/htmledit/tinymce/edit.php");
-		} elseif (	eregi(".*msie ([0-9]+).*",$HTTP_SERVER_VARS["HTTP_USER_AGENT"], $regs) &&
-				$regs[1] && 
-				(intval($regs[1])>=4) && 
-				($AR->user->data->editor!="text")
-			) { //ie4+ && wysiwyg set as default editor
-			include($this->store->get_config("code")."widgets/htmledit/ie/edit.php");
-		} else {
-			if ($file) {
-				$file.="/";
-			}
-?>
 <html>
 <head>
+<title><?php echo $ARnls["edit"]." ".$path; ?></title>
 <script>
 <!--
 
@@ -54,9 +12,11 @@
 		}
 		document.editform.ContentLanguage.value=language;
 		document.editform.htmltext.focus();
+		tinyMCE.updateContent("htmltext");
 	}
 
 	function checksubmit() {
+		tinyMCE.triggerSave();
 		if (window.opener && window.opener.wgHTMLEditContent) {
 			window.opener.wgHTMLEditContent.value=document.editform.htmltext.value;
 			window.close();
@@ -79,11 +39,38 @@
 </style>
 </head>
 <body bgcolor="#BFBFBF" onLoad="loadpage('<?php echo $root; ?>','<?php echo $path; ?>','<?php echo $file; ?>','<?php echo $name; ?>','<?php echo $language; ?>','<?php echo $language; ?>','<?php echo $content; ?>')">
+
+<!-- tinyMCE -->
+<script language="javascript" type="text/javascript" src="<?php echo $AR->dir->www; ?>widgets/htmledit/tinymce/tiny_mce.js"></script>
+<script language="javascript" type="text/javascript">
+   tinyMCE.init({
+         theme : "advanced",
+         plugins : "table,preview,searchreplace",
+         theme_advanced_buttons2_add : "preview,separator,search,replace",
+         theme_advanced_buttons3_add_before : "tablecontrols,separator",
+         theme_advanced_toolbar_location : "top",
+         theme_advanced_toolbar_align : "left",
+         mode : "exact",
+         elements : "htmltext",
+         extended_valid_elements : "a[href|target|name]",
+         language : "<?php echo $language; ?>",
+         auto_cleanup_word : "true"
+
+   });
+</script>
+<!-- /tinyMCE -->
+
+
 <form name="editform" method="post" action="<?php echo $root.$path.$file."edit.".$name.".save.phtml"; ?>" onSubmit="return checksubmit();">
 <input name="ContentLanguage" type="hidden" value="<?php echo $language; ?>">
-<textarea name="htmltext" cols="75" rows="24" wrap="soft"></textarea>
-<table width="100%" border="0" cellspacing="0">
-  <tr>
+<table width="100%" height="100%" border="0" cellspacing="0">
+  <tr style="height: 100%">
+    <td>
+      <div style="width: 100%; height: 100%">
+        <textarea style="width: 100%; height: 100%" name="htmltext" cols="75" rows="24" wrap="soft"></textarea>
+      </div>
+    </td>
+  </tr><tr style="height: 50px;">
 	<td align="right">
 		<input type="button" name="cancel" value="<?php echo $ARnls["cancel"]; ?>" onClick="window.close()">
 		<input type="submit" name="save" value="<?php echo $ARnls["save"]; ?>">
@@ -93,7 +80,4 @@
 </form>
 </body>
 </html>
-<?php
-		}
-	}
-?>
+
