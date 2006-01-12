30d9834ad2a910c52272d0a344e4dd81d9da788a
Gijsbert te Riet <gijs@muze.nl>
2006-01-12 10:40:24 +0000

    Added useful `keepurl' functions.
    git-svn-id: https://svn.muze.nl/svn/ariadne-php5/trunk@3551 7882d7ee-46e0-0310-a255-a28a61b628e4

---
+++ lib/modules/mod_keepurl.php
@@ -0,0 +1,59 @@
+<?php
+	class pinp_keepurl {
+
+		function _make_path($path=".") {
+		global $ARCurrent;
+			$path = $this->make_path($path);
+			if (@count($ARCurrent->shortcut_redirect)) {
+				$redir = end($ARCurrent->shortcut_redirect);
+				if ($redir["keepurl"] && substr($path, 0, strlen($redir["dest"])) == $redir["dest"]) {
+					$path = $redir["src"] .= substr($path, strlen($redir["dest"]));
+				}
+			}
+			return $path;
+		}
+
+		function _currentsection($path=".") {
+		global $ARCurrent;
+			$path = $this->make_path($path);
+			if (@count($ARCurrent->shortcut_redirect)) {
+				$redir = end($ARCurrent->shortcut_redirect);
+				if ($redir["keepurl"] && substr($path, 0, strlen($redir["dest"])) == $redir["dest"]) {
+					$path = $redir["src"];
+				}
+			}
+			$config=$this->loadConfig($path);
+			return $config->section;
+		}
+
+		function _get($rpath, $template, $args='') {
+		global $ARCurrent;
+			$rpath = $this->make_path($rpath);
+			$path = $rpath;
+			while ($path != $prevPath && !$this->exists($path)) {
+				$prevPath = $path;
+				$path = $this->store->make_path($path, '..');
+			}
+			if ($path != $rpath) {
+				$shortcut = current($this->get($path, 'system.get.phtml'));
+				if (!$shortcut->implements('pshortcut')) {
+					$result = Array();
+				} else {
+					if (!is_array($ARCurrent->shortcut_redirect)) {
+						$ARCurrent->shortcut_redirect = Array();
+					}
+					array_push($ARCurrent->shortcut_redirect, Array("src" => $path, "dest" => $shortcut->data->path, "keepurl" => $shortcut->data->keepurl));
+						$npath = $shortcut->data->path.substr($rpath, strlen($path));
+						$result = $this->get($npath, $template, $args);
+					array_pop($ARCurrent->shortcut_redirect);
+				}
+
+			} else {
+				$result = $this->get($path, $template, $args);
+			}
+
+			return $result;
+		}
+
+	}
+?>
