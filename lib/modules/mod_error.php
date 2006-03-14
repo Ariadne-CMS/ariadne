2599438d267c04933dc2509289c1de9cf0472345
Auke van Slooten <auke@muze.nl>
2006-03-14 16:07:12 +0000

    added new error class, which is a very simple version of the PEAR error class. This class is meant to standardize the way libraries and modules will return errors for Ariadne 2 (php4).
    git-svn-id: https://svn.muze.nl/svn/ariadne-php5/trunk@3605 7882d7ee-46e0-0310-a255-a28a61b628e4

---
+++ lib/modules/mod_error.php
@@ -0,0 +1,55 @@
+<?php
+
+class error {
+	var $message;
+	var $code;
+
+	function error($message, $code) {
+		$this->message=$message;
+		$this->code=$code;
+	}
+
+	function isError($ob) {
+		return (is_a($ob, 'error') || is_a($ob, 'PEAR_Error'));
+	}
+
+	function raiseError($message, $code) {
+		return new error($message, $code);
+	}
+
+	function getMessage() {
+		return $this->message;
+	}
+
+	function getCode() {
+		return $this->code;
+	}
+
+	function _raiseError($message, $error) {
+		return error::raiseError($message, $error);
+	}
+
+	function _isError($ob) {
+		return error::isError($ob);
+	}
+
+	function _getMessage() {
+		return $this->getMessage();
+	}
+
+	function _getCode() {
+		return $this->getCode();
+	}
+
+}
+
+class pinp_error extends error {
+
+	function pinp_error($message, $code) {
+		$this->error($message, $code);
+	}
+
+}
+
+	
+?>
