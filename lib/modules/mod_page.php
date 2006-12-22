c9e0f6084611f0a7dab62baf4a06692f094f87af
Auke van Slooten <auke@muze.nl>
2006-12-22 13:59:05 +0000

    renamed cleanHtml to clean, made it a bit easier to call with specific settings, removed the RAWtoAR call from clean, moved it to a new function: compile.
    git-svn-id: https://svn.muze.nl/svn/ariadne-php5/trunk@3709 7882d7ee-46e0-0310-a255-a28a61b628e4

--- lib/modules/mod_page.php
+++ lib/modules/mod_page.php
@@ -3,78 +3,6 @@
 include_once($this->store->get_config('code')."modules/mod_url.php");
 //include_once($this->store->get_config('code')."modules/mod_debug.php");
 
-class page {
-
-	function getBody($page) {
-		return eregi_replace('^.*<BODY[^>]*>', '', eregi_replace('</BODY.*$', '', $page));
-	}
-
-	function parse($page, $full=false) {
-		if (!$full) {
-			$page=page::getBody($page);
-		}
-		return URL::ARtoRAW($page);
-	}
-
-	function isEmpty($page) {
-		if (!$full) {
-			$page=page::getBody($page);
-		}		
-		return trim(str_replace('&nbsp;',' ',strip_tags($page, '<img>')))=='';
-	}
-
-	function getReferences($page) {
-		// Use Perl compatible regex for non-greedy matching
-		preg_match_all("/['\"](\{(arSite|arRoot|arBase|arCurrentPage)(\/[a-z][a-z])?}.*?)['\"]/", $page, $matches);
-		$refs=preg_replace(array("|{arSite(/[a-z][a-z])?}|","|{arRoot(/[a-z][a-z])?}|","|{arBase(/[a-z][a-z])?}|", "|{arCurrentPage(/[a-z][a-z])?}|"),
-			array($this->currentsite(), "", "", $this->path), $matches[1]);
-		foreach ($refs as $ref) { 
-			if ( substr($ref, -1) != '/' && !$this->exists($ref)) {
-				$ref = substr($ref, 0, strrpos($ref, "/")+1);
-			}
-			$result[] = $ref;
-		}
-		return $result;
-	}
-
-	function cleanHtml($var, $tags="_full", $arEditorSettings='') {
-		global $ARCurrent;
-		if( !$arEditorSettings ) {
-			if (!$ARCurrent->arEditorSettings) {
-				$arEditorSettings = $this->call("editor.ini");
-			} else {
-				$arEditorSettings = $ARCurrent->arEditorSettings;
-			}
-		}
-		if ($arEditorSettings) {
-			if ($arEditorSettings["htmlcleaner"]["enabled"]) {
-				include_once($this->store->get_config("code")."modules/mod_htmlcleaner.php");
-				$config = $arEditorSettings["htmlcleaner"];
-				$var = htmlcleaner::cleanup($var, $config);
-			}
-
-			if ($arEditorSettings["htmltidy"]["enabled"]) {
-				include_once($this->store->get_config("code")."modules/mod_tidy.php");
-				$config=$arEditorSettings["htmltidy"];
-				$config["temp"]=$this->store->get_config("files")."temp/";
-				$config["path"]=$AR->Tidy->path;
-				$tidy=new tidy($config);
-				$result=$tidy->clean($var);
-				if ($result["html"]) {
-					$var=$result["html"];
-				}
-			}
-			if ($arEditorSettings[$tags]) {
-				$var=strip_tags($var, $arEditorSettings[$tags]);
-			}
-
-		}
-		//contentlanguage stuff
-		$var = url::RAWtoAR($var, $contentLanguage);
-	    	return $var;
-	}
-}
-
 class pinp_page {
 
 	function _getBody($page) {
@@ -89,13 +17,114 @@ class pinp_page {
 		return page::isEmpty($page);
 	}
 
+	function _clean($page, $settings=false) {
+		return page::clean($page, $settings);
+	}
+
+	function _compile($page) {
+		return page::compile($page);
+	}
+
 	function _getReferences($page) {
 		return page::getReferences($page);
 	}
+
+}
 	
-	function _cleanHtml($var, $tags="_full", $arEditorSettings='') {
-		return page::cleanHtml($var, $tags, $arEditorSettings);
+class page {
+
+	function getBody($page) {
+		return eregi_replace('^.*<BODY[^>]*>', '', eregi_replace('</BODY.*$', '', $page));
 	}
+
+	function parse($page, $full=false) {
+		if (!$full) {
+			$page = page::getBody($page);
+		}
+		return URL::ARtoRAW($page);
+	}
+
+	function isEmpty($page) {
+		if (!$full) {
+			$page = page::getBody($page);
+		}		
+		return trim(str_replace('&nbsp;',' ',strip_tags($page, '<img>')))=='';
+	}
+
+	function clean($page, $settings=false) {
+		global $AR;
+		global $ARCurrent;
+
+		if( !$settings ) {
+			if (!$ARCurrent->arEditorSettings) {
+				$settings = $this->call("editor.ini");
+			} else {
+				$settings = $ARCurrent->arEditorSettings;
+			}
+		}
+
+		if ($settings["htmlcleaner"]["enabled"] || $settings["htmlcleaner"]===true) {
+			include_once($this->store->get_config("code")."modules/mod_htmlcleaner.php");
+			$config	= $settings["htmlcleaner"];
+			$page 	= htmlcleaner::cleanup($page, $config);
+		}
+
+		if ($settings["htmltidy"]["enabled"] || $settings["htmltidy"]===true) {
+			include_once($this->store->get_config("code")."modules/mod_tidy.php");
+			if ($settings["htmltidy"]===true) {
+				$config	= array();
+				$config["options"] = $AR->Tidy->options;
+			} else {
+				$config = $settings["htmltidy"];
+			}
+			$config["temp"]	= $this->store->get_config("files")."temp/";
+			$config["path"]	= $AR->Tidy->path;
+			$tidy			= new tidy($config);
+			$result			= $tidy->clean($page);
+			$page			= $result["html"];
+		}
+
+		if ($settings["allow_tags"]) {
+			$page			= strip_tags($page, $settings["allow_tags"]);
+		}
+
+		return $page;
+	}
+
+	function compile($page) {
+		return URL::RAWtoAR($page, $this->nls);
+	}
+
+	function getReferences($page) {
+		// Find out all references to other objects
+		// (images, links) in this object, so we can
+		// warn the user if he tries to delete/rename
+		// an object which is still referenced somewhere
+		// Use Perl compatible regex for non-greedy matching
+		preg_match_all("/['\"](\{(arSite|arRoot|arBase|arCurrentPage)(\/[a-z][a-z])?}.*?)['\"]/", $page, $matches);
+		$refs	= preg_replace(
+			array(
+				"|{arSite(/[a-z][a-z])?}|",
+				"|{arRoot(/[a-z][a-z])?}|",
+				"|{arBase(/[a-z][a-z])?}|", 
+				"|{arCurrentPage(/[a-z][a-z])?}|" ),
+			array(
+				$this->currentsite(), 
+				"", 
+				"", 
+				$this->path), 
+			$matches[1]);
+		$count	= 0;
+		foreach ($refs as $ref) {
+			if (substr($ref, -1) != '/' && !$this->exists($ref)) {
+				// Drop the template name
+				$ref	= substr($ref, 0, strrpos($ref, "/")+1);
+			}
+			$result[]	= $ref;
+		}
+		return $result;
+	}
+
 }
 
 ?>
