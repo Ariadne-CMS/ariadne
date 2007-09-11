6c8078d510e83f93bfa08587f106cd65972d302b
Gijsbert te Riet <gijs@muze.nl>
2007-09-11 10:44:49 +0000

    Added mod_html2text which is based on http://php-html.sourceforge.net/html2text.php.
    Usage:
    	<pinp>
    		load("mod_html2text.php");
    		// echo plaintext page, with columnwidth set to 80
    		echo html2text::convert(ParsePage($nlsdata->page), 80);
    	</pinp>
    
    
    
    
    git-svn-id: https://svn.muze.nl/svn/ariadne-php5/trunk@3922 7882d7ee-46e0-0310-a255-a28a61b628e4

---
+++ lib/modules/mod_html2text.php
@@ -0,0 +1,12 @@
+<?php
+	global $ariadne;
+	require_once($ariadne."/modules/mod_html2text/html2text.inc");
+
+	class pinp_html2text  {
+		function _convert($aHtmlText, $aMaxColumns) {
+			$html2text =  new Html2Text($aHtmlText, $aMaxColumns);
+			return $html2text->convert();
+		}
+	}
+
+?>
