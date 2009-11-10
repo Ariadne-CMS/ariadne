25ec192911c72e6da9ee69e80bedb740e6d7b426
Yvo Brevoort <yvo@muze.nl>
2009-11-10 21:32:21 +0000

    Added indent function to allow pretty output
    git-svn-id: https://svn.muze.nl/svn/ariadne-php5/trunk@4302 7882d7ee-46e0-0310-a255-a28a61b628e4

--- lib/modules/mod_json.php
+++ lib/modules/mod_json.php
@@ -120,6 +120,9 @@ class pinp_JSON {
 	function _decode($str, $use=0) {
 		return JSON::decode($str, $use);
 	}
+	function _indent($str, $newLine="\n", $indentStr="\t") {
+		return JSON::indent($str, $newLine, $indentStr);
+	}
 }
 
 class JSON {
@@ -146,6 +149,50 @@ class JSON {
 	function decode($str, $use=0) {
 		$json = JSON::getInstance($use);
 		return $json->decode($str);
+	}
+
+	function indent($json, $newLine="\n", $indentStr="\t") {
+	/**
+	 * Indents a flat JSON string to make it more human-readable
+	 *
+	 * @param string $json The original JSON string to process
+	 * @return string Indented version of the original JSON string
+	 */
+	    $result    = '';
+	    $pos       = 0;
+	    $strLen    = strlen($json);
+	 
+	    for($i = 0; $i <= $strLen; $i++) {
+	        
+	        // Grab the next character in the string
+	        $char = substr($json, $i, 1);
+	        
+	        // If this character is the end of an element, 
+	        // output a new line and indent the next line
+	        if($char == '}' || $char == ']') {
+	            $result .= $newLine;
+	            $pos --;
+	            for ($j=0; $j<$pos; $j++) {
+	                $result .= $indentStr;
+	            }
+	        }
+	        
+	        // Add the character to the result string
+	        $result .= $char;
+	 
+	        // If the last character was the beginning of an element, 
+	        // output a new line and indent the next line
+	        if ($char == ',' || $char == '{' || $char == '[') {
+	            $result .= $newLine;
+	            if ($char == '{' || $char == '[') {
+	                $pos ++;
+	            }
+	            for ($j = 0; $j < $pos; $j++) {
+	                $result .= $indentStr;
+	            }
+	        }
+	    }
+	    return $result;
 	}
 }
 
