ec465076bab0907e985581f34ed76b7a7f20ba01
Yvo Brevoort <yvo@muze.nl>
2010-06-20 20:34:04 +0000

    Move shortcut-specific actions to pshortcut, speeds up default pobject version.
    git-svn-id: https://svn.muze.nl/svn/ariadne-php5/trunk@4538 7882d7ee-46e0-0310-a255-a28a61b628e4

--- lib/templates/pobject/system.list.entry.php
+++ lib/templates/pobject/system.list.entry.php
@@ -22,17 +22,6 @@
 	if ($AR->user && !$this->CheckSilent("read")) {
 		$arResult['grants']['read'] = false;
 	}
-
-	if( $this->implements("pshortcut") ) {
-		$arResult["overlay_icon"] = $arResult["icons"]["small"];
-		$arResult["overlay_icons"] = $arResult["icons"];
-
-		$arResult["icons"] = array(
-			"small" => ( $ARCurrent->arTypeIcons[$this->vtype]["small"] ? $ARCurrent->arTypeIcons[$this->vtype]["small"] : $this->call('system.get.icon.php', array('type' => $this->vtype, 'size' => 'small')) ),
-			"medium" => ( $ARCurrent->arTypeIcons[$this->vtype]["medium"] ? $ARCurrent->arTypeIcons[$this->vtype]["medium"] : $this->call('system.get.icon.php', array('type' => $this->vtype, 'size' => 'medium')) ),
-			"large" => ( $ARCurrent->arTypeIcons[$this->vtype]["large"] ? $ARCurrent->arTypeIcons[$this->vtype]["large"] : $this->call('system.get.icon.php', array('type' => $this->vtype, 'size' => 'large')) )
-		);
-	} 
 	
 	$arResult["icon"]=$arResult["icons"]["small"];
 
--- lib/templates/pshortcut/system.list.entry.php
+++ lib/templates/pshortcut/system.list.entry.php
@@ -1,3 +1,14 @@
 <?php
 	$arResult = $this->call("pobject::system.list.entry.php", $arCallArgs);
+
+	$arResult["overlay_icon"] = $arResult["icons"]["small"];
+	$arResult["overlay_icons"] = $arResult["icons"];
+
+	$arResult["icons"] = array(
+		"small" => ( $ARCurrent->arTypeIcons[$this->vtype]["small"] ? $ARCurrent->arTypeIcons[$this->vtype]["small"] : $this->call('system.get.icon.php', array('type' => $this->vtype, 'size' => 'small')) ),
+		"medium" => ( $ARCurrent->arTypeIcons[$this->vtype]["medium"] ? $ARCurrent->arTypeIcons[$this->vtype]["medium"] : $this->call('system.get.icon.php', array('type' => $this->vtype, 'size' => 'medium')) ),
+		"large" => ( $ARCurrent->arTypeIcons[$this->vtype]["large"] ? $ARCurrent->arTypeIcons[$this->vtype]["large"] : $this->call('system.get.icon.php', array('type' => $this->vtype, 'size' => 'large')) )
+	);
+
+	$arResult["icon"]=$arResult["icons"]["small"];
 ?>
