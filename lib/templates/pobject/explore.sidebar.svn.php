7f5088facf4b7090322bfa9bf2a53f327bc45060
Yvo Brevoort <yvo@muze.nl>
2010-06-20 20:49:54 +0000

    Minor speedup in tasks-array building
    git-svn-id: https://svn.muze.nl/svn/ariadne-php5/trunk@4539 7882d7ee-46e0-0310-a255-a28a61b628e4

--- lib/templates/pobject/explore.sidebar.svn.php
+++ lib/templates/pobject/explore.sidebar.svn.php
@@ -10,72 +10,72 @@
 			$nlsdata=$data->$arLanguage;
 		}
 
-		$myName = $nlsdata->name;
-		
-		$fstore	= $this->store->get_filestore_svn("templates");
-		$svnstack = &PEAR_ErrorStack::singleton('VersionControl_SVN');
-		$svn	= $fstore->connect($this->id);
-		$svn_info = $fstore->svn_info($svn);
 
-		$svn_revision = $svn_info['Revision'];
-
-		$tasks = array();
 		if ($this->CheckSilent("layout")) {
+			$myName = $nlsdata->name;
+			
+			$fstore	= $this->store->get_filestore_svn("templates");
+			$svnstack = &PEAR_ErrorStack::singleton('VersionControl_SVN');
+			$svn	= $fstore->connect($this->id);
+			$svn_info = $fstore->svn_info($svn);
+			$svn_revision = $svn_info['Revision'];
+
 			if ($svn_revision) {
-				
-				$tasks[] = array(
-					'href' => $this->make_ariadne_url() . 'dialog.svn.tree.info.php',
-					'onclick' => "muze.ariadne.explore.arshow('svn_object_info',this.href); return false;",
-					'icon' => $AR->dir->images . 'icons/small/svninfo.png',
-					'nlslabel' => $ARnls['ariadne:svn:info']
-				);
-				$tasks[] = array(
-					'href' => $this->make_ariadne_url() . 'dialog.svn.tree.diff.php',
-					'onclick' => "muze.ariadne.explore.arshow('svn_object_diff',this.href); return false;",
-					'icon' => $AR->dir->images . 'icons/small/svndiff.png',
-					'nlslabel' => $ARnls["ariadne:svn:diff"]
-				);
-				$tasks[] = array(
-					'href' => $this->make_ariadne_url() . 'dialog.svn.tree.commit.php',
-					'onclick' => "muze.ariadne.explore.arshow('svn_object_commit',this.href); return false;",
-					'icon' => $AR->dir->images . 'icons/small/svncommit.png',
-					'nlslabel' => $ARnls['ariadne:svn:commit']
-				);
-				$tasks[] = array(
-					'href' => $this->make_ariadne_url() . 'dialog.svn.tree.revert.php',
-					'onclick' => "muze.ariadne.explore.arshow('svn_object_revert',this.href); return false;",
-					'icon' => $AR->dir->images . 'icons/small/svnrevert.png',
-					'nlslabel' => $ARnls['ariadne:svn:revert']
-				);
-				$tasks[] = array(
-					'href' => $this->make_ariadne_url() . 'dialog.svn.tree.update.php',
-					'onclick' => "muze.ariadne.explore.arshow('svn_object_update',this.href); return false;",
-					'icon' => $AR->dir->images . 'icons/small/svnupdate.png',
-					'nlslabel' => $ARnls['ariadne:svn:update']
-				);
-				$tasks[] = array(
-					'href' => $this->make_ariadne_url() . 'dialog.svn.tree.unsvn.php',
-					'onclick' => "muze.ariadne.explore.arshow('svn_object_unsvn',this.href); return false;",
-					'icon' => $AR->dir->images . 'icons/small/unsvn.png',
-					'nlslabel' => $ARnls['ariadne:svn:unsvn']
+				$tasks = array(
+					array(
+						'href' => $this->make_ariadne_url() . 'dialog.svn.tree.info.php',
+						'onclick' => "muze.ariadne.explore.arshow('svn_object_info',this.href); return false;",
+						'icon' => $AR->dir->images . 'icons/small/svninfo.png',
+						'nlslabel' => $ARnls['ariadne:svn:info']
+					),
+					array(
+						'href' => $this->make_ariadne_url() . 'dialog.svn.tree.diff.php',
+						'onclick' => "muze.ariadne.explore.arshow('svn_object_diff',this.href); return false;",
+						'icon' => $AR->dir->images . 'icons/small/svndiff.png',
+						'nlslabel' => $ARnls["ariadne:svn:diff"]
+					),
+					array(
+						'href' => $this->make_ariadne_url() . 'dialog.svn.tree.commit.php',
+						'onclick' => "muze.ariadne.explore.arshow('svn_object_commit',this.href); return false;",
+						'icon' => $AR->dir->images . 'icons/small/svncommit.png',
+						'nlslabel' => $ARnls['ariadne:svn:commit']
+					),
+					array(
+						'href' => $this->make_ariadne_url() . 'dialog.svn.tree.revert.php',
+						'onclick' => "muze.ariadne.explore.arshow('svn_object_revert',this.href); return false;",
+						'icon' => $AR->dir->images . 'icons/small/svnrevert.png',
+						'nlslabel' => $ARnls['ariadne:svn:revert']
+					),
+					array(
+						'href' => $this->make_ariadne_url() . 'dialog.svn.tree.update.php',
+						'onclick' => "muze.ariadne.explore.arshow('svn_object_update',this.href); return false;",
+						'icon' => $AR->dir->images . 'icons/small/svnupdate.png',
+						'nlslabel' => $ARnls['ariadne:svn:update']
+					),
+					array(
+						'href' => $this->make_ariadne_url() . 'dialog.svn.tree.unsvn.php',
+						'onclick' => "muze.ariadne.explore.arshow('svn_object_unsvn',this.href); return false;",
+						'icon' => $AR->dir->images . 'icons/small/unsvn.png',
+						'nlslabel' => $ARnls['ariadne:svn:unsvn']
+					)
 				);
 			} else {
-				$tasks[] = array(
-					'href' => $this->make_ariadne_url() . 'dialog.svn.tree.checkout.php',
-					'onclick' => "muze.ariadne.explore.arshow('svn_object_checkout',this.href); return false;",
-					'icon' => $AR->dir->images . 'icons/small/svncheckout.png',
-					'nlslabel' => $ARnls['ariadne:svn:checkout']
-				);
-				$tasks[] = array(
-					'href' => $this->make_ariadne_url() . 'dialog.svn.tree.import.php',
-					'onclick' => "muze.ariadne.explore.arshow('svn_object_import',this.href); return false;",
-					'icon' => $AR->dir->images . 'icons/small/svnimport.png',
-					'nlslabel' => $ARnls['ariadne:svn:import']
+				$tasks = array(
+					array(
+						'href' => $this->make_ariadne_url() . 'dialog.svn.tree.checkout.php',
+						'onclick' => "muze.ariadne.explore.arshow('svn_object_checkout',this.href); return false;",
+						'icon' => $AR->dir->images . 'icons/small/svncheckout.png',
+						'nlslabel' => $ARnls['ariadne:svn:checkout']
+					),
+					array(
+						'href' => $this->make_ariadne_url() . 'dialog.svn.tree.import.php',
+						'onclick' => "muze.ariadne.explore.arshow('svn_object_import',this.href); return false;",
+						'icon' => $AR->dir->images . 'icons/small/svnimport.png',
+						'nlslabel' => $ARnls['ariadne:svn:import']
+					)
 				);
 			}
-		}
 
-		if ($tasks) {
 			$svn_icon = false;
 			$label = $ARnls['ariadne:svn:settings'];
 			if ($svn_revision) {
