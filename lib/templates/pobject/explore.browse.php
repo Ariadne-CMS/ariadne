d4bd5cf128642fe2b984fadc905766e0a9b5adfc
Wouter Commandeur <wouter@muze.nl>
2010-06-22 09:07:47 +0000

    only build the data array for table display if we are actually going to use it
    git-svn-id: https://svn.muze.nl/svn/ariadne-php5/trunk@4551 7882d7ee-46e0-0310-a255-a28a61b628e4

--- lib/templates/pobject/explore.browse.php
+++ lib/templates/pobject/explore.browse.php
@@ -52,36 +52,39 @@
 
 		$object_list = $this->call('system.list.objects.php', $listargs);
 
-		$data = array();
 		if (!is_array($object_list)) {
 			$object_list = array();
 		}
 		if (!is_array($object_list['objects'])) {
 			$object_list['objects'] = array();
 		}
-		foreach($object_list['objects'] as $item) {
-			$datarow = array();
-			if ($AR->SVN->enabled) {
-				$datarow['svn'] = yui::getSvnIcon($item['svn']['status']);
-			}
-			$datarow['type'] = yui::getTypeIcon($item, $viewtype);
-			$datarow['name'] = $item['name'];
-			$datarow['filename'] = $item['filename'];
-			$datarow['path'] = $item['path'];
-			$datarow['size'] = $item['size'];
-			$datarow['owner']  = $item['owner'];
-			$datarow['modified'] = strftime("%d-%m-%Y",$item['lastchanged']);
-			$datarow['priority']  = $item['priority'];
-
-			if (is_array($item['language'])) {
-				foreach( $item['language'] as $key => $value ) {
-					$datarow['language'] .= "<a href='#' onClick=\"muze.ariadne.registry.set('store_root', muze.ariadne.registry.get('root') + '/-' + muze.ariadne.registry.get('SessionID') + '-/" . $key . "'); muze.ariadne.explore.objectadded(); return false;\" title=\"".htmlspecialchars($value)."\"><img class=\"flag\" src=\"".$AR->dir->images."nls/small/".$key.".gif\" alt=\"".htmlspecialchars($value)."\"></a> ";
-				}
-			}
-			array_push($data, $datarow);
-		}
 		$divId = "resultsDiv";
 		$tableId = "resultsTable";
+
+		if( $viewtype == "details" ) {
+			$data = array();
+			foreach($object_list['objects'] as $item) {
+				$datarow = array();
+				if ($AR->SVN->enabled) {
+					$datarow['svn'] = yui::getSvnIcon($item['svn']['status']);
+				}
+				$datarow['type'] = yui::getTypeIcon($item, $viewtype);
+				$datarow['name'] = $item['name'];
+				$datarow['filename'] = $item['filename'];
+				$datarow['path'] = $item['path'];
+				$datarow['size'] = $item['size'];
+				$datarow['owner']  = $item['owner'];
+				$datarow['modified'] = strftime("%d-%m-%Y",$item['lastchanged']);
+				$datarow['priority']  = $item['priority'];
+
+				if (is_array($item['language'])) {
+					foreach( $item['language'] as $key => $value ) {
+						$datarow['language'] .= "<a href='#' onClick=\"muze.ariadne.registry.set('store_root', muze.ariadne.registry.get('root') + '/-' + muze.ariadne.registry.get('SessionID') + '-/" . $key . "'); muze.ariadne.explore.objectadded(); return false;\" title=\"".htmlspecialchars($value)."\"><img class=\"flag\" src=\"".$AR->dir->images."nls/small/".$key.".gif\" alt=\"".htmlspecialchars($value)."\"></a> ";
+					}
+				}
+				array_push($data, $datarow);
+			}
+		}
 ?>
 
 <script type="text/javascript">
