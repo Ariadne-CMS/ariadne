bc55dab689db7e4417d49c84e48ebfcf419676c1
Yvo Brevoort <yvo@muze.nl>
2010-07-26 11:37:47 +0000

    Added display of grantsstring if there is no grants array available
    git-svn-id: https://svn.muze.nl/svn/ariadne-php5/trunk@4624 7882d7ee-46e0-0310-a255-a28a61b628e4

--- lib/templates/pobject/dialog.grants.review.php
+++ lib/templates/pobject/dialog.grants.review.php
@@ -4,13 +4,18 @@
 
 	$data = $this->getdata('data');
 	echo "<p>The grants specified below will be set. If you are satisfied with the changes, please press 'Apply' once more to set the grants.</p>";
-	
+
 	echo "<table width=\"100%\">";
 	echo "<tr><th>".$ARnls["path"]."</th><th>User/group</th><th>".$ARnls["grants"]."</th></tr>";
 	foreach( $data as $gpath => $gusers ) {
 		foreach( $gusers as $upath => $value ) {
 			echo "<tr><td>".htmlspecialchars($gpath)."</td><td>".htmlspecialchars($upath)."</td>";
-			echo "<td>".grantsArrayToString($value["grants"]["array"])."</td></tr>";
+			if (is_array($value['grants']['array'])) {
+				echo "<td>".grantsArrayToString($value["grants"]["array"])."</td>";
+			} else {
+				echo "<td>".$value['grants']['grantsstring']."</td>";
+			}
+			echo "</tr>";
 		}
 	}
 	echo "</table>";
