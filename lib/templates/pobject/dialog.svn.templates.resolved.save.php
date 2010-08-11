47ba950989f55266af6d9132e884043123b77540
Yvo Brevoort <yvo@muze.nl>
2010-08-11 16:01:53 +0000

    typofix
    git-svn-id: https://svn.muze.nl/svn/ariadne-php5/trunk@4647 7882d7ee-46e0-0310-a255-a28a61b628e4

--- lib/templates/pobject/dialog.svn.templates.resolved.save.php
+++ lib/templates/pobject/dialog.svn.templates.resolved.save.php
@@ -3,7 +3,7 @@
 	if ($this->CheckLogin("layout") && $this->CheckConfig()) {
 		echo "<pre class='svnresult'>\n";
 		set_time_limit(0);
-		$this->call("system.svn.revolved.php", $arCallArgs);
+		$this->call("system.svn.resolved.php", $arCallArgs);
 		echo "</pre>\n";
 		$this->call("window.opener.objectadded.js");
 	}
