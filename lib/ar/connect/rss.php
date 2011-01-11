5f54092286bf159c98e0da96156330fb946300ee
Auke van Slooten <auke@muze.nl>
2011-01-11 13:57:34 +0000

    rss now follows the ar components guidelines better it has a seperate factory and client the client uses dependency injection for the http client it now has seperate get() and parse() methods.
    git-svn-id: https://svn.muze.nl/svn/ariadne-php5/trunk@4786 7882d7ee-46e0-0310-a255-a28a61b628e4

--- lib/ar/connect/rss.php
+++ lib/ar/connect/rss.php
@@ -4,11 +4,43 @@ ar_pinp::allow('ar_connect_rss');
 
 ar::load('xml');
 
-class ar_connect_rss extends ar_xmlDataBinding {
+class ar_connect_rss extends arBase {
 
-	public function __construct( $feed = null ) {
-		if ($feed) {
-			$xml = ar_http::get($feed);
+	public static function client( $url, $httpClient = null ) {
+		if ( !isset($httpClient) ) {
+			$httpClient = ar_http::client();
+		}
+		return new ar_connect_rssClient( $url, $httpClient );
+	}
+
+}
+
+class ar_connect_rssClient extends ar_xmlDataBinding {
+
+	private $httpClient = null;
+	private $feed = null;
+	private $xml = null;
+	
+	public function __construct( $feed = null, $httpClient = null ) {
+		$this->feed = $feed;
+		$this->httpClient = $httpClient;
+		if ($feed && $this->httpClient) {
+			$this->get( $feed )->parse();
+		}
+	}
+
+	public function get( $url ) {
+		$this->xml = $this->httpClient->get( $url );
+		return $this;
+	}
+	
+	public function parse( $xml = null ) {
+		if ( !$xml ) {
+			if ($this->xml) {
+				$xml = $this->xml;
+			}
+		}
+		if ($xml) {
 			$dom = ar_xml::parse( $xml );
 
 			$this->bind( $dom->rss->channel->title, 'title' )
@@ -16,9 +48,10 @@ class ar_connect_rss extends ar_xmlDataBinding {
 				->bindAsArray( 
 					$dom->rss->channel->item, 
 					'items', 
-					array( 'ar_connect_rss', 'parseItem' )
+					array( 'ar_connect_rssClient', 'parseItem' )
 				);
 		}
+		return $this;
 	}
 
 	public function parseItem( $item ) {
@@ -30,10 +63,6 @@ class ar_connect_rss extends ar_xmlDataBinding {
 				->bind( $item->description, "description")
 				->bind( $item->{"dc:creator"}, "dc_creator")
 				->bind( $item->pubDate, "pubDate");	
-	}
-
-	public static function get( $url ) {
-		return new ar_connect_rss( $url );
 	}
 
 }
