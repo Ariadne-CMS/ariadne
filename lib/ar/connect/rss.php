cebb7cc90d0261a0167ce303376319aa0ea1d54f
Auke van Slooten <auke@muze.nl>
2011-01-11 16:36:33 +0000

    included the rss 2.0 items and only those, added a rawXML for the channel and item to be able to support other namespaced elements
    git-svn-id: https://svn.muze.nl/svn/ariadne-php5/trunk@4794 7882d7ee-46e0-0310-a255-a28a61b628e4

--- lib/ar/connect/rss.php
+++ lib/ar/connect/rss.php
@@ -6,11 +6,16 @@ ar::load('xml');
 
 class ar_connect_rss extends arBase {
 
-	public static function client( $url, $httpClient = null ) {
+	public static function client( $url = null, $httpClient = null ) {
 		if ( !isset($httpClient) ) {
 			$httpClient = ar_http::client();
 		}
 		return new ar_connect_rssClient( $url, $httpClient );
+	}
+	
+	public static function parse( $xml ) {
+		$client = new ar_connect_rssClient( null, ar_http::client() );
+		return $client->parse( $xml );
 	}
 
 }
@@ -19,50 +24,63 @@ class ar_connect_rssClient extends ar_xmlDataBinding {
 
 	private $httpClient = null;
 	private $feed = null;
-	private $xml = null;
 	
 	public function __construct( $feed = null, $httpClient = null ) {
 		$this->feed = $feed;
 		$this->httpClient = $httpClient;
 		if ($feed && $this->httpClient) {
-			$this->get( $feed )->parse();
+			$this->get( $feed );
 		}
 	}
 
-	public function get( $url ) {
-		$this->xml = $this->httpClient->get( $url );
-		return $this;
+	public function get( $url, $request = null, $options = array() ) {
+		$xml = $this->httpClient->get( $url, $request, $options );
+		if ( $xml && !ar_error::isError( $xml ) ) {
+			return $this->parse( $xml );
+		} else {
+			return $xml;
+		}
 	}
 	
-	public function parse( $xml = null ) {
-		if ( !$xml ) {
-			if ($this->xml) {
-				$xml = $this->xml;
-			}
-		}
-		if ($xml) {
-			$dom = ar_xml::parse( $xml );
-
-			$this->bind( $dom->rss->channel->title, 'title' )
-				->bind( $dom->rss->channel->link, 'link' )
-				->bindAsArray( 
-					$dom->rss->channel->item, 
-					'items', 
-					array( 'ar_connect_rssClient', 'parseItem' )
-				);
-		}
+	public function parse( $xml ) {
+		$dom = ar_xml::parse( $xml );
+		$channel = $dom->rss->channel[0];
+		$this->bind( $channel->title, 'title' )
+			->bind( $channel->link, 'link' )
+			->bind( $channel->description, 'description' )
+			->bind( $channel->language, 'language' )
+			->bind( $channel->copyright, 'copyright' )
+			->bind( $channel->managingEditor, 'managingEditor' )
+			->bind( $channel->webMaster, 'webMaster' )
+			->bind( $channel->pubDate, 'pubDate' )
+			->bind( $channel->lastBuildDate, 'lastBuildDate' )
+			->bind( $channel->category, 'category' )
+			->bind( $channel->generator, 'generator' )
+			->bind( $channel->cloud->attributes, 'cloud', 'array' )
+			->bind( $channel->ttl, 'ttl', 'int' )
+			->bind( $channel->image->attributes, 'image', 'array' )
+			->bindAsArray( 
+				$channel->item, 
+				'items', 
+				array( 'ar_connect_rssClient', 'parseItem' )
+			)
+			->bind( $dom, 'rawXML', 'xml' );
 		return $this;
 	}
 
 	public function parseItem( $item ) {
-		return $item->bind( $item->title, "title")
-				->bind( $item->link, "link" )
-				->bind( $item->guid, "guid" )
-				->bind( $item->{"media:content"}[0]->attributes, "media_content", "array")
-				->bind( $item->{"media:description"}[0], "media_description", "html")
-				->bind( $item->description, "description")
-				->bind( $item->{"dc:creator"}, "dc_creator")
-				->bind( $item->pubDate, "pubDate");	
+		return $item->bind( $item->title, 'title')
+				->bind( $item->link, 'link' )
+				->bind( $item->description, 'description')
+				->bind( $item->guid, 'guid' )
+				->bind( $item->author, 'author' )
+				->bind( $item->category, 'category' )
+				->bind( $item->comments, 'comments' )
+				->bind( $item->enclosure->attributes, 'enclosure', 'array' )
+				->bind( $item->source->nodeValue, 'source' )
+				->bind( $item->source->getAttribute('url'), 'source_url' )
+				->bind( $item->pubDate, 'pubDate')	
+				->bind( $item, 'rawXML', 'xml' );
 	}
 
 }
