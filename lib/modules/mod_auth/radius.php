feafc4a4723e58ddc1018ed54ebb49c73af10a52
Robbert Müller <muller@muze.nl>
2011-05-25 15:45:00 +0000

    Initial support for radius authentication This authenticates agains a radius server using pap authentication TODO: adding support for chap eap e.d. kinds of authentication
    git-svn-id: https://svn.muze.nl/svn/ariadne-php5/trunk@4933 7882d7ee-46e0-0310-a255-a28a61b628e4

---
+++ lib/modules/mod_auth/radius.php
@@ -0,0 +1,134 @@
+<?php
+
+	include_once($ariadne."/modules/mod_auth/default.php");
+
+	class mod_auth_radius extends mod_auth_default {
+
+		function __construct($config) {
+			$this->config = $config;
+		}
+
+	 	function storeExternalUser($login, $userData) {
+	 	global $AR, $store;
+	 		// become admin for the moment
+			$aLogin		= $this->config["import_user_by"];
+	 		$AR->user	= current($store->call("system.get.phtml", "", $store->find("/system/users/", "login.value='$aLogin' and object.implements='puser'")));
+
+	 		$user = $this->getUser($login);
+	 		if ($user !== true) {
+				$user_dir		= $this->config["import_user_directory"];
+				$user_profile		= $this->config["import_user_profile"];
+				$user_type		= $this->config["import_user_type"];
+				if (!$user_type) {
+					$user_type = "puser";
+				}
+
+	 			debug("ldAuthRadius: user ($login) didn't exist before: creating", "all");
+				$data = $userData;
+	 			$data["arNewFilename"] = "$user_dir$login/";
+				$data["profile"] = $user_profile;
+				$data["setowner"] = true;
+				
+	 			$user = $store->newobject(
+	 						"$user_dir$login/", 
+	 						"$user_dir", 
+	 						"$user_type", 
+	 						new object);
+
+	 			$user->arIsNewObject = true;
+	 			$user->call('system.save.data.phtml', $data);
+
+				return $user;
+	 		} else {
+				// $AR->user was set by getUser and contains the correct user now.
+				return $AR->user;
+			}
+	 	}
+
+	 	function authExternalUser($login, $password) {
+	 	global $store, $AR;
+
+
+			$res = radius_auth_open();
+			if (!radius_add_server($res, $this->config['radius_server'], $this->config['radius_port'], $this->config['sharedsecret'], 3, 3)) {
+				debug('RadiusError:' . radius_strerror($res). "\n",'auth');
+				return false;
+			}
+
+			if (!radius_create_request($res, RADIUS_ACCESS_REQUEST)) {
+				debug('RadiusError:' . radius_strerror($res). "\n",'auth');
+				return false;
+			}
+
+			if (!radius_put_string($res, RADIUS_NAS_IDENTIFIER, isset($_SERVER['HTTP_HOST']) ? $HTTP_HOST : 'localhost'))  {
+				debug('RadiusError:' . radius_strerror($res). "\n",'auth');
+				return false;
+			}
+
+			if (!radius_put_int($res, RADIUS_SERVICE_TYPE, RADIUS_FRAMED)) {
+				debug('RadiusError:' . radius_strerror($res). "\n",'auth');
+				return false;
+			}
+
+			if (!radius_put_int($res, RADIUS_FRAMED_PROTOCOL, RADIUS_PPP)) {
+				debug('RadiusError:' . radius_strerror($res). "\n",'auth');
+				return false;
+			}
+
+			if (!radius_put_string($res, RADIUS_CALLING_STATION_ID, isset($_SERVER['REMOTE_HOST']) ? $_SERVER['REMOTE_HOST'] : '127.0.0.1') == -1) {
+				debug('RadiusError:' . radius_strerror($res). "\n",'auth');
+				return false;
+			}
+
+			if (!radius_put_string($res, RADIUS_USER_NAME, $login)) {
+				debug('RadiusError:' . radius_strerror($res). "\n",'auth');
+				return false;
+			}
+			if($password){
+				if (!radius_put_string($res, RADIUS_USER_PASSWORD, $password)) {
+					debug('RadiusError:' . radius_strerror($res). "\n",'auth');
+					return false;
+				}
+			}
+
+			if (!radius_put_int($res, RADIUS_SERVICE_TYPE, RADIUS_FRAMED)) {
+				debug('RadiusError:' . radius_strerror($res). "\n",'auth');
+				return false;
+			}
+
+			if (!radius_put_int($res, RADIUS_FRAMED_PROTOCOL, RADIUS_PPP)) {
+				debug('RadiusError:' . radius_strerror($res). "\n",'auth');
+				return false;
+			}
+
+			$req = radius_send_request($res);
+			if (!$req) {
+				debug('RadiusError:' . radius_strerror($res). "\n",'auth');
+				return false;
+			}
+
+			$user = false;
+			switch($req) {
+				case RADIUS_ACCESS_ACCEPT:
+					/* generate a uniq, not guessable, password */
+					srand((double)microtime()*1000000);
+					$password = ARCrypt(uniqid(rand(), true));
+
+					$userData = Array();
+					$userData["name"] = $login;
+					$userData["newpass1"] = $password;
+					$userData["newpass2"] = $password;
+	 				$user = $this->storeExternalUser($login, $userData);
+					break;
+
+				case RADIUS_ACCESS_REJECT:
+					debug("RadiusError: Radius Request rejected\n",'auth');
+					break;
+
+				default:
+					debug("RadiusError: Unknown answer\n",'auth');
+			}
+			return $user;
+	 	}
+	}
+?>
