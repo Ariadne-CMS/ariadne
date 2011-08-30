e1765c7f8532ed4b5ef6468767c64c51c09ca4f4
Robbert Müller <muller@muze.nl>
2011-08-30 08:15:23 +0000

    Fixing nls handling of config.ini
    git-svn-id: https://svn.muze.nl/svn/ariadne-php5/trunk@5021 7882d7ee-46e0-0310-a255-a28a61b628e4

--- lib/objects/ariadne_object.php
+++ lib/objects/ariadne_object.php
@@ -1764,7 +1764,7 @@ abstract class ariadne_object extends object { // ariadne_object class definitio
 				// a language select dialog with all available languages for this object.
 				if (isset($this->data->nls) && !$this->data->name) {
 					if (!$ARCurrent->forcenls && (!isset($this->data->nls->list[$reqnls]) || !$config->nls->list[$reqnls])) {
-						if (!$ARCurrent->nolangcheck ) {
+						if (!$ARCurrent->nolangcheck && $arCallFunction != 'config.ini') {
 							$ARCurrent->nolangcheck=1;
 							$eventData = new object();
 							$eventData->arCallFunction = $arCallFunction;
