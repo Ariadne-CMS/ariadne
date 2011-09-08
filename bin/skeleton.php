484eb0abc1073bacfb1fa5d1a4c47225eb50377a
Auke van Slooten <auke@muze.nl>
2011-09-08 08:05:06 +0000

    added ar.php and changed all require/includes to more correct require_once
    git-svn-id: https://svn.muze.nl/svn/ariadne-php5/trunk@5049 7882d7ee-46e0-0310-a255-a28a61b628e4

--- bin/skeleton.php
+++ bin/skeleton.php
@@ -12,7 +12,7 @@
 	require_once($ariadne."/includes/loader.cmd.php");
 	require_once($ariadne."/stores/".$store_config["dbms"]."store.phtml");
 	include($ariadne."/nls/".$AR->nls->default);
-	include_once($ariadne."/modules/mod_ar.php");
+	require_once($ariadne."/ar.php");
 
 	/* instantiate the store */
 	$inst_store = $store_config["dbms"]."store";
