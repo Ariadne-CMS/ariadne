7cbe3cd535ca61b631477d96c2bc54b35bf3e7b1
Auke van Slooten <auke@muze.nl>
2011-09-08 08:18:11 +0000

    added some shortcut calls, but still not usable.
    git-svn-id: https://svn.muze.nl/svn/ariadne-php5/trunk@5055 7882d7ee-46e0-0310-a255-a28a61b628e4

--- lib/ar/core/store.php
+++ lib/ar/core/store.php
@@ -1,12 +1,44 @@
 <?php
-	class ar_core_store {
-	
-		static public function create( $storeOptions ) {
-			require_once($storeOptions['code'].'stores/'.$storeOptions['dbms'].'store.phtml');
-			$storeName = $storeOptions['dbms'].'store';
-			$store     = new $storeName($storeOptions['root'], $storeOptions);
-			return $store;
-		}
 
+	class ar_core_store {
+		
+		private static $store = null;
+
+		private static function getStore() {
+			if ( !self::$store ) {
+				self::$store = new ar_core_store_nodes( 
+					ar_core_registry::get('dbConnector'),
+					ar_core_registry::get('objectStore'),
+					ar_core_registry::get('propertyStore')
+				);
+			}
+			return self::$store;
+		}
+		
+		public static function get( $path ) {
+			$store = self::getStore();
+			return $store->get( $path );
+		}
+		
+		public static function exists( $path ) {
+			$store = self::getStore();
+			return $store->exists( $path );		
+		}
+		
+		public static function ls( $path ) {
+			$store = self::getStore();
+			return $store->ls( $path );		
+		}
+		
+		public static function find( $path, $query ) {
+			$store = self::getStore();
+			return $store->find( $path, $query );		
+		}
+		
+		public static function parents( $path ) {
+			$store = self::getStore();
+			return $store->parents( $path );		
+		}
 	}
+	
 ?>
