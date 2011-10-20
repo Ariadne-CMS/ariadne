1227f510aa628a42790974c279958676e5bfb806
Gijsbert te Riet <gijs@muze.nl>
2011-10-20 08:47:10 +0000

    backport of http://svn.muze.nl/trunk/www/install/set_admin_password.php?r1=4686&r2=5048&pathrev=5048&limit_changes=100&root=ariadne-php5
    git-svn-id: https://svn.muze.nl/svn/ariadne-php5/branches/2.7-fixes@5077 7882d7ee-46e0-0310-a255-a28a61b628e4

--- www/install/set_admin_password.php
+++ www/install/set_admin_password.php
@@ -1,10 +1,12 @@
 <?php
-	if ($new_admin_password) {
+	$ariadne = '';
+	if ( isset($_GET['new_admin_password']) ) {
 		include_once("../ariadne.inc");
 		include_once($ariadne."/configs/ariadne.phtml");
 		include_once($ariadne."/configs/store.phtml");
 		include_once($ariadne."/stores/".$store_config["dbms"]."store.phtml");
 		include_once($ariadne."/includes/loader.web.php");
+		include_once($ariadne."/modules/mod_ar.php");
 
 		/* become admin */
 		$ARLogin="admin";
@@ -14,16 +16,23 @@
 
 		$inst_store = $store_config["dbms"]."store";
 		$store=new $inst_store(".",$store_config);
+
+		global $ARCurrent;
+		$ARCurrent->nolangcheck = true;
+		$ARCurrent->allnls = true;
 	
 		/* update the admin user with the supplied password */
 		$store->call(
 			"system.save.data.phtml", 
 			Array(
-				"newpass1" => $new_admin_password,
-				"newpass2" => $new_admin_password
+				"newpass1" => $_GET['new_admin_password'],
+				"newpass2" => $_GET['new_admin_password']
 			),
 			$store->get("/system/users/admin/")
 		);
+
 		$store->close();
+	} else {
+		echo 'no password entered';
 	}
 ?>
