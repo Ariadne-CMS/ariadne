630eed5fddd87c7b6d1ffc82de6ad3a92b1006c6
Gijsbert te Riet <gijs@muze.nl>
2011-11-07 13:04:08 +0000

    Added getAttribute() method.
    git-svn-id: https://svn.muze.nl/svn/ariadne-php5/branches/2.7-fixes@5089 7882d7ee-46e0-0310-a255-a28a61b628e4

--- lib/ar/xml.php
+++ lib/ar/xml.php
@@ -976,6 +976,10 @@
 			return $this;
 		}
 
+		function getAttribute( $name ) {
+			return $this->attributes[$name];
+		}
+
 		function setAttribute( $name, $value ) {
 			if ( $name == 'id' ) {
 				$oldId = null;
