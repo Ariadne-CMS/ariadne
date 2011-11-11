b8dc3ad33e14cb4e143b601ee995f2c6c87e3388
Yvo Brevoort <yvo@muze.nl>
2011-11-11 10:45:35 +0000

    Fixed display of paths, added root option
    git-svn-id: https://svn.muze.nl/svn/ariadne-php5/trunk@5091 7882d7ee-46e0-0310-a255-a28a61b628e4

--- lib/templates/pobject/dialog.delete.form.php
+++ lib/templates/pobject/dialog.delete.form.php
@@ -2,24 +2,42 @@
 	$ARCurrent->nolangcheck=true;
 
 	if ($this->CheckLogin("delete") && $this->CheckConfig()) {
+		$root = $this->getvar("root") ? $this->getvar("root") : $this->currentsite();
+		if (substr($root, -1) != "/") {
+			$root .= "/";
+		}
+
 		$query = "object.path =~ '" . $target . "%' order by path DESC";
 		$total = $this->count_find($this->path, $query);
 		$total--; // children of current path.
 		$crumbs = '';
-		$parents = $this->parents($this->path, 'system.get.name.phtml', '', $this->currentsite());
+		$path = '';
+		$parents = $this->parents($this->path, 'system.get.name.phtml', '', $root);
+		$parentpaths = $this->parents($this->path, 'system.get.path.phtml', '', $root);
+		$path = array_pop($parentpaths);
+
+		if (substr($path, 0, strlen($root)) == $root) {
+			$path = substr($path, strlen($root), strlen($path));
+		}
+
+		$path = "/" . $path;
+
 		array_pop($parents); //remove current item from the list.
 		foreach ($parents as $name) {
 			$crumbs .= $name . " / ";
 		}
 
-		$crumbs .= $nlsdata->name;
-		if (strlen($crumbs) > 25) {
-			$oldcrumbs = htmlspecialchars($crumbs);
-			$crumbs = htmlspecialchars(mb_substr($crumbs, 0, 12, "utf-8") . "..." . mb_substr($crumbs, strlen($crumbs)-12, strlen($crumbs), "utf-8"));
-		} else {
-			$crumbs = htmlspecialchars($crumbs);
+		$oldcrumbs = $crumbs;
+		if (strlen($crumbs) > 18) {
+			$crumbs = mb_substr($crumbs, 0, 12, "utf-8") . "..." . mb_substr($crumbs, strlen($crumbs)-5, strlen($crumbs), "utf-8");
+			if (strlen($crumbs) >= strlen($oldcrumbs)) {
+				$crumbs = $oldcrumbs;
+			}
 		}
+		$crumbs = htmlspecialchars($crumbs . $nlsdata->name);
+		$oldcrumbs = htmlspecialchars($oldcrumbs);
 
+		
 ?>
 <fieldset id="data">
 	<legend><?php echo $ARnls["delete"]; ?> <span class="crumbs" title="<?php echo $oldcrumbs; ?>"><?php echo $crumbs; ?></span></legend>
@@ -28,7 +46,7 @@
 	} else { ?>
 		<span class="crumbs" title="<?php echo $oldcrumbs; ?>">
 			<?php echo $crumbs; ?>
-		</span> (<?php echo $this->path; ?>)
+		</span> (<?php echo $path; ?>)
 		<?php echo sprintf($ARnls['q:removeall'], '', $total); ?>
 
 		<div class="field checkbox">
