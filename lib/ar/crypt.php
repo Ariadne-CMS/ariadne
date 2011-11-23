50cd7ad834a6e15396c443908d0bf7e001f252c8
Gijsbert te Riet <gijs@muze.nl>
2011-11-23 10:44:37 +0000

    Added ar/crypt wrapper
    git-svn-id: https://svn.muze.nl/svn/ariadne-php5/branches/2.7-fixes@5107 7882d7ee-46e0-0310-a255-a28a61b628e4

---
+++ lib/ar/crypt.php
@@ -0,0 +1,23 @@
+<?php
+	ar_pinp::allow( 'ar_crypt' );
+	
+	class ar_crypt extends arBase {
+		private $key;
+		
+		public function __construct( $key = '', $encoding = MCRYPT_RIJNDAEL_256 ) {
+			global $AR;
+			if ($key == '') {
+				$key = $AR->sgSalt;
+			}
+			$this->key = $key;
+			$this->encoding = $encoding;
+		}
+		
+		public function crypt( $value ) {
+			return base64_encode(@mcrypt_encrypt($this->encoding, $this->key, $value, MCRYPT_MODE_CBC));
+		}
+		public function decrypt( $value ) {
+			return trim(@mcrypt_decrypt($this->encoding, $this->key, base64_decode($value), MCRYPT_MODE_CBC), "\0");
+		}
+	}
+?>
