21dbfee6a9427a1c99b9a553fdb2aaa8d3cbb94c
Gijsbert te Riet <gijs@muze.nl>
2011-12-05 11:06:47 +0000

    Backport of 5091: Fixed display of paths, added root option.
    git-svn-id: https://svn.muze.nl/svn/ariadne-php5/branches/2.7-fixes@5124 7882d7ee-46e0-0310-a255-a28a61b628e4

--- lib/templates/pobject/dialog.delete.save.php
+++ lib/templates/pobject/dialog.delete.save.php
@@ -3,6 +3,11 @@
 
 	$ARCurrent->nolangcheck=true;
 	if ($this->CheckLogin("delete") && $this->CheckConfig()) {
+		$root = $this->getvar("root") ? $this->getvar("root") : $this->currentsite();
+		if (substr($root, -1) != "/") {
+			$root .= "/";
+		}
+
 		$target = $this->path;
 		if ($this->getvar("childrenonly")) {
 			$query = "object.parent ~= '$target%' order by path DESC";
@@ -27,8 +32,14 @@
 		}
 
 		$current_object_path = $this->path;
+		if (substr($current_object_path, 0, strlen($root)) == $root) {
+			$current_object_path = substr($current_object_path, strlen($root), strlen($current_object_path));
+		}
+
+		$current_object_path = "/" . $current_object_path;
+
 		if (strlen($current_object_path) > 25) {
-			$current_object_path = substr($this->path, 0, 12) . "..." . substr($this->path, strlen($this->path) - 12, strlen($this->path));
+			$current_object_path = substr($this->path, 0, 7) . "..." . substr($this->path, strlen($this->path) - 17, strlen($this->path));
 		}
 ?>
 <div id="deleting"><?php echo $ARnls["deleting"] . " " . $current_object_path; ?></div>
@@ -49,8 +60,13 @@
 
 			$new_objects_left = $this->count_find($target, $query);
 			$next_object_path = current($this->find($target, $query, "system.get.path.phtml", array(), 1, $offset));
+			if (substr($next_object_path, 0, strlen($root)) == $root) {
+				$next_object_path = substr($next_object_path, strlen($root), strlen($next_object_path));
+			}
+			$next_object_path = "/" . $next_object_path;
+		
 			if (strlen($next_object_path) > 25) {
-				$next_object_path = substr($next_object_path, 0, 12) . "..." . substr($next_object_path, strlen($next_object_path) - 12, strlen($next_object_path));
+				$next_object_path = substr($next_object_path, 0, 7) . "..." . substr($next_object_path, strlen($next_object_path) - 17, strlen($next_object_path));
 			}
 
 			$objects_deleted = $objects_left - $new_objects_left;
