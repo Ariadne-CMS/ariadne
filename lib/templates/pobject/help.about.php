6ecd27047c67ffdbe4a72361cb6da5826f40b22f
Gijsbert te Riet <gijs@muze.nl>
2012-01-20 10:13:26 +0000

    Version bump
    git-svn-id: https://svn.muze.nl/svn/ariadne-php5/branches/2.7-fixes@5142 7882d7ee-46e0-0310-a255-a28a61b628e4

--- lib/templates/pobject/help.about.php
+++ lib/templates/pobject/help.about.php
@@ -19,9 +19,9 @@
 			<div id="tabsdata">
 				<div id="data">
 					<div id="version">
-						Version 2.7.6<br>
-						Sep 7; 2011<br><br>
-						&copy; 1998 - 2011 <a href="http://www.muze.nl/" target="_blank">Muze</a>
+						Version 2.7.7<br>
+						Jan 20; 2012<br><br>
+						&copy; 1998 - 2012 <a href="http://www.muze.nl/" target="_blank">Muze</a>
 					</div>
 					<form action="">
 						<div>
