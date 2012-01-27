10d24549330a1a5a11d71df32ba7a1d11f01d668
Auke van Slooten <auke@muze.nl>
2012-01-27 12:47:35 +0000

    from 2.7_fixes 5108: Made the encryption key overrideable.
    git-svn-id: https://svn.muze.nl/svn/ariadne-php5/trunk@5161 7882d7ee-46e0-0310-a255-a28a61b628e4

--- lib/ar/crypt.php
+++ lib/ar/crypt.php
@@ -12,10 +12,15 @@
 			$this->key = $key;
 			$this->encoding = $encoding;
 		}
+
+		public function setKey( $key ) {
+			$this->key = $key;
+		}
 		
 		public function crypt( $value ) {
 			return base64_encode(@mcrypt_encrypt($this->encoding, $this->key, $value, MCRYPT_MODE_CBC));
 		}
+
 		public function decrypt( $value ) {
 			return trim(@mcrypt_decrypt($this->encoding, $this->key, base64_decode($value), MCRYPT_MODE_CBC), "\0");
 		}
