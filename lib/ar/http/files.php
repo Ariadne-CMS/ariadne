c8e106ff0313f42925f2280a8240c94b67ceea27
Robbert Müller <muller@muze.nl>
2012-03-16 14:08:20 +0000

    merging the following commits from trunk ------------------------------------------------------------------------ r5079 | auke | 2011-11-04 10:33:45 +0100 (vr, 04 nov 2011) | 3 lines
    made load and autoload more robust
    ar now implements the arKeyValueStoreInterface
    
    ------------------------------------------------------------------------
    r5080 | auke | 2011-11-04 10:36:08 +0100 (vr, 04 nov 2011) | 2 lines
    
    added generic file access class, only accepts resource pointers so you cannot just open any file in pinp
    
    ------------------------------------------------------------------------
    r5081 | auke | 2011-11-04 10:36:53 +0100 (vr, 04 nov 2011) | 3 lines
    
    added generic way to access and manipulate uploaded files
    added a registry to keep uploaded files in a temporary folder for later access
    
    ------------------------------------------------------------------------
    r5082 | auke | 2011-11-04 10:37:37 +0100 (vr, 04 nov 2011) | 2 lines
    
    added access methods for ariadne's session object
    
    ------------------------------------------------------------------------
    r5083 | auke | 2011-11-04 10:38:27 +0100 (vr, 04 nov 2011) | 2 lines
    
    added routines to read and write csv strings, files and streams
    
    ------------------------------------------------------------------------
    r5084 | auke | 2011-11-04 10:56:28 +0100 (vr, 04 nov 2011) | 2 lines
    
    moved session to loader section
    
    ------------------------------------------------------------------------
    r5085 | auke | 2011-11-04 10:59:58 +0100 (vr, 04 nov 2011) | 2 lines
    
    fixed reference to ar session.
    
    ------------------------------------------------------------------------
    
    
    git-svn-id: https://svn.muze.nl/svn/ariadne-php5/branches/2.7-fixes@5188 7882d7ee-46e0-0310-a255-a28a61b628e4

---
+++ lib/ar/http/files.php
@@ -0,0 +1,107 @@
+<?php
+
+	ar_pinp::allow( 'ar_http_files');
+	ar_pinp::allow( 'ar_http_filesRegistry');
+	
+	class ar_http_files extends arBase {
+	
+		protected static $store = null;
+	
+		protected static function getStore( $store = null ) {
+			if ( !$store ) {
+				$store = self::$store;
+			}
+			if ( !$store ) {
+				self::$store =  ar('loader/session')->get();
+				if ( !self::$store ) {
+					self::$store = ar('loader/session')->start();
+				}
+				$store = self::$store;
+			}
+			return $store;
+		}
+	
+		public static function ls() {
+			return $_FILES;
+		}
+		
+		public static function getfile( $filename ) {
+			// return opened file if exists, null otherwise
+			$file = null;
+			$info = $_FILES[$filename];
+			if ( $info && is_uploaded_file( $info['tmp_name'] ) ) {
+				$fp = fopen( $info['tmp_name'], 'r+b' );
+				$file = new ar_content_filesFile( $fp );
+			}
+			return $file;
+		}		
+
+		public static function registry( $store = null ) {
+			$store = self::getStore( $store );
+			return new ar_http_filesRegistry( $store );
+		}
+		
+	}
+	
+	class ar_http_filesRegistry extends arBase {
+
+		protected $store = null;
+		
+		public function __construct( $store ) { 
+			$this->store = $store;
+		}
+		
+		public function ls() {
+			return $this->store->getvar('registeredFiles');
+		}
+		
+		public function getfile( $filename, $nls = 'none' ) {
+			$registeredFiles = $this->ls();
+			$info = $registeredFiles[$filename][$nls];
+			if ($info && $info[$filename.'_temp']) {
+				// check before removing
+				$tempfile = preg_replace( "|[\\\/]|", "", $info[$filename.'_temp'] );
+				$tempOb = ar::context()->getObject();
+				$tempfile = $tempOb->store->get_config('files').'temp/'.$tempfile;
+				$resource = fopen( $tempfile, 'r+b' );
+				return new ar_content_filesFile( $resource );
+			} else {
+				return null;
+			}
+		}
+		
+		public function putfile( $filename, $nls = 'none') {
+			$registeredFiles = (array) $this->store->getvar('registeredFiles');
+			$error = '';
+			$fileInfo = ldRegisterFile( $filename, $error );
+			if (!$error) {
+				$registeredFiles[ $filename ][ $nls ] = $fileInfo;
+				$this->store->putvar('registeredFiles', $registeredFiles );
+				$result = $fileInfo;
+			} else {
+				$result = ar('error')->raiseError( $error, 501 );
+			}		
+			return $result;
+		}
+		
+		public function remove( $filename, $nls = 'none') {
+			$registeredFiles = $this->ls();
+			$info = $registeredFile[$filename][$nls];
+			unset($registeredFiles[$filename][$nls]);
+			if (!$registeredFiles[$filename]) {
+				unset($registeredFiles[$filename]);
+			}
+			$this->store->putvar('registeredFiles', $registeredFiles );
+			if ($info && $info[$filename.'_temp']) {
+				// check before removing
+				$tempfile = preg_replace( "|[\\\/]|", "", $info[$filename.'_temp'] );
+				$tempOb = ar::context()->getObject();
+				$tempfile = $tempOb->store->get_config('files').'temp/'.$tempfile;
+				if ($tempfile && file_exists($tempfile) ) {
+					unlink($tempfile);
+				}
+			}
+		}
+		
+	}
+?>
---
+++ lib/ar/loader/session.php
@@ -0,0 +1,62 @@
+<?php
+	ar_pinp::allow('ar_loader_session');
+	ar_pinp::allow('ar_loader_sessionStore');
+	
+	class ar_loader_session extends arBase {
+	
+		public static function start() {
+			global $ARCurrent;
+			ldStartSession(0);
+			return new ar_loader_sessionStore( $ARCurrent->session );
+		}
+		
+		public static function get() {
+			global $ARCurrent;
+			if ($ARCurrent->session && $ARCurrent->session->id ) {
+				return new ar_loader_sessionStore( $ARCurrent->session );
+			} else {
+				return null;
+			}
+		}
+
+		public static function kill() {
+			global $ARCurrent;
+			if ($ARCurrent->session) {
+				$ARCurrent->session->kill();
+				unset($ARCurrent->session);
+			}
+		}
+		
+	}
+	
+	class ar_loader_sessionStore extends arBase implements arKeyValueStoreInterface {
+	
+		protected $session = null;
+	
+		public function __construct( $session ) {
+			$this->session = $session;
+		}
+		
+		function getvar( $name ) {
+			return $this->session->get( $name );
+		}
+		
+		function putvar( $name, $value ) {
+			return $this->session->put( $name, $value );
+		}
+
+		function save() {
+			return $this->session->save();
+		}
+		
+		function suspend() {
+			return $this->session->suspend();
+		}
+		
+		function kill() {
+			return $this->session->kill();
+		}
+		
+	}
+
+?>
