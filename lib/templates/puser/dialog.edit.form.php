84b6be79c0b2b20f1d65e2eda67a91104309f563
Auke van Slooten <auke@muze.nl>
2012-05-18 12:04:08 +0000

    added option to set login independant of filename of a user
    git-svn-id: https://svn.muze.nl/svn/ariadne-php5/branches/2.7-fixes@5236 7882d7ee-46e0-0310-a255-a28a61b628e4

--- lib/templates/puser/dialog.edit.form.php
+++ lib/templates/puser/dialog.edit.form.php
@@ -17,6 +17,11 @@
 <fieldset id="data">
 	<legend><?php echo $ARnls["data"]; ?></legend>
 	<div class="field">
+		<label for="login" class="required"><?php echo $ARnls["login"]; ?></label>
+		<input id="login" type="text" name="login" 
+			value="<?php $this->showdata("login", "none"); ?>" class="inputline">
+	</div>
+	<div class="field">
 		<label for="name" class="required"><?php echo $ARnls["name"]; ?></label>
 		<input id="name" type="text" name="name" 
 			value="<?php $this->showdata("name", "none"); ?>" class="inputline wgWizAutoFocus">
