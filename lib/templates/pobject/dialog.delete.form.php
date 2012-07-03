638bb0561386b2311ac96c92049d11197a34aa32
Yvo Brevoort <yvo@muze.nl>
2012-07-03 14:46:11 +0000

    Added object name, object icon to the delete dialog
    git-svn-id: https://svn.muze.nl/svn/ariadne-php5/trunk@5256 7882d7ee-46e0-0310-a255-a28a61b628e4

--- lib/templates/pobject/dialog.delete.form.php
+++ lib/templates/pobject/dialog.delete.form.php
@@ -37,11 +37,33 @@
 		$crumbs = htmlspecialchars($crumbs . $nlsdata->name);
 		$oldcrumbs = htmlspecialchars($oldcrumbs);
 
-		
+		if( !$ARCurrent->arTypeTree ) {
+			$this->call('typetree.ini');
+		}
+		$icons = $ARCurrent->arTypeIcons;
+		$names = $ARCurrent->arTypeNames;
+
+		$icon = $ARCurrent->arTypeIcons[$this->type]['medium'] ? $ARCurrent->arTypeIcons[$this->type]['medium'] : $this->call("system.get.icon.php", array('size' => 'medium'));
+
+		$iconalt = $this->type;
+		if( 1 || $this->implements("pshortcut") ) {
+			$overlay_icon = $icon;
+			$overlay_alt = $this->type;
+			$icon = $ARCurrent->arTypeIcons[$this->vtype]['medium'] ? $ARCurrent->arTypeIcons[$this->vtype]['medium'] : current($this->get($this->data->path, "system.get.icon.php", array('size' => 'medium')));
+			$iconalt = $this->vtype;
+		}
 ?>
-<fieldset id="data">
-	<legend><?php echo $ARnls["delete"]; ?> <span class="crumbs" title="<?php echo $oldcrumbs; ?>"><?php echo $crumbs; ?></span></legend>
-	<?php if ($total == 0) {
+<fieldset id="data" class="delete">
+	<legend>
+<?php echo $ARnls["delete"]; ?> <?php echo $names[$this->type] . ": "; echo $nlsdata->name; ?> (<span class="crumbs" title="<?php echo $oldcrumbs; ?>"><?php echo $crumbs; ?></span>)</legend>
+<img src="<?php echo $icon; ?>" alt="<?php echo htmlspecialchars($iconalt); ?>" title="<?php echo htmlspecialchars($iconalt); ?>" class="typeicon">
+	<?php
+		if( $overlay_icon ) {
+	?>
+	<img src="<?php echo $overlay_icon; ?>" alt="<?php echo htmlspecialchars($overlay_alt); ?>" title="<?php echo htmlspecialchars($overlay_alt); ?>" class="overlay_typeicon">
+	<?php
+		}
+	?>	<?php if ($total == 0) {
 		echo $ARnls["q:removeobject"];
 	} else { ?>
 		<span class="crumbs" title="<?php echo $oldcrumbs; ?>">
