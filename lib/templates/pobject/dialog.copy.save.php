9ad997aae97ae2cdb5a2eac74a79a345aa0abaca
Auke van Slooten <auke@muze.nl>
2012-11-27 07:59:11 +0000

    restyled delete dialog added better javascript checks for the delete.save and copy.save.dialog so they are more usable outside ariadne's UI
    git-svn-id: https://svn.muze.nl/svn/ariadne-php5/branches/2.7-fixes@5308 7882d7ee-46e0-0310-a255-a28a61b628e4

--- lib/templates/pobject/dialog.copy.save.php
+++ lib/templates/pobject/dialog.copy.save.php
@@ -48,11 +48,6 @@
 			<div id="progress"></div>
 			<div id="progress_text">0/<?php echo $total; ?></div>
 		</div>
-		<div class="buttons">
-		<div class="left">
-		<input unselectable="on" type="submit" name="wgWizControl" class="wgWizControl" onClick="document.wgWizForm.wgWizAction.value='cancel';" value="<?php echo $ARnls['cancel']; ?>">
-		</div>
-		</div>
 		<?php 
 				while ($offset < $objects_left) {
 					flush();
@@ -109,11 +104,13 @@
 					document.getElementById('progress').style.width = '100%';
 					document.getElementById('copy').innerHTML = '<?php echo $ARnls["copying"] . " " . $current_object_path; ?>';
 					// FIXME: Add copied path to the tree?
-					currentpath = window.opener.muze.ariadne.registry.get('path');
-					if (currentpath == '<?php echo $this->make_path($copytarget . "../"); ?>') {
-						window.opener.muze.ariadne.explore.objectadded();
-					} else {
-						window.opener.muze.ariadne.explore.tree.view('<?php echo $this->make_path($copytarget . "../"); ?>');
+					if ( window.opener && window.opener.muze && window.opener.muze.ariadne && window.opener.muze.ariadne.registry ) {
+						currentpath = window.opener.muze.ariadne.registry.get('path');
+						if (currentpath == '<?php echo $this->make_path($copytarget . "../"); ?>') {
+							window.opener.muze.ariadne.explore.objectadded();
+						} else {
+							window.opener.muze.ariadne.explore.tree.view('<?php echo $this->make_path($copytarget . "../"); ?>');
+						}
 					}
 					window.close();
 				</script>
--- lib/templates/pobject/dialog.delete.form.php
+++ lib/templates/pobject/dialog.delete.form.php
@@ -9,22 +9,21 @@
 
 		$query = "object.path =~ '" . $target . "%' order by path DESC";
 		$total = $this->count_find($this->path, $query);
-		$total--; // children of current path.
+		$total--; // children of current path - don't include yourself.
 		$crumbs = '';
 		$path = '';
 		$parents = $this->parents($this->path, 'system.get.name.phtml', '', $root);
 		$parentpaths = $this->parents($this->path, 'system.get.path.phtml', '', $root);
 		$path = array_pop($parentpaths);
 
-		if (substr($path, 0, strlen($root)) == $root) {
-			$path = substr($path, strlen($root), strlen($path));
+		if ( strpos( $path, $root ) === 0 ) {
+			$path = substr( $path, strlen($root)-1 );
 		}
 
-		$path = "/" . $path;
-
 		array_pop($parents); //remove current item from the list.
+		array_shift( $parents ); // remove site root from the list.
 		foreach ($parents as $name) {
-			$crumbs .= $name . " / ";
+			$crumbs .= "/ $name";
 		}
 
 		$oldcrumbs = $crumbs;
@@ -34,26 +33,53 @@
 				$crumbs = $oldcrumbs;
 			}
 		}
-		$crumbs = htmlspecialchars($crumbs . $nlsdata->name);
-		$oldcrumbs = htmlspecialchars($oldcrumbs);
+		$crumbs = htmlspecialchars( $crumbs . "/ " . $nlsdata->name );
+		$oldcrumbs = htmlspecialchars( $oldcrumbs . "/ " . $nlsdata->name );
 
+		if( !$ARCurrent->arTypeTree ) {
+			$this->call('typetree.ini');
+		}
+		$icons = $ARCurrent->arTypeIcons;
+		$names = $ARCurrent->arTypeNames;
+
+		$icon = $ARCurrent->arTypeIcons[$this->type]['medium'] ? $ARCurrent->arTypeIcons[$this->type]['medium'] : $this->call("system.get.icon.php", array('size' => 'medium'));
+
+		$iconalt = $this->type;
+		if ( $this->implements("pshortcut") ) {
+			$overlay_icon = $icon;
+			$overlay_alt = $this->type;
+			if ( $ARCurrent->arTypeIcons[$this->vtype]['medium'] ) {
+				$icon = $ARCurrent->arTypeIcons[$this->vtype]['medium'];
+			} else {
+				$icon = current($this->get($this->data->path, "system.get.icon.php", array('size' => 'medium')));
+			}
+			$iconalt = $this->vtype;
+		}
+		echo '<fieldset id="data" class="delete">';
 		
+		echo '<legend>';
+		echo $ARnls["delete"] . ' ' . $names[$this->type];
+		echo '</legend>';
+		
+		echo '<img src="' . $icon . '" alt="' . htmlspecialchars($iconalt) . '" title="' . htmlspecialchars($iconalt) . '" class="typeicon">';
+		
+		if ( $overlay_icon ) {
+			echo '<img src="' . $overlay_icon . '" alt="' . htmlspecialchars($overlay_alt) . '" title="' . htmlspecialchars($overlay_alt) . '" class="overlay_typeicon">';
+		}
+		echo '<div class="name">' . $nlsdata->name . ' ';
+		echo '( <span class="crumbs" title="' . $oldcrumbs . '">' . $crumbs . '</span> )';
+		echo '</div>';
+		echo '<div class="path">' . $path . '</div>';
+		
+		if ($total == 0) {
+			echo $ARnls["q:removeobject"];
+		} else {
+			echo sprintf( $ARnls['q:removeall'], $nlsdata->name, $total);
+			echo '<div class="field checkbox">';
+			echo '  <input type="checkbox" id="childrenonly" name="childrenonly" value="true">';
+			echo '  <label for="childrenonly">' . $ARnls['ariadne:childrenonly'] . '</label>';
+			echo '</div>';
+		}
+		echo '</fieldset>';
+	}
 ?>
-<fieldset id="data">
-	<legend><?php echo $ARnls["delete"]; ?> <span class="crumbs" title="<?php echo $oldcrumbs; ?>"><?php echo $crumbs; ?></span></legend>
-	<?php if ($total == 0) {
-		echo $ARnls["q:removeobject"];
-	} else { ?>
-		<span class="crumbs" title="<?php echo $oldcrumbs; ?>">
-			<?php echo $crumbs; ?>
-		</span> (<?php echo $path; ?>)
-		<?php echo sprintf($ARnls['q:removeall'], '', $total); ?>
-
-		<div class="field checkbox">
-			<input type="checkbox" id="childrenonly" name="childrenonly" value="true">
-			<label for="childrenonly"><?php echo $ARnls['ariadne:childrenonly']; ?></label> 
-		</div>
-
-	<?php } ?>
-</fieldset>
-<?php } ?>
