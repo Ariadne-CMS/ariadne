1b288cf6b379e3d8449b27fdcc1a2658b8154024
Gijsbert te Riet <gijs@muze.nl>
2012-12-19 09:17:47 +0000

    Rewrite engine available from dialog.
    git-svn-id: https://svn.muze.nl/svn/ariadne-php5/branches/2.7-fixes@5333 7882d7ee-46e0-0310-a255-a28a61b628e4

---
+++ lib/templates/pobject/dialog.rewrite.form.php
@@ -0,0 +1,33 @@
+<?php
+	$ARCurrent->nolangcheck=true;
+	if ($this->CheckSilent("edit") && $this->CheckConfig()) {
+?>
+<fieldset id="url">
+	<legend><?php echo $ARnls["ariadne:rewrite.urls"]; ?></legend>
+	<div class="field">
+		<label for="oldurl" class="required"><?php echo $ARnls["ariadne:rewrite.oldurl"]; ?></label>
+		<input id="oldurl" type="text" name="oldurl" 
+			value="<?php $this->getvar("oldurl"); ?>" class="inputline wgWizAutoFocus">
+	</div>
+	<div class="field">
+		<label for="newurl" class="required"><?php echo $ARnls["ariadne:rewrite.newurl"]; ?></label>
+		<input id="newurl" type="text" name="newurl" 
+			value="<?php $this->getvar("newurl"); ?>" class="inputline">
+	</div>
+</fieldset>
+
+<fieldset id="reference">
+	<legend><?php echo $ARnls["ariadne:rewrite.references"]; ?></legend>
+	<div class="field">
+		<label for="oldreference" class="required"><?php echo $ARnls["ariadne:rewrite.oldreference"]; ?></label>
+		<input id="oldreference" type="text" name="oldreference" 
+			value="<?php $this->getvar("oldreference"); ?>" class="inputline wgWizAutoFocus">
+	</div>
+	<div class="field">
+		<label for="newreference" class="required"><?php echo $ARnls["ariadne:rewrite.newreference"]; ?></label>
+		<input id="newreference" type="text" name="newreference" 
+			value="<?php $this->getvar("newreference"); ?>" class="inputline">
+	</div>
+</fieldset>
+
+<?php } ?>
---
+++ lib/templates/pobject/dialog.rewrite.php
@@ -0,0 +1,41 @@
+<?php
+	$ARCurrent->nolangcheck=true;
+	if ($this->CheckLogin("edit") && $this->CheckConfig()) {
+		include($this->store->get_config("code")."widgets/wizard/code.php");
+
+		$wgWizFlow = array(
+			array(
+				"current" => $this->getvar("wgWizCurrent","none"),
+				"template" => "dialog.rewrite.form.php",
+				"cancel" => "window.close.js",
+				"save" => "dialog.rewrite.save.php"
+			)
+		);
+		
+		if ($wgWizAction == 'save') {
+			$wgWizButtons = array(
+				"cancel" => array(
+					"value" => $ARnls["cancel"]
+				),
+				"template" => array(
+					"value" => $ARnls["back"]
+				)
+			);
+		} else {
+			$wgWizButtons = array(
+				"cancel" => array(
+					"value" => $ARnls["cancel"]
+				),
+				"save" => array(
+					"value" => $ARnls["ariadne:rewrite"]
+				),
+			);
+		}				
+
+		$wgWizTitle=$ARnls["ariadne:rewrite"];
+		$wgWizHeader = $wgWizTitle;
+		$wgWizHeaderIcon = $AR->dir->images.'icons/large/mogrify.png';
+
+		include($this->store->get_config("code")."widgets/wizard/yui.wizard.html");
+	}
+?>
