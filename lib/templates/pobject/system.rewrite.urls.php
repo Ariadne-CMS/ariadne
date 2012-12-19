4d04a07b260f161f50d35c604355d483f9b3bba7
Gijsbert te Riet <gijs@muze.nl>
2012-12-19 09:19:06 +0000

    rewrite system templates.
    git-svn-id: https://svn.muze.nl/svn/ariadne-php5/branches/2.7-fixes@5334 7882d7ee-46e0-0310-a255-a28a61b628e4

---
+++ lib/templates/pobject/system.rewrite.urls.php
@@ -0,0 +1,43 @@
+<?php
+	if ($this->CheckLogin("edit") && $this->CheckConfig()) {
+		$rewrite_urls = $this->getvar('rewrite_urls');
+
+		if (is_array($rewrite_urls)) {
+			$nls_list = $AR->nls->list;
+			$nls_list['none'] = 'None';
+			foreach ($rewrite_urls as $oldURL => $newURL) {
+				if (substr($oldURL, -1) != '/') {
+					$oldURL .= '/';
+				}
+				foreach ($nls_list as $nls => $nls_name) {
+					if ($this->data->$nls) {
+						$nls_fields = array( 'page' , 'summary' );
+						foreach ($nls_fields as $nls_field) {
+							$page = $this->data->$nls->$nls_field;
+							if ($page) {
+								$regexp = '|' . str_replace('|', '\|', $oldURL) . '|';
+								$page = preg_replace($regexp, $newURL, $page);
+								if ($page !== $this->data->$nls->$nls_field) {
+									$this->data->$nls->$nls_field = $page;
+								}
+							}
+						}
+					}
+					if (is_array($this->data->custom[ $nls ])) {
+						foreach ($this->data->custom[ $nls ] as $customField => $page) {
+							if ($page) {
+								$regexp = '|' . str_replace('|', '\|', $oldURL) . '|';
+								$page = preg_replace($regexp, $newURL, $page);
+								if ($page !== $this->data->custom[$nls][$customField]) {
+									$this->data->custom[$nls][$customField] = $page;
+								}
+							}
+						}
+					}
+				}
+			}
+
+			$this->save();
+		}
+	}
+?>
