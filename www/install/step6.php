24587efc13ad212fa828a4a9f88853a83a7ea568
Robbert Müller <muller@muze.nl>
2012-12-23 09:55:04 +0000

    We forgot to include the password hashing algoritm in the installer
    git-svn-id: https://svn.muze.nl/svn/ariadne-php5/trunk@5369 7882d7ee-46e0-0310-a255-a28a61b628e4

--- www/install/step6.php
+++ www/install/step6.php
@@ -94,6 +94,7 @@
 			include("conf/debug.conf");
 			include("conf/outputzip.conf");
 			include("conf/hidesession.conf");
+			include("conf/hashalgorithm.conf");
 			include("conf/ldap.conf");
 			include("conf/auto.conf");
 		$configfile = ob_get_contents();
