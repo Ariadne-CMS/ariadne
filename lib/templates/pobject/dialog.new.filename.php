a7ad6a748f6783ee1458ca90eb7bbe83f98a334a
Yvo Brevoort <yvo@muze.nl>
2013-01-03 14:42:55 +0000

    Added automatic numbering option, removed confusing 5:id default value
    git-svn-id: https://svn.muze.nl/svn/ariadne-php5/trunk@5386 7882d7ee-46e0-0310-a255-a28a61b628e4

--- lib/templates/pobject/dialog.new.filename.php
+++ lib/templates/pobject/dialog.new.filename.php
@@ -2,15 +2,40 @@
 	$ARCurrent->nolangcheck = true;
 	if( $this->CheckLogin("read") && $this->CheckConfig() ) {
 		$fname = $this->getdata("arNewFilename", "none");
-		if( !$fname ) {
-			$fname = "{5:id}";
+
+		$autoNumberChecked = "";
+		$inputDisabled = "";
+		if ($fname == "{5:id}") {
+			$autoNumberChecked = "checked";
+			$inputDisabled = "disabled";
+			$fname = "";
 		}
 ?>
+<script type="text/javascript">
+	function toggleAutoNumber() {
+		var checked = document.getElementById("autonumber").checked;
+		if (checked) {
+			document.getElementById("arNewFilename").disabled = true;
+		} else {
+			document.getElementById("arNewFilename").disabled = false;
+		}
+	}
+</script>
 <fieldset id="data">
 	<legend><?php echo $ARnls["filename"]; ?></legend>
-	<div class="field">
-		<label for="arNewFilename" class="required"><?php echo $ARnls["filename"]; ?></label>
-		<input id="arNewFilename" class="inputline wgWizAutoSelect wgWizAutoFocus" type="text" name="arNewFilename" value="<?php echo $fname ?>">
+	<div class="left">
+		<div class="field">
+			<label for="arNewFilename" class="required"><?php echo $ARnls["filename"]; ?></label>
+			<input id="arNewFilename" class="inputline wgWizAutoSelect wgWizAutoFocus" <?php echo $inputDisabled; ?> type="text" name="arNewFilename" value="<?php echo $fname ?>">
+			<div class="help"><?php echo $ARnls['ariadne:new:lettersnumbers']; ?></div>
+		</div>
+	</div>
+	<div class="right">
+		<label for="autonumber">&nbsp;</label>
+		<div class="field checkbox">
+			<input onchange="toggleAutoNumber();" onclick="toggleAutoNumber();" <?php echo $autoNumberChecked; ?> type="checkbox" name="arNewFilename" value="{5:id}" id="autonumber" <?php if ($fname == "{5:id}") { echo "checked"; }?>>
+			<label for="autonumber"><?php echo $ARnls["ariadne:new:autonumber"]; ?></label>
+		</div>
 	</div>
 </fieldset>
 <?php
