2f3766a07028ebe1c637ca95aa417e63133e6d46
Robbert Müller <muller@muze.nl>
2013-01-09 15:31:22 +0000

    disable workspace checkbox for database servers other then mysql
    git-svn-id: https://svn.muze.nl/svn/ariadne-php5/trunk@5406 7882d7ee-46e0-0310-a255-a28a61b628e4

--- www/install/step5.php
+++ www/install/step5.php
@@ -50,14 +50,18 @@
 					</div>
 					<div class="field checkbox">
 						<?php
-							if ($enable_workspaces) {
+							$disabled = '';
+							if ($database != "mysql") {
+								$checked = '';
+								$disabled = "disabled";
+							} elseif ($enable_workspaces) {
 								$checked = "checked='checked'";
 							} else {
 								$checked = '';
 							}
 						?>
 						<input type="hidden" name="enable_workspaces" value="0">
-						<input <?php echo $checked; ?> type="checkbox" id="enable_workspaces" name="enable_workspaces" value="1">
+						<input <?php echo $checked . ' ' . $disabled; ?> type="checkbox" id="enable_workspaces" name="enable_workspaces" value="1">
 						<label for='enable_workspaces'><?php echo $ARnls['install:enable_workspaces']; ?></label>
 					</div>
 					<?php if (check_demo_ax()) { ?>
