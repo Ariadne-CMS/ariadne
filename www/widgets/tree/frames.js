00299a319e19511767fb59077e69c687e3887956
Robbert Müller <muller@muze.nl>
2013-01-09 15:31:38 +0000

    remove legacy code
    git-svn-id: https://svn.muze.nl/svn/ariadne-php5/trunk@5410 7882d7ee-46e0-0310-a255-a28a61b628e4

--- www/widgets/tree/frames.js
+++
@@ -1,5 +0,0 @@
-  <frameset rows="30,*,30" BORDER="0" FRAMEBORDER="no" FRAMESPACING="0">
-    <frame name="treeload" src="<?php echo $loader.$path; ?>tree.init.phtml" scrolling="no" marginwidth="0" marginheight="0" BORDER="0" FRAMEBORDER="no" FRAMESPACING="0">
-    <frame name="treeview" src="white.html" scrolling="auto" marginwidth="1" marginheight="1" BORDER="0" FRAMEBORDER="no" FRAMESPACING="0">
-    <frame name="treecontrol" src="control.php?arUserPath=<?php echo $loader.$path; ?>&interface=<?php echo $interface; ?>" scrolling="no" marginwidth="0" marginheight="0" BORDER="0" FRAMEBORDER="no" FRAMESPACING="0">
-  </frameset>
