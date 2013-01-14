4efbb81d9bcd8b464a072a73bc81998733e21773
Yvo Brevoort <yvo@muze.nl>
2013-01-14 14:18:46 +0000

    Fixes bug #247: enter key resets priority value
    git-svn-id: https://svn.muze.nl/svn/ariadne-php5/trunk@5425 7882d7ee-46e0-0310-a255-a28a61b628e4

--- lib/templates/pobject/dialog.priority.form.php
+++ lib/templates/pobject/dialog.priority.form.php
@@ -1,12 +1,19 @@
 <?php
 	$ARCurrent->nolangcheck=true;
 	if( $this->CheckLogin("edit") && $this->CheckConfig() ) {
+		$priority = $this->getvar("priority");
+
+		if (!isset($priority)) {
+			$priority = $this->priority;
+		} else {
+			$priority = (int)$priority;
+		}
 ?>
 	<fieldset id="data">
 			<legend><?php echo $ARnls["priority"]; ?></legend>
 			<div class="field">
 				<label for="priority" class="required"><?php echo $ARnls["priority"]; ?></label>
-				<input id="priority" type="text" name="priority" value="<?php echo $this->priority; ?>" class="inputline wgWizAutoFocus">
+				<input id="priority" type="text" name="priority" value="<?php echo $priority; ?>" class="inputline wgWizAutoFocus">
 			</div>
 	</fieldset>
 <?php
