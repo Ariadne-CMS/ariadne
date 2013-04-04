578c5ad9f6e62d5aa4fe36eddc362cb5476cd231
Auke van Slooten <auke@muze.nl>
2013-04-04 12:51:33 +0000

    added check for webkit config
    git-svn-id: https://svn.muze.nl/svn/ariadne-php5/trunk@5584 7882d7ee-46e0-0310-a255-a28a61b628e4

--- lib/modules/mod_webkit.php
+++ lib/modules/mod_webkit.php
@@ -16,12 +16,14 @@
 		function toPng( $url ) {
 			global $AR;
 			$image = false;
-			$url = ar::url($url);
-			if ( $url->host ) { // only allow remote fetches
-				$cmd = $AR->Webkit2png->xvfbPath . ' ' . $AR->Webkit2png->xvfbOptions 
-				. ' ' . $AR->Webkit2png->path . ' ' . $AR->Webkit2png->options . ' ' . escapeshellarg( (string) $url ); 
-				//echo $cmd;
-				$image = shell_exec( $cmd );
+			if ( $AR->Webkit2png->path ) {
+				$url = ar::url($url);
+				if ( $url->host ) { // only allow remote fetches
+					$cmd = $AR->Webkit2png->xvfbPath . ' ' . $AR->Webkit2png->xvfbOptions 
+					. ' ' . $AR->Webkit2png->path . ' ' . $AR->Webkit2png->options . ' ' . escapeshellarg( (string) $url ); 
+					//echo $cmd;
+					$image = shell_exec( $cmd );
+				}
 			}
 			return $image;
 		}
