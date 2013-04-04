220dd3fc674a6b4def9f68b000bab2b7a75cf8e1
Auke van Slooten <auke@muze.nl>
2013-04-04 12:48:20 +0000

    experimental module to allow access to webkit2png and later perhaps webkit2pdf / wkhtml2pdf
    git-svn-id: https://svn.muze.nl/svn/ariadne-php5/trunk@5582 7882d7ee-46e0-0310-a255-a28a61b628e4

---
+++ lib/modules/mod_webkit.php
@@ -0,0 +1,31 @@
+<?php
+	/*
+	
+	*/
+
+	class pinp_webkit {
+
+		function _toPng($url) {
+			return webkit::toPng($url);
+		}
+
+	}
+
+	class webkit {
+
+		function toPng( $url ) {
+			global $AR;
+			$image = false;
+			$url = ar::url($url);
+			if ( $url->host ) { // only allow remote fetches
+				$cmd = $AR->Webkit2png->xvfbPath . ' ' . $AR->Webkit2png->xvfbOptions 
+				. ' ' . $AR->Webkit2png->path . ' ' . $AR->Webkit2png->options . ' ' . escapeshellarg( (string) $url ); 
+				//echo $cmd;
+				$image = shell_exec( $cmd );
+			}
+			return $image;
+		}
+
+	}
+
+?>
