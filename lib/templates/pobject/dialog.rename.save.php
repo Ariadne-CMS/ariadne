71311b538f73535fbb015f7a93e3cef260ba4414
Yvo Brevoort <yvo@muze.nl>
2013-06-07 07:10:19 +0000

    Added check for existing parent and correct error message if that is not the case.
    git-svn-id: https://svn.muze.nl/svn/ariadne-php5/trunk@5685 7882d7ee-46e0-0310-a255-a28a61b628e4

--- lib/templates/pobject/dialog.rename.save.php
+++ lib/templates/pobject/dialog.rename.save.php
@@ -11,41 +11,45 @@
 
 		$target_parent_path = $this->make_path($target . "../");
 
-		$target_ob = current($this->get($target_parent_path, "system.get.phtml"));
-		$target_typetree = $target_ob->call("typetree.ini");
-		if (
-			$target_typetree[$target_ob->type][$this->type] || 	// The object is allowed under the target by the typetree.
-			($target_parent_path == $this->parent) || 				// The object is not moved, just gets a new filename
-			($this->CheckSilent('layout') && $this->getvar('override_typetree')) // Layout grant allows typetree changes, so allow typetree overrides as well.
-		) {
-			// This type is allowed.
-		
-			$folder=$this->call("system.get.folder.phtml");
-			$oldlocation=$this->path;
-			$this->call("system.rename.phtml", $arCallArgs); // system.rename will fix the path by itself and do more checks, so do not pass $target to it from here.
-			if (!$this->error) {
-				// FIXME: update the tree and explore window.
-				?>
-					<script type="text/javascript">
-						if ( window.opener && window.opener.muze && window.opener.muze.dialog ) {
-							window.opener.muze.dialog.callback( window.name, 'renamed', { 
-								'path': '<?php echo $this->path; ?>'
-							});
-						} else {
-							// FIXME: Add copied path to the tree?
-							if ( window.opener && window.opener.muze && window.opener.muze.ariadne  ) {
-								window.opener.muze.ariadne.explore.tree.refresh('<?php echo $this->path; ?>');
-								window.opener.muze.ariadne.explore.view('<?php echo $this->path; ?>');
-							}
-							window.close();
-						}
-					</script>
-				<?php
-			} else {
-				echo "<font color='red'>$this->error</font>";
-			}
+		if (!$this->exists($target_parent_path)) {
+			$this->error=sprintf($ARnls["err:noparentcreatefirst"],$target_parent_path);			
 		} else {
-			echo $ARnls['err:typetree_does_not_allow'];
+			$target_ob = current($this->get($target_parent_path, "system.get.phtml"));
+			$target_typetree = $target_ob->call("typetree.ini");
+		
+			if (
+				$target_typetree[$target_ob->type][$this->type] || 	// The object is allowed under the target by the typetree.
+				($target_parent_path == $this->parent) || 				// The object is not moved, just gets a new filename
+				($this->CheckSilent('layout') && $this->getvar('override_typetree')) // Layout grant allows typetree changes, so allow typetree overrides as well.
+			) {
+				// This type is allowed.
+			
+				$folder=$this->call("system.get.folder.phtml");
+				$oldlocation=$this->path;
+				$this->call("system.rename.phtml", $arCallArgs); // system.rename will fix the path by itself and do more checks, so do not pass $target to it from here.
+				if (!$this->error) {
+					// FIXME: update the tree and explore window.
+					?>
+						<script type="text/javascript">
+							if ( window.opener && window.opener.muze && window.opener.muze.dialog ) {
+								window.opener.muze.dialog.callback( window.name, 'renamed', { 
+									'path': '<?php echo $this->path; ?>'
+								});
+							} else {
+								// FIXME: Add copied path to the tree?
+								if ( window.opener && window.opener.muze && window.opener.muze.ariadne  ) {
+									window.opener.muze.ariadne.explore.tree.refresh('<?php echo $this->path; ?>');
+									window.opener.muze.ariadne.explore.view('<?php echo $this->path; ?>');
+								}
+								window.close();
+							}
+						</script>
+					<?php
+				}
+			}
+		}
+		if ($this->error) {
+			echo "<font color='red'>$this->error</font>";
 		}
 	}
 ?>
