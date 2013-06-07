a38108c1bfa557bedf3e2bc887ad5d715e73d42f
Robbert Müller <muller@muze.nl>
2013-06-07 13:40:51 +0000

    updated nls update script
    git-svn-id: https://svn.muze.nl/svn/ariadne-php5/trunk@5689 7882d7ee-46e0-0310-a255-a28a61b628e4

--- lib/nls/update.php
+++ lib/nls/update.php
@@ -1,62 +1,50 @@
-#!/usr/bin/php4 -q
+#!/usr/bin/php5 -q
 <?php
-  $ariadne="../";
-  include($ariadne."configs/ariadne.phtml");
-  include($ariadne."configs/store.phtml");
-  include($ariadne."includes/loader.web.php");
+	$ariadne="../";
+	include($ariadne."configs/ariadne.phtml");
+	include($ariadne."configs/store.phtml");
+	include($ariadne."includes/loader.web.php");
 
-  $languages=Array('nl','de','es','pl','hr','it','fr','sv', 'en');
-  $modules=Array('','ariadne.');
-  $target='./new/';
-  define('NLSESCAPE', "\"\\\n\r");
+	$languages=Array('nl','de','es','pl','hr','it','fr','sv', 'en');
+	$modules=Array('','ariadne.');
+	$target='./new/';
+	define('NLSESCAPE', "\"\\\n\r");
 
-  ob_start();
+	reset($modules);
+	foreach ($modules as $module){
+		include($module."en");
+		$default=$ARnls;
+		ksort($default);
 
+		unset($ARnls);
 
-  reset($modules);
-  while (list($key1, $module)=each($modules)) {
-    include($module."en");  
-    $default=$ARnls;
-    ksort($default);
-    unset($ARnls);
-    reset($languages);
-    while( list($key2, $language)=each($languages)) {
-//      echo "Updating ".$module.$language."\n";
-      include($module.$language);
-      reset($default);
+		foreach ($languages as $language) {
+			echo "Updating ".$module.$language."\n";
+			include($module.$language);
 
+			$content = "<?"."php\n\n";
+			if ( file_exists($module.$language.".head") ) {
+				$content .= file_get_contents($module.$language.".head");
+			}
+			foreach ($default as $arkey => $arvalue ) {
+				$tabs=substr("								",(int)((strlen($arkey)+2)/4));
+				if (isset($ARnls[$arkey])) {
+					$content .= sprintf("\t%-47s =    \"%s\";\n", "\$ARnls[\"$arkey\"]", AddCSlashes($ARnls[$arkey], NLSESCAPE));
+				} else {
+					$content .= sprintf("//\t%-47s =    \"%s\"; // value from english\n", "\$ARnls[\"$arkey\"]", AddCSlashes($arvalue, NLSESCAPE));
+				}
+			}
+			unset($ARnls);
+			$content .= "\n\n?".">";
 
+			$filename=$target.$module.$language;
+			// write file
+			$result = file_put_contents($filename,$content);
+			if ( $result === false ) {
+				die("ERROR: couldn't open $filename for writing.\n");
+			}
 
-      echo "<?"."php\n\n";
-      @include($module.$language.".head");
-      while (list($arkey, $arvalue)=each($default)) {
-        $tabs=substr("								",(int)((strlen($arkey)+2)/4));
-        if ($ARnls[$arkey]) {
-          echo "	\$ARnls[\"$arkey\"]$tabs=	\"".AddCSlashes($ARnls[$arkey], NLSESCAPE)."\";\n";
-        } else {
-          echo "	\$ARnls[\"$arkey\"]$tabs=	\"!".AddCSlashes($arvalue, NLSESCAPE)."\";\n";
-        }
-      }
-      unset($ARnls);
-      echo "\n\n?".">";
-
-      $file=ob_get_contents();
-      ob_clean();
-
-      $filename=$target.$module.$language;
-      // write file
-	  if ($fp=fopen($filename, 'w')) {
-        fwrite($fp, $file);
-        fclose($fp);
-      } else {
-        die("ERROR: couldn't open $filename for writing.\n");
-      }
-
-    }
-    unset($default);
-  }
-
-  ob_end_clean();
-
+		}
+	}
 
 ?>
