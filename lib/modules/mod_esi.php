428386b354674d8eff1efbe8c9d87efcad49ed0a
Yvo Brevoort <yvo@muze.nl>
2013-07-23 13:22:32 +0000

    Added vars, comment, remove, marker replacement
    git-svn-id: https://svn.muze.nl/svn/ariadne-php5/trunk@5777 7882d7ee-46e0-0310-a255-a28a61b628e4

--- lib/modules/mod_esi.php
+++ lib/modules/mod_esi.php
@@ -31,7 +31,34 @@ class ESI {
 		return $result;
 	}
 
+	function esiRemove($page) {
+		$regExp = '|<esi:remove>(.*)</esi:remove>|Uis';
+		return preg_replace($regExp, "", $page);
+	}
+
+	function esiComment($page) {
+		$regExp = '|<esi:comment[^>]*>|Uis';
+		return preg_replace($regExp, "", $page);
+	}
+
+	function esiMarker($page) {
+		$regExp = '|<!--esi(.*)-->|Uis';
+		return preg_replace($regExp, '$1', $page);
+	}
+
+	function esiVars($page) {
+		$regExp = '|<esi:vars>(.*)</esi:vars>|Uis';
+		$page = preg_replace_callback($regExp, function($matches){
+			return ESI::esiExpression($matches[1]);
+		}, $page);
+		return $page;
+	}
+
 	function esiInclude($page) {
+		/* TODO:
+			alt
+			onerror
+		*/
 		global $ARCurrent, $AR;
 
 		// parse <esi:include src="view.html">
@@ -63,6 +90,29 @@ class ESI {
 				$page = str_replace($match, $result, $page);
 			}
 		}
+
+		return $page;
+	}
+
+	function esiProcess($page) {
+		/*
+			TODO:
+				inline
+				choose/when/otherwise
+				try/attempt/except
+
+				[v] include
+				[v] comment
+				[v] remove
+				[v] vars
+				[v] <!-- esi -->
+		*/
+
+		$page = ESI::esiMarker($page);
+		$page = ESI::esiInclude($page);
+		$page = ESI::esiVars($page);
+		$page = ESI::esiRemove($page);
+		$page = ESI::esiComment($page);
 
 		return $page;
 	}
