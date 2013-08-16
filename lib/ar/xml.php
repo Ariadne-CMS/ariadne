7e44a46bdf9c2900b3e03416e8fa2dad74b364a6
Auke van Slooten <auke@muze.nl>
2013-08-16 08:57:16 +0000

    removed option to automatically parse strings as xml it removes encoded characters or removes the encoding this may break other code...
    git-svn-id: https://svn.muze.nl/svn/ariadne-php5/trunk@5788 7882d7ee-46e0-0310-a255-a28a61b628e4

--- lib/ar/xml.php
+++ lib/ar/xml.php
@@ -367,11 +367,11 @@
 			if ( is_array($nodes) || $nodes instanceof Traversable ) {
 				foreach ( $nodes as $node ) {
 					if ( !$node instanceof ar_xmlNodeInterface ) {
-						if ( ( strpos( '<', (string)$node )===false ) ) {
+						//if ( ( strpos( (string)$node, '<' )===false ) ) {
 							$node = new ar_xmlNode( $node );
-						} else {
-							$node = $this->_tryToParse( $node );
-						}
+						//} else {
+						//	$node = $this->_tryToParse( $node );
+						//}
 					}
 					if ( is_array($node) || $node instanceof Traversable ) {
 						$subnodes = $this->_normalizeNodes( $node );
