61c7fb0e5b760f26f0a1c560b0e868ffc4b45a64
Yvo Brevoort <yvo@muze.nl>
2013-09-20 07:34:40 +0000

    Provide more visual feedback about the uploaded file
    git-svn-id: https://svn.muze.nl/svn/ariadne-php5/trunk@5805 7882d7ee-46e0-0310-a255-a28a61b628e4

--- lib/templates/pfile/dialog.new.upload.php
+++ lib/templates/pfile/dialog.new.upload.php
@@ -15,16 +15,29 @@
 		position: absolute;
 		top: 0px;
 		left: 0px;
-		width: 300px;
-		height: 32px;
+		width: 100%;
+		height: 100%;
 		background-color: #eeeeee;
-		padding-top: 20px;
 		border: 1px solid #cccccc;
 		display: none;
 		text-align: center;
 	}
-	#data {
+	#busy span {
+		display: inline-block;
+		height: 20px;
+		position: absolute;
+		top: 50%;
+		margin-top: -10px;
+		text-align: center;
+		left: 0px;
+		width: 100%;
+	}
+	#tabsdata label.fileinfo {
+		margin-top: 0px;
+	}
+	#tabsdata #data {
 		position: relative;
+		margin-bottom: 10px;
 	}
 </style>
 <script type="text/javascript">
@@ -35,13 +48,26 @@
 </script>
 <fieldset id="data">
 	<legend><?php echo $ARnls["file"]; ?></legend>
-	<div class="field">
-		<label for="file" class="required"><?php echo $ARnls["file"]; ?></label>
-		<img class="flag" src="<?php echo $flagurl; ?>" alt="<?php echo $selectedlanguage; ?>">
-		<input id="file" type="file" value="<?php echo $this->getvar($selectednls . "[file]"); ?>" name="<?php echo $selectednls."[file]"; ?>" class="inputline wgWizAutoFocus" onchange="this.form.submit();">
+	<div class="field fileinfo">
+		<?php   if ($this->getdata('file_temp', $selectednls)) { ?>
+			<div class="left">
+				<label for="file" class="fileinfo">
+					<?php echo $this->getdata('file', $selectednls); ?><br>
+					<?php echo $this->make_filesize($this->getdata('file_size', $selectednls)); ?>
+					<br><br>
+				</label>
+			</div>
+			<div class="right">
+				<img class="flag" src="<?php echo $flagurl; ?>" alt="<?php echo $selectedlanguage; ?>">
+				<input id="file" type="file" value="<?php echo $this->getvar($selectednls . "[file]"); ?>" name="<?php echo $selectednls."[file]"; ?>" class="inputline wgWizAutoFocus" onchange="document.getElementById('busy').style.display = 'block'; this.form.submit();">
+			</div>
+		<?php	} else { ?>
+			<img class="flag" src="<?php echo $flagurl; ?>" alt="<?php echo $selectedlanguage; ?>">
+			<input id="file" type="file" value="<?php echo $this->getvar($selectednls . "[file]"); ?>" name="<?php echo $selectednls."[file]"; ?>" class="inputline wgWizAutoFocus" onchange="document.getElementById('busy').style.display = 'block'; this.form.submit();">
+		<?php	} ?>
 		<!-- input type="submit" name="upload" value="Upload" -->
 	</div>
-	<div id="busy"><?php echo $ARnls["ariadne:uploading"]; ?>...</div>
+	<div id="busy"><span><?php echo $ARnls["ariadne:uploading"]; ?>...</span></div>
 </fieldset>
 <?php
 	}
