3210eba35f65bf6231d9f03ff41ce48a01c38d51
Yvo Brevoort <yvo@muze.nl>
2013-11-05 20:44:40 +0000

    Added form secret validation
    git-svn-id: https://svn.muze.nl/svn/ariadne-php5/trunk@5848 7882d7ee-46e0-0310-a255-a28a61b628e4

--- lib/templates/puser/dialog.preferences.save.php
+++ lib/templates/puser/dialog.preferences.save.php
@@ -1,5 +1,9 @@
 <?php
 	$ARCurrent->nolangcheck=true;
+	if (!$this->validateFormSecret()) {
+		error($ARnls['ariadne:err:invalidsession']);      
+		exit;
+	}
 	$this->call("system.save.preferences.phtml");
 	if (!$this->error) {
 		$this->call("window.close.js");
