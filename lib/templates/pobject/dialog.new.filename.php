93c6a65910c05e151fdd9dcf9654b7d8156e440a
Robbert Müller <muller@muze.nl>
2013-12-06 14:00:26 +0000

    Added event in filename bit to allow different settings per site - this allows automatic numbering on particles
    git-svn-id: https://svn.muze.nl/svn/ariadne-php5/branches/8.2@5903 7882d7ee-46e0-0310-a255-a28a61b628e4

--- lib/templates/pobject/dialog.new.filename.php
+++ lib/templates/pobject/dialog.new.filename.php
@@ -2,42 +2,48 @@
 	$ARCurrent->nolangcheck = true;
 	if( $this->CheckLogin("read") && $this->CheckConfig() ) {
 		$fname = $this->getdata("arNewFilename", "none");
+		$result = ar('events')->fire('dialog.new.filename', array('fname' => $fname));
+		if ($result['fname']) {
+			$fname = $result['fname'];
+		}
 
-		$autoNumberChecked = "";
-		$inputDisabled = "";
-		if ($fname == "{5:id}") {
-			$autoNumberChecked = "checked";
-			$inputDisabled = "disabled";
-			$fname = "";
+		if ($result !== false) {
+			$autoNumberChecked = "";
+			$inputDisabled = "";
+			if ($fname == "{5:id}") {
+				$autoNumberChecked = "checked";
+				$inputDisabled = "disabled";
+				$fname = "";
+			}
+	?>
+	<script type="text/javascript">
+		function toggleAutoNumber() {
+			var checked = document.getElementById("autonumber").checked;
+			if (checked) {
+				document.getElementById("arNewFilename").disabled = true;
+			} else {
+				document.getElementById("arNewFilename").disabled = false;
+			}
 		}
-?>
-<script type="text/javascript">
-	function toggleAutoNumber() {
-		var checked = document.getElementById("autonumber").checked;
-		if (checked) {
-			document.getElementById("arNewFilename").disabled = true;
-		} else {
-			document.getElementById("arNewFilename").disabled = false;
-		}
-	}
-</script>
-<fieldset id="data">
-	<legend><?php echo $ARnls["filename"]; ?></legend>
-	<div class="right">
-		<label for="autonumber">&nbsp;</label>
-		<div class="field checkbox">
-			<input onchange="toggleAutoNumber();" onclick="toggleAutoNumber();" <?php echo $autoNumberChecked; ?> type="checkbox" name="arNewFilename" value="{5:id}" id="autonumber" <?php if ($fname == "{5:id}") { echo "checked"; }?>>
-			<label for="autonumber"><?php echo $ARnls["ariadne:new:autonumber"]; ?></label>
+	</script>
+	<fieldset id="data">
+		<legend><?php echo $ARnls["filename"]; ?></legend>
+		<div class="right">
+			<label for="autonumber">&nbsp;</label>
+			<div class="field checkbox">
+				<input onchange="toggleAutoNumber();" onclick="toggleAutoNumber();" <?php echo $autoNumberChecked; ?> type="checkbox" name="arNewFilename" value="{5:id}" id="autonumber" <?php if ($fname == "{5:id}") { echo "checked"; }?>>
+				<label for="autonumber"><?php echo $ARnls["ariadne:new:autonumber"]; ?></label>
+			</div>
 		</div>
-	</div>
-	<div class="left">
-		<div class="field">
-			<label for="arNewFilename" class="required"><?php echo $ARnls["filename"]; ?></label>
-			<input id="arNewFilename" class="inputline wgWizAutoSelect wgWizAutoFocus" <?php echo $inputDisabled; ?> type="text" name="arNewFilename" value="<?php echo $fname ?>">
-			<div class="help"><?php echo $ARnls['ariadne:new:lettersnumbers']; ?></div>
+		<div class="left">
+			<div class="field">
+				<label for="arNewFilename" class="required"><?php echo $ARnls["filename"]; ?></label>
+				<input id="arNewFilename" class="inputline wgWizAutoSelect wgWizAutoFocus" <?php echo $inputDisabled; ?> type="text" name="arNewFilename" value="<?php echo $fname ?>">
+				<div class="help"><?php echo $ARnls['ariadne:new:lettersnumbers']; ?></div>
+			</div>
 		</div>
-	</div>
-</fieldset>
+	</fieldset>
 <?php
+		}
 	}
 ?>
