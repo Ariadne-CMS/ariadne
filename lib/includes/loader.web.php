5e1b9ed92d30bb9c16d5c041ba448ddfd2a3d334
Yvo Brevoort <yvo@muze.nl>
2013-12-17 16:26:33 +0000

    Added error messages for failed uploads
    git-svn-id: https://svn.muze.nl/svn/ariadne-php5/trunk@5927 7882d7ee-46e0-0310-a255-a28a61b628e4

--- lib/includes/loader.web.php
+++ lib/includes/loader.web.php
@@ -93,6 +93,38 @@
 		if ($subfield) {
 			$field = $subfield;
 		}
+
+		if ($http_post_file['error']) {
+			switch ($http_post_file['error']) {
+				case UPLOAD_ERR_INI_SIZE: 
+					$error = $ARnls['err:upload_ini_size']; // "The uploaded file exceeds the upload_max_filesize directive in php.ini"; 
+					break; 
+				case UPLOAD_ERR_FORM_SIZE: 
+					$error = $ARnls['err:upload_form_size']; // The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form"; 
+					break; 
+				case UPLOAD_ERR_PARTIAL: 
+					$error = $ARnls['err:upload_partial']; // "The uploaded file was only partially uploaded"; 
+					break; 
+				case UPLOAD_ERR_NO_FILE: 
+					$error = $ARnls['err:upload_no_file']; // No file was uploaded"; 
+					break; 
+				case UPLOAD_ERR_NO_TMP_DIR: 
+					$error = $ARnls['err:upload_no_tmp_dir']; // "Missing a temporary folder"; 
+					break; 
+				case UPLOAD_ERR_CANT_WRITE: 
+					$error = $ARnls['err:upload_cant_write']; // "Failed to write file to disk"; 
+					break; 
+				case UPLOAD_ERR_EXTENSION: 
+					$error = $ARnls['err:upload_extension']; // "File upload stopped by extension"; 
+					break; 
+
+				default: 
+					$error = sprintf($ARnls['err:upload_error'], $http_post_file['error']); // "Unknown upload error %s"; 
+					break; 
+			}
+			return $result;
+		}
+
 		$file_temp = $http_post_file['tmp_name'];
 		$file = $http_post_file['name'];
 		if ($file && is_uploaded_file($file_temp)) {
