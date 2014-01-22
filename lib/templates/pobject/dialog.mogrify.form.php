419bdc13c2106479cd822df88ae479e0d12150a3
Yvo Brevoort <yvo@muze.nl>
2014-01-22 11:04:21 +0000

    Mogrify dialog with support for multiple objects
    git-svn-id: https://svn.muze.nl/svn/ariadne-php5/trunk@6082 7882d7ee-46e0-0310-a255-a28a61b628e4

--- lib/templates/pobject/dialog.mogrify.form.php
+++ lib/templates/pobject/dialog.mogrify.form.php
@@ -1,6 +1,6 @@
 <?php
 	$ARCurrent->nolangcheck=true;
-	if ($this->CheckLogin("edit") && $this->CheckConfig()) {
+//	if ($this->CheckLogin("edit") && $this->CheckConfig()) {
             
             if ($this->getvar('targets')) {
 			$targets = $this->getvar("targets");
@@ -8,12 +8,24 @@
 			   $targets = array($this->path);
 		}
 ?>
-<fieldset id="data" class="mogrify">
+<fieldset id="data" class="browse">
 	<legend><?php echo $ARnls["ariadne:mogrify"]; ?></legend>
             <?php
                 foreach ($targets as $target) {
                     $targetob = current($this->get($target, "system.get.phtml"));
                     $type = $this->getvar("type");
+                    
+                    if (!$targetob->CheckSilent("config")) {
+                        $checkfailed = true;
+                    } else {
+                        $checkfailed = false;
+                    }
+                    
+                    if (!$targetob->can_mogrify()) {
+                        $cantmogrify = true;
+                    } else {
+                        $cantmogrify = false;
+                    }
 
                     if (!$type) {
                         $type = $targetob->type;
@@ -32,6 +44,15 @@
             ?>
             <div class="field">
                 <?php
+                    if($checkfailed){
+                            echo '<div class="checkfailed">';
+                            echo '<span class="error">' . $ARnls["err:nogrants"] .'</span>';
+                        } elseif($cantmogrify){
+                            echo '<div class="checkfailed">';
+                            echo '<span class="error"' . $ARnls["err:cantmogrify"] . '</span>';
+                        } else {
+                            echo '<div>';
+                        }
                     echo '<img src="' . $icon . '" alt="' . htmlspecialchars($iconalt) . '" title="' . htmlspecialchars($iconalt) . '" class="typeicon">';
                         if ( $overlay_icon ) {
                             echo '<img src="' . $overlay_icon . '" alt="' . htmlspecialchars($overlay_alt) . '" title="' . htmlspecialchars($overlay_alt) . '" class="overlay_typeicon">';
@@ -39,7 +60,8 @@
                     echo '<div class="name">' . $targetob->nlsdata->name . ' ';
                     echo "( " . $ARCurrent->arTypeNames[$targetob->type] . " / " . $targetob->type . " )";
                     echo '</div>';
-                    echo '<div class="path">' . $target . '</div><br>';
+                    echo '<div class="path">' . $target . '</div>';
+                    echo '</div>';
                 }
                 ?>
             <label for="target" class="required" style="float: left; width: 65px; margin-top: 5px;"><?php echo $ARnls["ariadne:type"]; ?></label>
@@ -56,5 +78,5 @@
             </select>
         </div>
 </fieldset>
-<?php	} 
+<?php	//} 
 ?>
