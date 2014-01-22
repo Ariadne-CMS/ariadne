3bdbe6bce9c6871a5d50ca99eadeb72b77dd97a8
Yvo Brevoort <yvo@muze.nl>
2014-01-22 11:37:18 +0000

    Added option to svn diff against the server Added server diff dialogs Added server diff before actually running svn update
    git-svn-id: https://svn.muze.nl/svn/ariadne-php5/trunk@6089 7882d7ee-46e0-0310-a255-a28a61b628e4

--- lib/templates/pobject/dialog.svn.templates.serverdiff.php
+++ lib/templates/pobject/dialog.svn.templates.serverdiff.php
@@ -6,10 +6,9 @@
 		$wgWizFlow = array(
 			array(
 				"current" => $this->getdata("wgWizCurrent","none"),
-				"template" => "dialog.svn.templates.update.form.php",
+				"template" => "dialog.svn.templates.serverdiff.form.php",
 				"cancel" => "window.close.js",
-				"diff" => "dialog.svn.templates.serverdiff.save.php",
-				"save" => "dialog.svn.templates.update.save.php"
+				"save" => "dialog.svn.templates.serverdiff.save.php"
 			)
 		);
 
@@ -23,30 +22,20 @@
 					"value" => $ARnls["ok"]
 				)
 			);
-		} else if ($wgWizAction == "diff") {
-			$wgWizStyleSheets = array( $AR->dir->styles."svn.css" );
-			$wgWizButtons = array(
-				"cancel" => array(
-					"value" => $ARnls["cancel"]
-				),
-				"save" => array(
-					"value" => $ARnls["ariadne:svn:update"]
-				),
-			);
 		} else {
 			$wgWizButtons = array(
 				"cancel" => array(
 					"value" => $ARnls["cancel"]
 				),
-				"diff" => array(
-					"value" => $ARnls["ariadne:svn:update"]
+				"save" => array(
+					"value" => $ARnls["ariadne:svn:serverdiff"]
 				),
 			);
 		}
 	
-		$wgWizTitle=$ARnls['ariadne:svn:update'];
+		$wgWizTitle=$ARnls['ariadne:svn:serverdiff'];
 		$wgWizHeader = $wgWizTitle;
-		$wgWizHeaderIcon = $AR->dir->images.'icons/large/svnupdate.png';
+		$wgWizHeaderIcon = $AR->dir->images.'icons/large/svndiff.png';
 
 		include($this->store->get_config("code")."widgets/wizard/yui.wizard.html");
 	}
---
+++ lib/templates/pobject/dialog.svn.templates.serverdiff.save.php
@@ -0,0 +1,31 @@
+<?php
+	ldDisablePostProcessing();
+	$ARCurrent->nolangcheck=true;
+	if (!$this->validateFormSecret()) {
+		error($ARnls['ariadne:err:invalidsession']);      
+		exit;
+	}
+	if ($this->CheckLogin("layout") && $this->CheckConfig()) {
+		set_time_limit(0);
+		$arCallArgs["colorize"] = true;
+		$arCallArgs['revision'] = $this->getdata['revision'] ? $this->getdata['revision'] : "HEAD";
+		$result = $this->call("system.svn.diff.php", $arCallArgs);
+		if( $result != "" ) {
+			echo $result;
+		} else {
+			echo "<pre class='svn_result'>".$ARnls["ariadne:svn:nomod"]."</pre>";
+		}
+		$type = $this->getvar("type");
+		$language = $this->getvar("language");
+		$function = $this->getvar("function");
+		
+		if( $type && $language && $function ) {
+?>
+		<input id="type" type="hidden" name="type" value="<?php echo htmlspecialchars($type); ?>">
+		<input id="language" type="hidden" name="language" value="<?php echo htmlspecialchars($language); ?>">
+		<input id="function" type="hidden" name="function" value="<?php echo htmlspecialchars($function); ?>">
+<?php
+		}
+
+	}
+?>
