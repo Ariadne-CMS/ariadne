5d974e88a8c910480bd0f1f7b504722b03bc5186
Yvo Brevoort <yvo@muze.nl>
2014-01-29 14:13:25 +0000

    Added handling for dropping files and saving them
    git-svn-id: https://svn.muze.nl/svn/ariadne-php5/trunk@6113 7882d7ee-46e0-0310-a255-a28a61b628e4

---
+++ www/js/muze/ariadne/dropzone.js
@@ -0,0 +1,107 @@
+(function() {
+	var holder = document.getElementById('archildren'),
+		tests = {
+			filereader: typeof FileReader != 'undefined',
+			dnd: 'draggable' in document.createElement('span'),
+			formdata: !!window.FormData,
+			progress: "upload" in new XMLHttpRequest
+		}, 
+		progress = document.getElementById('uploadprogress'),
+		fileupload = document.getElementById('upload');
+
+	function filesExist(files) {
+		var formData = tests.formdata ? new FormData() : null;
+		for (var i = 0; i < files.length; i++) {
+			if (tests.formdata) formData.append('filenames[]', files[i].name);
+		}
+		formData.append("filecount", files.length);
+		// now post a new XHR request
+		if (tests.formdata) {
+			var xhr = new XMLHttpRequest();
+			xhr.open('POST', muze.ariadne.registry.get('store_root') + muze.ariadne.registry.get('path') + "mfu.exists.ajax", false); 
+			var result = xhr.send(formData);
+			if (xhr.responseText) {
+				return JSON.parse(xhr.responseText);
+			}
+		}
+		return false;
+	}
+
+	function readfiles(files) {
+		var overwrite = false;
+		var existingFiles = filesExist(files);
+
+		if (existingFiles) {
+			var message = "Overwrite existing files? (";
+			message += existingFiles.join(", ");
+			message += ")";
+
+			overwrite = confirm(message);
+		}
+
+		var formData = tests.formdata ? new FormData() : null;
+		YAHOO.util.Dom.addClass(document.getElementById("archildren"), "dropzone-uploading");
+		for (var i = 0; i < files.length; i++) {
+			if (tests.formdata) formData.append('file[]', files[i]);
+		}
+		formData.append("filecount", files.length);
+		formData.append("overwrite", overwrite);
+
+		// now post a new XHR request
+		if (tests.formdata) {
+			var xhr = new XMLHttpRequest();
+			xhr.open('POST', muze.ariadne.registry.get('store_root') + muze.ariadne.registry.get('path') + "mfu.save.html"); 
+			xhr.onload = function() {
+				progress.value = progress.innerHTML = 100;
+				YAHOO.util.Dom.removeClass(document.getElementById("archildren"), "dropzone-uploading");
+				progress.style.display = "none";
+				window.setTimeout(function() {
+					muze.ariadne.explore.viewpane.view(muze.ariadne.explore.viewpane.path);
+				}, 500);
+			};
+
+			if (tests.progress) {
+				xhr.upload.onprogress = function (event) {
+					if (event.lengthComputable) {
+						var complete = (event.loaded / event.total * 100 | 0);
+						progress.value = progress.innerHTML = complete;
+					}
+				}
+			}
+
+			xhr.send(formData);
+		}
+	}
+
+	function initdropzone() {
+		holder = document.getElementById("archildren");
+		fileupload = document.getElementById("upload");
+		progress = document.getElementById("uploadprogress");
+		if (holder) {
+			YAHOO.util.Dom.addClass(holder, 'dropzone');
+
+			if (tests.dnd) { 
+				holder.ondragover = function () { YAHOO.util.Dom.addClass(this, 'dropzone-hover'); return false; };
+			//	holder.ondragleave = function() { YAHOO.util.Dom.removeClass(this, 'dropzone-hover'); return false; };
+				holder.ondragend = function () { YAHOO.util.Dom.removeClass(this, 'dropzone-hover'); return false; };
+				holder.ondrop = function (e) {
+					this.className = '';
+					e.preventDefault();
+					readfiles(e.dataTransfer.files);
+				}
+			}
+			if (fileupload) {
+				fileupload.style.display = "none";
+				fileupload.onchange = function() {
+					console.log(this.files);
+					readfiles(this.files);
+					this.outerHTML = this.outerHTML; // clear uploader;
+				}
+			}
+		}
+	}
+	muze.event.attach(window, "load", function() {
+		muze.event.attach(document.body, "viewpaneLoaded", initdropzone);
+		initdropzone();
+	});
+}());
