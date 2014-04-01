4a31ff0fedc31e16cbab68a7a1061c55b1fcb025
Yvo Brevoort <yvo@muze.nl>
2014-04-01 07:30:06 +0000

    Removed one 'exit' and whitespace fixes
    git-svn-id: https://svn.muze.nl/svn/ariadne-php5/trunk@6166 7882d7ee-46e0-0310-a255-a28a61b628e4

--- www/install/check.php
+++ www/install/check.php
@@ -48,82 +48,81 @@
 	$ariadne_files = substr($ariadne, 0, strlen($ariadne)-3)."files";
 	check_file( $ariadne_files );
 	if( ! is_writable( $ariadne_files ) ) {
+		cdebug( "$ariadne_files is not writable" );
 		echo "[FATAL] can't write to $ariadne_files even though it exists, check permissions: ";
 		display_perms( fileperms($ariadne_files) );
-		exit();
+	} else {
+		cdebug( "$ariadne_files is writable" );
 	}
-	cdebug( "$ariadne_files is writable" );
 
 	cdebug( "check.php END\n\n");
 
+	function check_file( $file ) {
 
+		if( ! file_exists( $file ) ) {
+			echo "[FATAL] $file can not be found.\n";
+			exit();
+		}
+		
+		cdebug( "$file exists" );
 
-function check_file( $file ) {
-
-	if( ! file_exists( $file ) ) {
-		echo "[FATAL] $file can not be found.\n";
-		exit();
+		if( ! is_readable( $file ) ) {
+			echo "[FATAL] can't read $file even though it exists, check permissions: ";
+			display_perms( fileperms($file) );
+			exit();
+		}
+		
+		cdebug( "$file is readable" );
+		return true;		
 	}
-	
-	cdebug( "$file exists" );
 
-	if( ! is_readable( $file ) ) {
-		echo "[FATAL] can't read $file even though it exists, check permissions: ";
-		display_perms( fileperms($file) );
-		exit();
-	}
-	
-	cdebug( "$file is readable" );
-	
-}
+	function display_perms( $mode ) { 
+		/* Determine Type */ 
+		if( $mode & 0x1000 ) {
+			$type='p'; /* FIFO pipe */ 
+		} else if( $mode & 0x2000 ) {
+			$type='c'; /* Character special */ 
+		} else if( $mode & 0x4000 ) {
+			$type='d'; /* Directory */ 
+		} else if( $mode & 0x6000 ) {
+			$type='b'; /* Block special */ 
+		} else if( $mode & 0x8000 ) {
+			$type='-'; /* Regular */ 
+		} else if( $mode & 0xA000 ) {
+			$type='l'; /* Symbolic Link */ 
+		} else if( $mode & 0xC000 ) {
+			$type='s'; /* Socket */ 
+		} else {
+			$type='u'; /* UNKNOWN */ 
+		}
 
+		/* Determine permissions */ 
+		$owner["read"]	 = ($mode & 00400) ? 'r' : '-'; 
+		$owner["write"]	= ($mode & 00200) ? 'w' : '-'; 
+		$owner["execute"] = ($mode & 00100) ? 'x' : '-'; 
+		$group["read"]	 = ($mode & 00040) ? 'r' : '-'; 
+		$group["write"]	= ($mode & 00020) ? 'w' : '-'; 
+		$group["execute"] = ($mode & 00010) ? 'x' : '-'; 
+		$world["read"]	 = ($mode & 00004) ? 'r' : '-'; 
+		$world["write"]	= ($mode & 00002) ? 'w' : '-'; 
+		$world["execute"] = ($mode & 00001) ? 'x' : '-'; 
 
+		/* Adjust for SUID, SGID and sticky bit */ 
+		if( $mode & 0x800 ) {
+			$owner["execute"] = ($owner['execute']=='x') ? 's' : 'S'; 
+		}
+		if( $mode & 0x400 ) {
+			$group["execute"] = ($group['execute']=='x') ? 's' : 'S'; 
+		}
+		if( $mode & 0x200 ) {
+			$world["execute"] = ($world['execute']=='x') ? 't' : 'T'; 
+		}
 
-
-function display_perms( $mode ) 
-   { 
-      /* Determine Type */ 
-      if( $mode & 0x1000 ) 
-         $type='p'; /* FIFO pipe */ 
-      else if( $mode & 0x2000 ) 
-         $type='c'; /* Character special */ 
-      else if( $mode & 0x4000 ) 
-         $type='d'; /* Directory */ 
-      else if( $mode & 0x6000 ) 
-         $type='b'; /* Block special */ 
-      else if( $mode & 0x8000 ) 
-         $type='-'; /* Regular */ 
-      else if( $mode & 0xA000 ) 
-         $type='l'; /* Symbolic Link */ 
-      else if( $mode & 0xC000 ) 
-         $type='s'; /* Socket */ 
-      else 
-         $type='u'; /* UNKNOWN */ 
-
-      /* Determine permissions */ 
-      $owner["read"]    = ($mode & 00400) ? 'r' : '-'; 
-      $owner["write"]   = ($mode & 00200) ? 'w' : '-'; 
-      $owner["execute"] = ($mode & 00100) ? 'x' : '-'; 
-      $group["read"]    = ($mode & 00040) ? 'r' : '-'; 
-      $group["write"]   = ($mode & 00020) ? 'w' : '-'; 
-      $group["execute"] = ($mode & 00010) ? 'x' : '-'; 
-      $world["read"]    = ($mode & 00004) ? 'r' : '-'; 
-      $world["write"]   = ($mode & 00002) ? 'w' : '-'; 
-      $world["execute"] = ($mode & 00001) ? 'x' : '-'; 
-
-      /* Adjust for SUID, SGID and sticky bit */ 
-      if( $mode & 0x800 ) 
-         $owner["execute"] = ($owner['execute']=='x') ? 's' : 'S'; 
-      if( $mode & 0x400 ) 
-         $group["execute"] = ($group['execute']=='x') ? 's' : 'S'; 
-      if( $mode & 0x200 ) 
-         $world["execute"] = ($world['execute']=='x') ? 't' : 'T'; 
-
-      printf("%1s", $type); 
-      printf("%1s%1s%1s", $owner['read'], $owner['write'], $owner['execute']); 
-      printf("%1s%1s%1s", $group['read'], $group['write'], $group['execute']); 
-      printf("%1s%1s%1s\n", $world['read'], $world['write'], $world['execute']); 
-   } 
+		printf("%1s", $type); 
+		printf("%1s%1s%1s", $owner['read'], $owner['write'], $owner['execute']); 
+		printf("%1s%1s%1s", $group['read'], $group['write'], $group['execute']); 
+		printf("%1s%1s%1s\n", $world['read'], $world['write'], $world['execute']); 
+	} 
 
 	function cdebug( $text ) {
 		global $DEBUGON;
