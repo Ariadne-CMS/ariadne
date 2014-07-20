06572064a2a6aa32b6857aa298875cf473141fe1
Auke van Slooten <auke@muze.nl>
2014-07-20 13:28:28 +0000

    added unit test for ar\files
    git-svn-id: https://svn.muze.nl/svn/ariadne-php5/trunk@6277 7882d7ee-46e0-0310-a255-a28a61b628e4

---
+++ tests/unit/ar/files.php
@@ -0,0 +1,68 @@
+<?php
+
+class arFilesTest extends PHPUnit_Framework_TestCase
+{
+
+	function setUp()
+	{
+		global $ariadne,$store_config,$store,$AR;
+		/* instantiate the store */
+		$inst_store = $store_config["dbms"]."store";
+		$store = new $inst_store($root,$store_config);
+
+		/* now load a user (admin in this case)*/
+		$login = "admin";
+		$query = "object.implements = 'puser' and login.value='$login'";
+		$AR->user = current($store->call('system.get.phtml', '', $store->find('/system/users/', $query)));
+		
+		//FIXME: make a copy of the FilesNls.ax for this test as well.
+	}
+
+	public function testSaveAndGet() {
+		global $AR;
+
+		$me = $this;
+		ar::context()->callAtPath(TESTBASE.'file-nls/', function() use ($me) {
+			$content = "file contents";
+			$result = ar_files::save('test',$content,'nl');
+			$me->assertEquals($result, strlen($content));
+
+			$result = ar_files::get('test','nl');
+			if ( !ar_error::isError($result) ) {
+				$result = $result->getContents();
+			}
+			$me->assertEquals($content, $result);
+		} );
+	}
+
+	public function testLs() {
+		global $AR;
+
+		$me = $this;
+		ar::context()->callAtPath(TESTBASE.'file-nls/', function() use ($me) {
+			$content = "file contents";
+			$result = ar_files::ls();
+			$me->assertEquals(count($result), 1);
+			$me->assertEquals($result[0]['name'], 'test');
+			$me->assertEquals($result[0]['nls'], 'nl');
+
+		} );
+	}
+
+	public function testLs() {
+		global $AR;
+
+		$me = $this;
+		ar::context()->callAtPath(TESTBASE.'file-nls/', function() use ($me) {
+			$content = "file contents";
+			$result = ar_files::ls();
+			$me->assertEquals(count($result), 1);
+			$me->assertEquals($result[0]['name'], 'test');
+			$me->assertEquals($result[0]['nls'], 'nl');
+
+		} );
+	}
+
+
+}
+?>
