7e5607258d5a47a59351c7bc9c5722437b77de3d
Robbert Müller <muller@muze.nl>
2014-08-01 15:27:17 +0000

    remove copyright/license information, this should not be in this file
    git-svn-id: https://svn.muze.nl/svn/ariadne-php5/trunk@6306 7882d7ee-46e0-0310-a255-a28a61b628e4

--- lib/ar/beta/lessc.php
+++ lib/ar/beta/lessc.php
@@ -1,15 +1,5 @@
 <?php
 
-/**
- * lessphp v0.3.8
- * http://leafo.net/lessphp
- *
- * LESS css compiler, adapted from http://lesscss.org
- *
- * Copyright 2012, Leaf Corcoran <leafot@gmail.com>
- * Licensed under MIT or GPLv3, see LICENSE
- */
-
 ar_pinp::allow('ar_beta_lessc');
 
 class ar_beta_lessc extends arBase {
