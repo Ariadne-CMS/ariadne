b894fd101444d6298542765a7da91e020232da72
Yvo Brevoort <yvo@muze.nl>
2014-08-27 09:13:02 +0000

    simplified wizard for articles, just add the tabs you need instead of jumbling the existing order of things
    git-svn-id: https://svn.muze.nl/svn/ariadne-php5/trunk@6354 7882d7ee-46e0-0310-a255-a28a61b628e4

--- lib/templates/particle/dialog.edit.flow.php
+++ lib/templates/particle/dialog.edit.flow.php
@@ -2,19 +2,6 @@
 
 $ARCurrent->nolangcheck=true;
 if (($this->CheckLogin("edit") || $this->CheckLogin("add", ARANYTYPE)) && $this->CheckConfig()) {
-
-	foreach( $wgWizFlow as $key => $flow ) {
-		if( $flow["template"] == "dialog.edit.form.php" ) {
-			$wgWizFlow[$key] = array( //overwrite this index which contains form. form comes last.
-				"title" => $ARnls["scenario"],
-				"image" => $AR->dir->images."wizard/scenario.png",
-				"template" => "dialog.edit.scenario.php",
-				"nolang" => true,
-			);
-			break;
-		}
-	}
-
 	$wgWizFlow[] = array(
 		"title" => $ARnls["date"]."&nbsp;/&nbsp;".$ARnls["time"],
 		"image" => $AR->dir->images."wizard/schedule.png",
@@ -22,12 +9,13 @@ if (($this->CheckLogin("edit") || $this->CheckLogin("add", ARANYTYPE)) && $this-
 		"nolang" => true,
 	);
 
-	$wgWizFlow[] = array(
-		"title" => $ARnls["summary"],
-		"image" => $AR->dir->images."wizard/data.png",
-		"template" => "dialog.edit.form.php",
+	$wgWizFlow[] = array( //overwrite this index which contains form. form comes last.
+		"title" => $ARnls["scenario"],
+		"image" => $AR->dir->images."wizard/scenario.png",
+		"template" => "dialog.edit.scenario.php",
+		"nolang" => true,
 	);
-	
+
 	$arResult = $wgWizFlow;
 }
 
