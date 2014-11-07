1e01f11a2a18cc14832d8251b0768e8bbe41ce10
Robbert Müller <muller@muze.nl>
2014-11-07 21:26:26 +0000

    fix typo and rename
    git-svn-id: https://svn.muze.nl/svn/ariadne-php5/trunk@6483 7882d7ee-46e0-0310-a255-a28a61b628e4

---
+++ tests/unit/pinpCompilerTest.php
@@ -0,0 +1,213 @@
+<?php
+
+require_once(AriadneBasePath."/modules/mod_pinp.phtml");
+
+class pinpCompilerTest extends PHPUnit_Framework_TestCase
+{
+	function testBaseCompile() {
+		$template = <<<'EOD'
+<pinp> $test = 'test'; </pinp>
+EOD;
+		$compiler = new pinp("header", "object->", "\$object->_");
+		$res = $compiler->compile($template);
+		$this->assertNull($compiler->error);
+		$this->assertEquals("<?php  \$object->test = 'test';  ?>",$res);
+	}
+
+	function testObjectVariables() {
+		$this->markTestSkipped("pinp compiler currently has a bug, needs to be fixed");
+		$template = <<<'EOD'
+<pinp>
+	$test = ar('store')->rememberShortcuts;
+	ar('store')->rememberShortcuts = false;
+</pinp>
+EOD;
+		$compiler = new pinp("header", "object->", "\$object->_");
+		$res = $compiler->compile($template);
+		$this->assertNull($compiler->error);
+		if(isset($compiler->error)){
+			var_dump($compiler);
+			var_dump($res);
+		}
+	}
+
+
+	function testclassOperator() {
+		$template = <<<'EOD'
+<pinp>
+	MyClass::CONST_VALUE;
+	parent::myFunc();	
+</pinp>
+EOD;
+
+		$compiler = new pinp("header", "object->", "\$object->_");
+		$res = $compiler->compile($template);
+		$this->assertNull($compiler->error);
+	}
+
+
+	function testclone() {
+		$template = <<<'EOD'
+<pinp>
+  $a = clone($b);
+  $b = clone $b;
+</pinp>
+EOD;
+
+		$compiler = new pinp("header", "object->", "\$object->_");
+		$res = $compiler->compile($template);
+		$this->assertNull($compiler->error);
+	}
+
+
+	function testcomments() {
+		$template = <<<'EOD'
+<pinp>
+// This is a comment
+/*
+  This is a multiline comment
+*/
+# This is also a comment
+</pinp>
+EOD;
+
+		$compiler = new pinp("header", "object->", "\$object->_");
+		$res = $compiler->compile($template);
+		$this->assertNull($compiler->error);
+	}
+
+
+	function testdefine() {
+		$template = <<<'EOD'
+<pinp>
+	define("FOO", "BAR");
+</pinp>
+EOD;
+
+		$compiler = new pinp("header", "object->", "\$object->_");
+		$res = $compiler->compile($template);
+		$this->assertNull($compiler->error);
+	}
+
+
+	function testderef() {
+		$template = <<<'EOD'
+<pinp>
+	$func = "readfile";
+	${$func}();
+
+	$$a;
+</pinp>
+EOD;
+
+		$compiler = new pinp("header", "object->", "\$object->_");
+		$res = $compiler->compile($template);
+		$this->assertNull($compiler->error);
+	}
+
+
+	function testfunctionCalls() {
+		$template = <<<'EOD'
+<pinp>
+	$a->_frop();
+	_frop();
+	$a->{"_frop"}();
+	$$a();
+</pinp>
+EOD;
+
+		$compiler = new pinp("header", "object->", "\$object->_");
+		$res = $compiler->compile($template);
+		$this->assertNull($compiler->error);
+	}
+
+
+	function testheredoc() {
+		$template = <<<'EOD'
+<?php
+
+
+
+<pinp>
+$MyVar = "frop";
+$frop = "Mijn string";
+$str = <<<EOT
+   Example of string {$frop}
+spanning multiple lines
+using heredoc syntax.
+
+MyVar: $MyVar;
+De inhoud van de $MyVar variabel is: ${$MyVar}
+
+EOT;
+
+echo "$str\n";
+</pinp>
+
+EOD;
+
+		$compiler = new pinp("header", "object->", "\$object->_");
+		$res = $compiler->compile($template);
+		$this->assertNull($compiler->error);
+	}
+
+
+	function testmagicMethods() {
+		$template = <<<'EOD'
+<pinp>
+	$object->_call("phpFunc", array());
+</pinp>
+EOD;
+
+		$compiler = new pinp("header", "object->", "\$object->_");
+		$res = $compiler->compile($template);
+		$this->assertNull($compiler->error);
+	}
+
+
+	function teststring() {
+		$template = <<<'EOD'
+<pinp>
+$a = "MyVar";
+$MyVar = "MyVar Value";
+echo "run(\${a}): ${"a"} abc\n";
+echo "run({\$a}): {$a} abc\n";
+echo "run(\${\$a}): ${$a.substr("", 0, 0)} abc\n";
+echo "run(\$a): $a[0]->frop()
+ abc xyz\n";
+echo "frop: $$a;\n";
+
+</pinp>
+
+EOD;
+
+		$compiler = new pinp("header", "object->", "\$object->_");
+		$res = $compiler->compile($template);
+		$this->assertNull($compiler->error);
+	}
+
+
+	function testtypeCasting() {
+		$template = <<<'EOD'
+<pinp>
+	$bool_a		= (bool) 1;
+	$bool_b		= (boolean) 1;
+	$int_a		= (int) "2";
+	$int_b		= (integer) "3";
+	$float_a	= (float) "1.1";
+	$float_b	= (double) "1.2";
+	$float_c	= (real) "1.3";
+	$string_a	= (string) 1;
+	$array_a 	= (array) null;
+	$object_a	= (object) Array("a" => "1", "b" => "2");
+</pinp>
+EOD;
+
+		$compiler = new pinp("header", "object->", "\$object->_");
+		$res = $compiler->compile($template);
+		$this->assertNull($compiler->error);
+	}
+
+
+}
+?>
