1d07d6652652f4995f7c13631d32a385bd17a5e0
Robbert Müller <muller@muze.nl>
2014-11-18 16:24:07 +0000

    add very basic test for mod_htmlcleaner
    git-svn-id: https://svn.muze.nl/svn/ariadne-php5/trunk@6560 7882d7ee-46e0-0310-a255-a28a61b628e4

---
+++ tests/unit/modules/htmlcleanerTest.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once(AriadneBasePath."/modules/mod_htmlcleaner.php");
+
+class htmlcleanerTest extends AriadneBaseTest
+{
+	public function testSimple() {
+		$html = '<html><head><title>test</title></head><body>testbody</body></html>';
+		$clean = htmlcleaner::cleanup($html,array());
+		$this->assertEquals($html, $clean);
+	}
+
+	public function testFormSelectOptionSelected() {
+		$html = '<body>testbody<form><select><option selected></option></select></form></body>';
+		$clean = htmlcleaner::cleanup($html,array());
+		var_dump($clean);
+		$this->assertEquals($html, $clean);
+	}
+
+
+
+}
+?>
