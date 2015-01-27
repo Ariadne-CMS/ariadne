2215a0c0a68d4208117d0ed2508503f2ae577c35
Robbert Müller <muller@muze.nl>
2015-01-27 14:18:54 +0000

    mogrify dirs to subtypes
    git-svn-id: https://svn.muze.nl/svn/ariadne-php5/trunk@6742 7882d7ee-46e0-0310-a255-a28a61b628e4

---
+++ www/install/upgrade/9.0/mogrify.system.folders.php
@@ -0,0 +1,20 @@
+<pre>
+<?php
+$importpath = false;
+
+function mogrifyDir($path,$type) {
+	echo "== mogrify $path to $type\n";
+	$res = current(ar::get($path)->call('system.get.phtml'));
+	if($res && $res->type == "pdir") {
+		print "Oldtype  is pdir, mogrify to new type $type\n";
+		$res = ar::get($path)->call('system.mogrify.phtml',array('type' => $type));
+	}
+}
+
+mogrifyDir('/system/profiles/','pdir.profiles');
+mogrifyDir('/system/users/',   'pdir.users');
+mogrifyDir('/system/groups/',  'pdir.groups');
+?>
+
+</pre>
+
