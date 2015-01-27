186fe301328ff623dd18a21a30c7869208e79244
Robbert Müller <muller@muze.nl>
2015-01-27 14:18:51 +0000

    add lib importer
    git-svn-id: https://svn.muze.nl/svn/ariadne-php5/trunk@6741 7882d7ee-46e0-0310-a255-a28a61b628e4

---
+++ www/install/upgrade/9.0/install.muze.libs.php
@@ -0,0 +1,48 @@
+<pre>
+<?php
+$importpath = false;
+echo "== testing for lib folder\n\n";
+$res = current(ar::get('/system/lib/muze/')->call('system.get.phtml'));
+if(!$res) {
+	print "No muze libs found\n";
+	$importpath = '/system/lib/muze/';
+	$res = current(ar::get('/system/lib/')->call('system.get.phtml'));
+	if(!$res) {
+		print "No lib folder found\n";
+		$importpath = '/system/lib/';
+	}
+} else {
+	print "Old muzelibs found, upgrade ?\n";
+}
+
+if($importpath) {
+	print "importing $importpath\n";
+	$ARCurrent->nolangcheck = true;
+	$ARCurrent->options["verbose"]=true;
+	$ARCurrent->AXAction == "import";
+
+	// become admin
+	$AR->user=new object;
+	$AR->user->data=new object;
+	$AR->user->data->login=$ARLogin="admin";
+
+	$ax_config["writeable"]=false;
+	$ax_config["database"]="../packages/base.ax";
+	echo "ax file (".$ax_config["database"].")\n";
+	set_time_limit(0);
+	$inst_store = $ax_config["dbms"]."store";
+	$axstore=new $inst_store("", $ax_config);
+	if (!$axstore->error) {
+		$ARCurrent->importStore=&$store;
+		$args['srcpath'] = $importpath;
+		$args['destpath'] = $importpath;
+		$axstore->call("system.export.phtml", $args, $axstore->get("/"));
+		$error=$axstore->error;
+		$axstore->close();
+	} else {
+		$error=$axstore->error;
+	}
+}
+
+?>
+</pre>
