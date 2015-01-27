20a117c934d11027fa86877e6401dc0612311631
Robbert Müller <muller@muze.nl>
2015-01-27 14:18:48 +0000

    import missing data upgrade script
    git-svn-id: https://svn.muze.nl/svn/ariadne-php5/trunk@6740 7882d7ee-46e0-0310-a255-a28a61b628e4

---
+++ www/install/upgrade/9.0/install.missing.data.php
@@ -0,0 +1,56 @@
+<pre>
+<?php
+
+function importMissingData($path) {
+	global $ARCurrent, $AR, $ax_config, $store;
+	$importpath = false;
+	echo "== testing for $path\n\n";
+	$res = current(ar::get($path)->call('system.get.phtml'));
+	if(!$res) {
+		print "$path does not exists\n";
+		$importpath = $path;
+	}
+
+	if($importpath) {
+		print "importing $importpath\n";
+		$ARCurrent->nolangcheck = true;
+		$ARCurrent->options["verbose"]=true;
+		$ARCurrent->AXAction == "import";
+
+		// become admin
+		$AR->user=new object;
+		$AR->user->data=new object;
+		$AR->user->data->login=$ARLogin="admin";
+
+		$ax_config["writeable"]=false;
+		$ax_config["database"]="../packages/base.ax";
+		echo "ax file (".$ax_config["database"].")\n";
+		set_time_limit(0);
+		$inst_store = $ax_config["dbms"]."store";
+		$axstore=new $inst_store("", $ax_config);
+		if (!$axstore->error) {
+			$ARCurrent->importStore=&$store;
+			$args['srcpath'] = $importpath;
+			$args['destpath'] = $importpath;
+			$axstore->call("system.export.phtml", $args, $axstore->get("/"));
+			$error=$axstore->error;
+			$axstore->close();
+		} else {
+			$error=$axstore->error;
+		}
+	}
+}
+
+$paths = array (
+		'/system/newspaper/scenarios/fixed/',
+		'/system/newspaper/displays/fixed/',
+		'/system/profiles/administrators/',
+		'/system/scaffolds/'
+	);
+
+foreach($paths as $path){
+	importMissingData($path);
+}
+
+?>
+</pre>
