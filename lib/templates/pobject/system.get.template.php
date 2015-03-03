1da685dfa7d2ce4e9698212483d276a4d871b10e
Auke van Slooten <auke@muze.nl>
2015-03-03 09:14:30 +0000

    added defensive checks
    git-svn-id: https://svn.muze.nl/svn/ariadne-php5/trunk@6879 7882d7ee-46e0-0310-a255-a28a61b628e4

--- lib/templates/pobject/system.get.template.php
+++ lib/templates/pobject/system.get.template.php
@@ -1,7 +1,7 @@
 <?php
 	if ( ( $this->CheckLogin('layout') || $this->CheckLogin('layout:read') ) && $this->CheckConfig() ) {
         $file = "";
-        if ($this->data->config->pinp[$type][$function][$language]) {
+        if ( isset($this->data->config->pinp[$type][$function][$language]) ) {
             $template=$type.".".$function.".".$language.".pinp";
             $templates=$this->store->get_filestore("templates");
             if ($templates->exists($this->id, $template)) {
