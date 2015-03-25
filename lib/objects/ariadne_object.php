a4e93c2f55ce6f2970dc7ba41018d4795da18d2e
Robbert Müller <muller@muze.nl>
2015-03-25 10:36:05 +0100

    fire event before cleaning private cache

--- lib/objects/ariadne_object.php
+++ lib/objects/ariadne_object.php
@@ -2118,6 +2118,12 @@ debug("loadLibrary: loading cache for $this->path");
 					$ids=$this->store->info($this->store->find($path, "" , $limit, $offset));
 					while (is_array($ids) && sizeof($ids)) {
 						foreach($ids as $value) {
+							$eventData = new object();
+							$eventData = ar_events::fire( 'onbeforeclearprivatecache', $eventData, $value['type'], $value['path'] );
+							if ( !$eventData ) {
+								continue;
+							}
+
 							$pcache->purge($value["id"]);
 						}
 
@@ -2125,7 +2131,11 @@ debug("loadLibrary: loading cache for $this->path");
 						$ids = $this->store->info($this->store->find($path, "", $limit, $offset));
 					}
 				} else {
-					$pcache->purge($this->id);
+					$eventData = new object();
+					$eventData = ar_events::fire( 'onbeforeclearprivatecache', $eventData, $this->type, $this->path );
+					if ( $eventData ) {
+						$pcache->purge($this->id);
+					}
 				}
 			}
 
