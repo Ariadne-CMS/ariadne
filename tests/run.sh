c620f4f67a672e0e8fa5ad5f567befd5851f9c08
Robbert Müller <muller@muze.nl>
2015-12-23 10:50:13 +0100

    Add php7.0
    also split syntax checking in seperate build
    allow php 7.0 to fail on syntax checks

--- tests/run.sh
+++ tests/run.sh
@@ -248,14 +248,4 @@ if [ ${EXPORT_BASE_TEMPLATES_COUNT} -ne ${EXPORT_EXPORT_TEMPLATES_COUNT} ] ; the
 	cat ${TMPDIR}/BASE_TEMPLATES.txt ${TMPDIR}/EXPORT_TEMPLATES.txt
 	exit 1
 fi
-
-cd ${BUILDROOT}
-SYNTAX_ERROR_COUNT=`(find www -type f -name \*php ; find www/install/conf lib ftp soap webdav -type f)  | grep -v 'lib/ar/beta/' | xargs -n1 --replace={} bash -c 'php -d "error_reporting=E_ALL & ~E_STRICT & ~E_NOTICE " -d short_open_tag=off -l {} || true' | grep -v 'No syntax errors detected in'  | tee ${TMPDIR}/syntax.errors.txt | wc -l`
-
-if [ ${SYNTAX_ERROR_COUNT} -ge 1 ]; then
-	echo "syntax errors found in build";
-	cat ${TMPDIR}/syntax.errors.txt
-	exit 1;
-fi
-
 echo "Tests done"
