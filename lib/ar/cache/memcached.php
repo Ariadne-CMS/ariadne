33a85d51e9dbf97cd55e56bc52b067d1cfba8f8b
Robbert Müller <spam.me@grols.ch>
2016-03-12 18:34:47 +0100

    initial, dry coded, memcache implementation

---
+++ lib/ar/cache/memcached.php
@@ -0,0 +1,58 @@
+<?php
+
+ar_pinp::allow('ar_cache_memcachedStore');
+
+class ar_cache_memcachedStore implements ar_cacheStoreInterface, arKeyValueStoreInterface {
+	private $timeout = 7200;
+	private $mc = null;
+	public function __construct( $servers, $timeout = 7200,  $prefix = 'AR_') {
+
+		if ( is_string($timeout) ) {
+			$timeout = strtotime( $timeout, 0);
+		}
+
+		$this->timeout = $timeout;
+		$this->mc = $mc = new memCached();
+		$mc->addServers($servers);
+
+	}
+
+	// key
+	public function get( $path ) {
+		$res = $this->mc->get([$path]);
+		if(count($res) === 0 ){
+			return current($res);
+		}
+		return null;
+	}
+
+	// key value expire
+	public function set( $path, $value, $timeout = 7200 ) {
+			return $this->mc->set($path, $value, 0, $timeout);
+	}
+	
+
+
+	// meta info
+	public function info( $path ) {
+		$res = [];
+		// FIXME: implement this
+		return $res;
+	}
+
+	// remove key
+	public function clear( $path = null ) {
+		return $this->mc->delete($path);
+	}
+
+
+	public function subStore( $path ) {
+		// geen idee eigenlijk
+	}
+
+	// hoeveel tijd hebben we nog
+	public function isFresh( $path ) {
+		return null; // we hebben deze data niet
+	}
+}
+
