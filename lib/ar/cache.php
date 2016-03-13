507111107752ff33c8400a1d9d1b6a9d519ddd3e
Robbert Müller <spam.me@grols.ch>
2016-03-13 11:04:20 +0100

    add missing function from interface

--- lib/ar/cache.php
+++ lib/ar/cache.php
@@ -194,6 +194,7 @@
 		public function clear( $path = null );
 		public function subStore( $path );
 		public function isFresh( $path );
+		public function getIfFresh( $name, $freshness=0 );
 	}
 
 	class ar_cacheStore implements ar_cacheStoreInterface, arKeyValueStoreInterface {
