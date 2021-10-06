da8c398e7802acdbe7b83c47cd070300d40275ce
Auke van Slooten <auke@muze.nl>
2021-10-06 14:45:51 +0200

    fixed incorrect padding of array

--- lib/ar/template/filestore.php
+++ lib/ar/template/filestore.php
@@ -61,7 +61,7 @@
 				if (isset($names)) foreach($names as $name => $languages) {
 					if (isset($languages)) foreach($languages as $language => $id ) {
 						$tempname = sprintf("%s.%s.%s",$type,$name,$language);
-						list($maintype,$subtype) = array_pad(explode('.', $type, 2), 2, '');
+						list($maintype,$subtype) = array_pad(explode('.', $type, 2), 2, null);
 						if(!isset($result[$name])) {
 							$result[$name] = [];
 						}
