2fec5250c7d5082455c2deb2eb6c4d56dc1153f6
Yvo Brevoort <yvo@muze.nl>
2025-03-14 10:58:52 +0000

    add test, refactor testRunner to its own file

---
+++ tests/surface/help.about.php
@@ -0,0 +1,9 @@
+#!/usr/bin/env php
+<?php
+	$tests = [
+		"help.about.php" => [
+			[]
+		]
+	];
+
+	include_once("testRunner.php");
---
+++ tests/surface/testRunner.php
@@ -0,0 +1,72 @@
+#!/usr/bin/env php
+<?php
+	if (!isset($tests)) {
+		echo "OK" . PHP_EOL;
+		exit(0);
+	}
+	
+	$ARLoader = 'cmd';
+	$currentDir = getcwd();
+	$ariadne = dirname($currentDir,2).'/lib/';
+
+	if (!@include_once($ariadne."/bootstrap.php")) {
+		chdir(substr($_SERVER['PHP_SELF'], 0, strrpos($_SERVER['PHP_SELF'], '/')));
+		$ariadne = dirname(getcwd()).'/lib/';
+
+		if(!include_once($ariadne."/bootstrap.php")){
+			echo "could not find Ariadne";
+			exit(1);
+		}
+
+		chdir($currentDir);
+	}
+
+	$inst_store = $store_config["dbms"]."store";
+	$store=new $inst_store($root??null,$store_config);
+
+	/* now load a user (admin in this case)*/
+	$login = "admin";
+	$query = "object.implements = 'puser' and login.value='$login'";
+	$AR->user = current($store->call('system.get.phtml', '', $store->find('/system/users/', $query)));
+	
+	function runTemplate($targetPath, $templateName, $args) {
+		global $store;
+		$targetOb = current($store->call("system.get.phtml", '', $store->get($targetPath)));
+		ob_start();
+		$targetOb->call($templateName, $args);
+		$result = ob_get_contents();
+		ob_end_clean();
+		return $result;
+	}
+
+	$foundFailures = false;
+	$targetPath = "/";
+	foreach ($tests as $templateName => $argVariants) {
+		foreach ($argVariants as $args) {
+			$testFailed = false;
+			$result = runTemplate($targetPath, $templateName, $args);
+			if (preg_match_all("/(Deprecated|Fatal|Warning).*/", $result, $matches)) {
+				if (!$foundFailures) {
+					echo "FAILED" . PHP_EOL;
+				}
+				$foundFailures = true;
+				$testFailed = true;
+				echo "-----------------------------------" . PHP_EOL;
+				echo "Failures in test $templateName with args " . json_encode($args) . PHP_EOL;
+				echo "-----------------------------------" . PHP_EOL;
+				foreach ($matches[0] as $match) {
+					echo $match . PHP_EOL;
+				}
+				echo "-----------------------------------" . PHP_EOL;
+			}
+		}
+	}
+
+	$store->close();
+
+	if ($foundFailures) {
+		exit(1);
+	} else {
+		echo "OK" . PHP_EOL;
+		exit(0);
+	}
