6edc7fdd824d5e449ba86d515c1aff5c39be121c
Yvo Brevoort <yvo@muze.nl>
2025-04-18 12:58:58 +0200

    rmeove var_dumps

--- lib/templates/pobject/login.redirect.php
+++ lib/templates/pobject/login.redirect.php
@@ -4,8 +4,6 @@
 		if( count($_GET) ) {
 			$query = "?".http_build_query($_GET);
 		}
-		var_dump($arRequestedTemplate);
-		var_dump($query);
 		ldRedirect($this->make_local_url().$arRequestedTemplate.$query);
 	}
 ?>
