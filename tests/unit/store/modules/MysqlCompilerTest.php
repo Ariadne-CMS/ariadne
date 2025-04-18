fb908c2f23c69a3e2da8034d0b17a0037ebe5c46
Yvo Brevoort <yvo@muze.nl>
2025-04-18 15:18:06 +0200

    assert empty instead of null, mysql compiler has empty strings for errors

--- tests/unit/store/modules/MysqlCompilerTest.php
+++ tests/unit/store/modules/MysqlCompilerTest.php
@@ -24,7 +24,7 @@ class MysqlCompilerTest extends AriadneBaseTest
 		$compiler = 'mysql_compiler';
 		$compiler = new $compiler($store,'store_');
 		$res = $compiler->compile('/',"owner.value = 'admin'");
-		$this->assertNull($compiler->error,"Compiler error");
+		$this->assertEmpty($compiler->error,"Compiler error");
 		$prepared["select_query"]=
 		     "select distinct(store_nodes.path), store_objects.id, store_nodes.parent, store_nodes.priority, store_objects.type,  UNIX_TIMESTAMP(store_objects.lastchanged) as lastchanged, store_objects.vtype from (store_prop_owner, store_nodes, store_objects)   where store_nodes.object=store_objects.id  and store_prop_owner.object=store_objects.id  and store_nodes.path like '/%'  and (   store_prop_owner.AR_value  = 'admin'  )   order by store_nodes.parent ASC, store_nodes.priority DESC, store_nodes.path ASC   limit 0, 100  ";
 		$prepared["count_query"]=
@@ -38,7 +38,7 @@ class MysqlCompilerTest extends AriadneBaseTest
 		$compiler = 'mysql_compiler';
 		$compiler = new $compiler($store,'store_');
 		$res = $compiler->compile('/',"owner.value = 'ad\'min'");
-		$this->assertNull($compiler->error,"Compiler error");
+		$this->assertEmpty($compiler->error,"Compiler error");
 		$prepared["select_query"] =
 					 "select distinct(store_nodes.path), store_objects.id, store_nodes.parent, store_nodes.priority, store_objects.type,  UNIX_TIMESTAMP(store_objects.lastchanged) as lastchanged, store_objects.vtype from (store_prop_owner, store_nodes, store_objects)   where store_nodes.object=store_objects.id  and store_prop_owner.object=store_objects.id  and store_nodes.path like '/%'  and (   store_prop_owner.AR_value  = 'ad\'min'  )   order by store_nodes.parent ASC, store_nodes.priority DESC, store_nodes.path ASC   limit 0, 100  ";
 		$prepared["count_query"] =
@@ -51,7 +51,7 @@ class MysqlCompilerTest extends AriadneBaseTest
 		$compiler = 'mysql_compiler';
 		$compiler = new $compiler($store,'store_');
 		$res = $compiler->compile('/system/',"");
-		$this->assertNull($compiler->error,"Compiler error");
+		$this->assertEmpty($compiler->error,"Compiler error");
 		$prepared["select_query"]=
 		     "select distinct(store_nodes.path), store_objects.id, store_nodes.parent, store_nodes.priority, store_objects.type,  UNIX_TIMESTAMP(store_objects.lastchanged) as lastchanged, store_objects.vtype from (store_nodes, store_objects)   where store_nodes.object=store_objects.id  and store_nodes.path like '/system/%'   order by store_nodes.parent ASC, store_nodes.priority DESC, store_nodes.path ASC   limit 0, 100  ";
 		$prepared["count_query"]=
@@ -95,7 +95,7 @@ class MysqlCompilerTest extends AriadneBaseTest
 		$compiler = 'mysql_compiler';
 		$compiler = new $compiler($store,'store_');
 		$res = $compiler->compile('/system/',"order by none limit 10,42");
-		$this->assertNull($compiler->error,"Compiler error");
+		$this->assertEmpty($compiler->error,"Compiler error");
 		$prepared = array (
 		    'select_query' => "select distinct(store_nodes.path), store_objects.id, store_nodes.parent, store_nodes.priority, store_objects.type,  UNIX_TIMESTAMP(store_objects.lastchanged) as lastchanged, store_objects.vtype from (store_nodes, store_objects)   where store_nodes.object=store_objects.id  and store_nodes.path like '/system/%'    limit 10, 42  ",
 			 'count_query' => "select count(distinct(store_nodes.path)) as count from store_nodes, store_objects  where store_nodes.object=store_objects.id  and store_nodes.path like '/system/%' "
