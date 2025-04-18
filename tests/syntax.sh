e7882f9752d2d6bd74b2f2f9cb7c35871fd1ad1f
Yvo Brevoort <yvo@muze.nl>
2025-04-18 14:30:48 +0200

    fix xargs issue to let this run again

--- tests/syntax.sh
+++ tests/syntax.sh
@@ -10,7 +10,7 @@ TMPDIR=`mktemp -d `
 
 cd ${BUILDROOT}
 
-SYNTAX_ERROR_COUNT=`(find www -type f -name \*php ; find www/install/conf lib ftp soap webdav -type f)  | grep -v 'lib/ar/beta/' | xargs -n1 --replace={} bash -c 'php -d "error_reporting=E_ALL & ~E_STRICT & ~E_NOTICE " -d short_open_tag=off -l {} || true' | grep -v 'No syntax errors detected in'  | tee ${TMPDIR}/syntax.errors.txt | wc -l`
+SYNTAX_ERROR_COUNT=`(find www -type f -name \*php ; find www/install/conf lib ftp soap webdav -type f)  | grep -v 'lib/ar/beta/' | xargs --replace={} bash -c 'php -d "error_reporting=E_ALL & ~E_STRICT & ~E_NOTICE " -d short_open_tag=off -l {} || true' | grep -v 'No syntax errors detected in'  | tee ${TMPDIR}/syntax.errors.txt | wc -l`
 
 if [ ${SYNTAX_ERROR_COUNT} -ge 1 ]; then
 	echo "syntax errors found in build";
