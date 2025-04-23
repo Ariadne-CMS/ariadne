6fe4ef95c3142e7dff5ba442f7b967a53a2e4eb7
Yvo Brevoort <yvo@muze.nl>
2025-04-23 22:24:18 +0200

    update version

--- lib/version.php
+++ lib/version.php
@@ -1,6 +1,6 @@
 <?php
 	$ARversion = array();
-	$ARversion['version'] = '12rc1';
-	$ARversion['date'] = strtotime('2025-03-01');
+	$ARversion['version'] = '12-rc1';
+	$ARversion['date'] = strtotime('2025-04-24');
 	$ARversion['year'] = DateTimeImmutable::createFromFormat('U', $ARversion['date'])->format('Y');
 
