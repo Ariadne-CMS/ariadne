0660d3379735465b42135067f41ff93286008c71
Yvo Brevoort <yvo@muze.nl>
2025-05-19 15:23:56 +0200

    update installer

--- docker/install-ariadne.sh
+++ docker/install-ariadne.sh
@@ -5,7 +5,7 @@ chown www-data:www-data /opt/ariadne/lib/configs/ariadne.phtml
 chown -R www-data:www-data /opt/ariadne/lib/configs/svn/
 chown -R www-data:www-data /opt/ariadne/files/
 
-curl --data "language=en&step=step6&database=mysql&database_host=mysql&database_user=ariadne&database_pass=secret&database_name=ariadne&admin_pass=test&admin_pass_repeat=test&ariadne_location&enable_svn=1&enable_workspaces=0&install_demo=1" http://localhost/ariadne/install/index.php
+curl --data "language=en&step=step6&database=mysql&database_host=mysql&database_user=ariadne&database_pass=Hohtai6shaht&database_name=ariadne&admin_pass=test&admin_pass_repeat=test&ariadne_location&enable_svn=1&enable_workspaces=0&install_demo=1" http://localhost/ariadne/install/index.php
 
 # Add vhosts
 cp /opt/ariadne/docker/000-default.conf /etc/apache2/sites-enabled/000-default.conf
