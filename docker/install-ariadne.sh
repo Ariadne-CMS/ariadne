13e9109367d4e51a399691c6844aa7b321640525
Yvo Brevoort <yvo@muze.nl>
2025-05-23 11:29:52 +0200

    add default apache config update composer stuff in install

--- docker/install-ariadne.sh
+++ docker/install-ariadne.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-rm /var/www/html/install/.htaccess
 chown www-data:www-data /opt/ariadne/lib/configs/ariadne.phtml
 chown -R www-data:www-data /opt/ariadne/lib/configs/svn/
 chown -R www-data:www-data /opt/ariadne/files/
 
+# Reinstall composer packages
+cd /opt/ariadne/
+rm -rf /opt/ariadne/vendor/*
+rm composer.lock
+composer install
+
+# Run Ariadne installer
 curl --data "language=en&step=step6&database=mysql&database_host=mysql&database_user=ariadne&database_pass=Hohtai6shaht&database_name=ariadne&admin_pass=test&admin_pass_repeat=test&ariadne_location&enable_svn=1&enable_workspaces=0&install_demo=1" http://localhost/ariadne/install/index.php
 
 # Add vhosts
