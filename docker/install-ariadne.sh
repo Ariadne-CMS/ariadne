de075988a159dc16c48938f2663ed528196645b4
Yvo Brevoort <yvo@muze.nl>
2025-05-23 11:52:41 +0200

    move ariadne.inc to docker build

--- docker/install-ariadne.sh
+++ docker/install-ariadne.sh
@@ -10,8 +10,5 @@ rm -rf /opt/ariadne/vendor/*
 rm composer.lock
 composer install
 
-# Setup default ariadne.inc
-echo "<?php\n\$ariadne='/opt/ariadne/lib';\n" > /var/www/html/ariadne/ariadne.inc
-
 # Run Ariadne installer
 curl --data "language=en&step=step6&database=mysql&database_host=mysql&database_user=ariadne&database_pass=${MYSQL_PASSWORD}&database_name=ariadne&admin_pass=${ARIADNE_PASSWORD}&admin_pass_repeat=${ARIADNE_PASSWORD}&ariadne_location&enable_svn=1&enable_workspaces=0&install_demo=1" https://localhost/ariadne/install/index.php
