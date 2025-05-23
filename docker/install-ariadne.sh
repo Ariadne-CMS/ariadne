712ef1923895edc72aed7c6bdcec8a460b2cf2e6
Yvo Brevoort <yvo@muze.nl>
2025-05-23 11:43:05 +0200

    use vars form env

--- docker/install-ariadne.sh
+++ docker/install-ariadne.sh
@@ -10,8 +10,11 @@ rm -rf /opt/ariadne/vendor/*
 rm composer.lock
 composer install
 
+# Setup default ariadne.inc
+echo "<?php\n\$ariadne='/opt/ariadne/lib';\n" > /var/www/html/ariadne/ariadne.inc
+
 # Run Ariadne installer
-curl --data "language=en&step=step6&database=mysql&database_host=mysql&database_user=ariadne&database_pass=Hohtai6shaht&database_name=ariadne&admin_pass=test&admin_pass_repeat=test&ariadne_location&enable_svn=1&enable_workspaces=0&install_demo=1" http://localhost/ariadne/install/index.php
+curl --data "language=en&step=step6&database=mysql&database_host=mysql&database_user=ariadne&database_pass=${MYSQL_PASSWORD}&database_name=ariadne&admin_pass=${ARIADNE_PASSWORD}&admin_pass_repeat=${ARIADNE_PASSWORD}&ariadne_location&enable_svn=1&enable_workspaces=0&install_demo=1" http://localhost/ariadne/install/index.php
 
 # Add vhosts
 cp /opt/ariadne/docker/000-default.conf /etc/apache2/sites-enabled/000-default.conf
